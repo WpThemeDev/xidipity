ccc6752736bf75f3ca65a074f993804f1e0b727a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2018-10-30 17:58:39 -0400

    Change image url
    Changed image URL to a SVG definition.

--- assets/tinymceplugins/twocolumn/plugin.js
+++ assets/tinymceplugins/twocolumn/plugin.js
@@ -1,7 +1,7 @@
 /**
  * Plugin Name: TinyMCE Two Column Plugin
  * Plugin URI: http:www.xidipity.com
- * Version: 1.0
+ * Build: 81030
  * Author: John Baer
  * Description: A TinyMCE Plugin to add two column format to the MCE Visual Editor
  * License: GPL2
@@ -12,7 +12,7 @@
         editor.addButton( 'twocolumn', {
             title: 'Two Column',
             icon: 'mce-column',
-            image: '/wp-content/themes/xidipity/assets/tinymceplugins/twocolumn/mce-column.svg',
+            image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB3aWR0aD0iMjQiIGhl%0D%0AaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIw%0D%0AMDAvc3ZnIj4KICA8cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDBWMHoiLz4KICA8cGF0%0D%0AaCBkPSJNIDMgMTUgTCAxMSAxNSBMIDExIDEzIEwgMyAxMyBMIDMgMTUgWiBNIDMgMTkgTCAxMSAx%0D%0AOSBMIDExIDE3IEwgMyAxNyBMIDMgMTkgWiBNIDMgMTEgTCAxMSAxMSBMIDExIDkgTCAzIDkgTCAz%0D%0AIDExIFogTSAzIDUgTCAzIDcgTCAxMSA3IEwgMTEgNSBMIDMgNSBaIE0gMTMgNSBMIDIxIDUgTCAy%0D%0AMSAxOSBMIDEzIDE5IEwgMTMgNSBaIiBzdHlsZT0iZmlsbDogcmdiKDMzLCAzMywgMzMpOyIvPgog%0D%0AIDxyZWN0IHg9IjEyLjk1IiB5PSI0Ljk5OCIgd2lkdGg9IjguMDQyIiBoZWlnaHQ9IjE0LjAwMyIg%0D%0Ac3R5bGU9ImZpbGw6IHJnYigxMTcsIDExNywgMTE3KTsiLz4KPC9zdmc+',
             onclick: function() {
               var html = '<table id="twocol" class="twocolumn">' + String.fromCharCode(13) +
               '  <tbody>' + String.fromCharCode(13) +
