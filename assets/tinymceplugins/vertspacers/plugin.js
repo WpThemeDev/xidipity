00b14b612b60739d0b71a6673a3d0f91b2714167
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2018-10-30 18:02:03 -0400

    Change image url
    Changed image URL to a SVG definition.

--- assets/tinymceplugins/vertspacers/plugin.js
+++ assets/tinymceplugins/vertspacers/plugin.js
@@ -1,19 +1,17 @@
 /**
    * Plugin Name: TinyMCE Vertical Spacers Plugin
    * Plugin URI: http:www.xidipity.com
-   * Version: 80906.1
+   * Build: 81030
    * Author: John Baer
    * Description: A TinyMCE Plugin to add horizonal spacer snippets to the MCE Visual Editor
    * License: GPL2
    */
    
-  var imgvertsp = '/wp-content/themes/xidipity/assets/tinymceplugins/vertspacers/mce-vert-spacer.svg';
-
   (function() {
     tinymce.PluginManager.add('vspacer', function(editor, url) {
       editor.addButton('vspacer', {
         title: 'Vertical Spacers',
-        image: imgvertsp,
+        image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAg%0D%0AMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTSAz%0D%0AIDE5IEwgMjEgMTkgTCAyMSAxMyBMIDMgMTMgTCAzIDE5IFogTSAzIDExIEwgMjEgMTEgTCAyMSA5%0D%0AIEwgMyA5IEwgMyAxMSBaIE0gMyA1IEwgMyA3IEwgMjEgNyBMIDIxIDUgTCAzIDUgWiIgc3R5bGU9%0D%0AImZpbGw6IHJnYigzMywgMzMsIDMzKTsiLz4KPC9zdmc+',
         type: 'menubutton',
         menu: [{
             text: '• 1/2 Height',
