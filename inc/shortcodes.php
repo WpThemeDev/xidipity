2eb07d82bf92deeaeacafcecaf071b35724415d1
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2018-12-18 19:50:43 -0500

    shortcodes.php - 81218.2 lst_blogs
    Update error message to say "List Blogs"..

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -2,7 +2,7 @@
 /**
  * ------------------------- Xidipity Short Codes -------------------------
  file        - shortcodes.php
- Build       - 81218.1
+ Build       - 81218.2
  Programmer  - John Baer
  Purpose     - Support file for Xidipity Wordpress Theme
  License     - GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
@@ -640,7 +640,7 @@ function lst_blogs_shortcode($atts, $category_list)
       /**
        * Return if no posts match the current query.
        */
-      return disp_error('Xidipity Blogs Shortcode - no posts assigned to (' . $categories . ').');
+      return disp_error('List Blogs Shortcode - no posts assigned to (' . $categories . ').');
     }
     
     $html       = '';
