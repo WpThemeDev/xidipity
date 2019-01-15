300e533be9ddedb2d8edea479b432297146a7a31
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-01-15 15:14:52 -0500

    plugin.js - 90115.1
    Update template wrapper.

--- assets/tinymceplugins/adsense/plugin.js
+++ assets/tinymceplugins/adsense/plugin.js
@@ -1,7 +1,7 @@
 /**
  * Plugin Name: TinyMCE Adsense Plugin
  * Plugin URI: http:www.xidipity.com
- * Build: 81010
+ * Build: 90115.1
  * Author: John Baer
  * Description: A TinyMCE Plugin to add templates / snippets to the MCE Visual Editor
  * License: GPL2
@@ -28,12 +28,16 @@
               minWidth: window.innerWidth * .4,
               minHeight: window.innerHeight * .61,
               style:  "direction: ltr; text-align: left; height: 100%; font-family: 'Roboto Mono', monospace; color: #212121; border: 1px solid #f5f5f5;",
-              value:  '<blockquote class="web:hidden" style="border-color: #d1c4e9;" cite="https://www.google.com/adsense/">' + String.fromCharCode(13) +
+              value:  '<p>&nbsp;</p>' + String.fromCharCode(13) +
+                      '<!-- Template: adsense -->' + String.fromCharCode(13) +
+                      '<blockquote class="web:hidden" style="border-color: #d1c4e9;" cite="https://www.google.com/adsense/">' + String.fromCharCode(13) +
                       '<h3 style="color: #616161;">Google Adsense</h3>' + String.fromCharCode(13) +
                       '<p><i class="material-icons pr-2" style="font-size: 1.25rem; vertical-align: bottom;">devices_other</i>Responsive</p>' + String.fromCharCode(13) +
                       '</blockquote>' + String.fromCharCode(13) +
-                      '<p class="mce:hidden">[google_adsense client="CLIENT ID" slot="SLOT ID"]</p>'
-                      },
+                      '<p class="mce:hidden">[google_adsense client="CLIENT ID" slot="SLOT ID"]</p>' + String.fromCharCode(13) +
+                      '<!-- End Template -->' + String.fromCharCode(13) +
+                      '<p>&nbsp;</p>'
+            },
             onSubmit: function() {
               var html = document.getElementsByClassName("mce-textbox")[0].value;
               editor.insertContent(html);
