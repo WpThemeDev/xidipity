831cea4c6f543a00040566c1fe552c07ea15540e
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-03-21 15:47:21 -0400

    404.php - 90321.1 update
    Numerous updates.

--- 404.php
+++ 404.php
@@ -1,31 +1,45 @@
 <?php
-/**
- * Template for displaying 404 image.
+/*
+ *        file: 404.php
+ *       build: 90321.1
+ * description: Template for displaying 404 image.
+ *      github: https://github.com/WpThemeDev/xidipity
+ *    comments:
  *
- * @package xidipity
- */
-?>
+ * @package WordPress
+ * @subpackage Xidipity
+ * @since 5.0.0
+ *
+ ***/ ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset=" <?php bloginfo( 'charset' ); ?> ">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-  <div id="page" class="pl-4">
-  	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'xidipity' ); ?></a>
-  	<header id="masthead" class="site-header">
-  	  <div class="entry-header-wrapper">
-		    <p class="text-5xl"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-  			<p class="site-description text-left pt-3"><?php bloginfo( 'description' ); ?></p>
-  	  </div>
-   	</header><!-- #masthead -->
-  	<div id="content" class="site-content">
-  	  <h1>🤔&nbsp;404 Error</h1>
-  	  <h6>Unable to locate requested page.</h6>
-  	</div><!-- .post-content-wrapper -->
-  </div>
+    <div id="page" class="pl-4">
+        <a class="skip-link screen-reader-text" href="#content">
+            <?php esc_html_e( 'Skip to content', 'xidipity' ); ?>
+        </a>
+        <header id="masthead" class="site-header">
+            <div class="entry-header-wrapper">
+                <p class="site-title">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <?php bloginfo( 'name' ); ?>
+                    </a>
+                </p>
+                <p class="site-description mb-4">
+                    <?php bloginfo( 'description' ); ?>
+                </p>
+            </div>
+        </header>
+        <div id="content" class="site-content">
+            <h1><i class="fas fa-search-minus fg-pri-300 pr-4"></i><?php _e( '404 Error', 'xidipity' ); ?></h1>
+            <h5 class="fg-bas-700"><?php _e( 'Unable to locate requested page.', 'xidipity' ); ?></h5>
+        </div>
+    </div>
 </body>
