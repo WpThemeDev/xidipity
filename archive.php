906b6df7a02e5504d14b4fd65f459330c1a0a02e
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-03-21 15:48:37 -0400

    archive.php - 90321.1 update
    Update format.

--- archive.php
+++ archive.php
@@ -1,61 +1,65 @@
 <?php
-/**
- * The template for displaying category pages.
+/*
+ *        file: archive.php
+ *       build: 90321.1
+ * description: Template for displaying posts assigned to category.
+ *      github: https://github.com/WpThemeDev/xidipity
+ *    comments:
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * @package WordPress
+ * @subpackage Xidipity
+ * @since 5.0.0
  *
- * @package xidipity
- */
+ ***/ ?>
+<?php get_header(); ?>
 
-get_header(); ?>
+<div class="content-area-container">
+  <section id="primary" class="content-area <?php xidipity_layout_class( 'content' ); ?>">
+    <main id="main" class="site-main">
 
-  <div class="content-area-container">
-    <section id="primary" class="content-area <?php xidipity_layout_class( 'content' ); ?>">
-      <main id="main" class="site-main">
+    <?php if ( have_posts() ) : ?>
 
-      <?php if ( have_posts() ) : ?>
+      <header class="page-header">
+        <?php
+          the_archive_title( '<h1 class="page-title">', '</h1>' );
+          the_archive_description( '<div class="taxonomy-description">', '</div>' );
+        ?>
+      </header><!-- .page-header -->
 
-        <header class="page-header">
-          <?php
-            the_archive_title( '<h1 class="page-title">', '</h1>' );
-            the_archive_description( '<div class="taxonomy-description">', '</div>' );
-          ?>
-        </header><!-- .page-header -->
-
-        <div id="post-wrapper" class="post-wrapper post-wrapper-archive">
-        <?php /* Start the Loop */ ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-
-          <?php
-            /* Include the Post-Format-specific template for the content.
-             * If you want to override this in a child theme, then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
-            get_template_part( 'template-parts/content', get_post_format() );
-          ?>
-
-        <?php endwhile; ?>
-        </div><!-- .post-wrapper -->
+      <div id="post-wrapper" class="post-wrapper post-wrapper-archive">
+      <?php /* Start the Loop */ ?>
+      <?php while ( have_posts() ) : the_post(); ?>
 
         <?php
-        $ppp = get_option( 'posts_per_page' ); // posts per page
-        $cnt_posts = wp_count_posts();
-        $tot_posts = $cnt_posts->publish;
-        $max_pg = $tot_posts / $ppp;
+          /* Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part( 'template-parts/content', get_post_format() );
+        ?>
 
-        xidipity_the_posts_pagination( $max_pg ); ?>
+      <?php endwhile; ?>
+      </div><!-- .post-wrapper -->
 
-      <?php else : ?>
+      <?php
+      $ppp = get_option( 'posts_per_page' ); // posts per page
+      $cnt_posts = wp_count_posts();
+      $tot_posts = $cnt_posts->publish;
+      $max_pg = $tot_posts / $ppp;
 
-      <?php get_template_part( 'template-parts/content', 'none' ); ?>
+      xidipity_the_posts_pagination( $max_pg ); ?>
 
-      <?php endif; ?>
+    <?php else : ?>
 
-      </main><!-- #main -->
-    </section><!-- #primary -->
+    <?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-    <?php get_sidebar(); ?>
+    <?php endif; ?>
 
-  </div><!-- .container -->
+    </main><!-- #main -->
+  </section><!-- #primary -->
+
+  <?php get_sidebar(); ?>
+
+</div><!-- .container -->
 
 <?php get_footer(); ?>
