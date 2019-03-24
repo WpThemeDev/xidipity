9b4c583252a4c8c8e4b657a091870efeef34fe32
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-03-23 21:00:28 -0400

    404.php - 90323.1 update
    Update to PHP syntax.

--- 404.php
+++ 404.php
@@ -1,45 +1,51 @@
 <?php
 /*
- *        file: 404.php
- *       build: 90321.1
- * description: Template for displaying 404 image.
- *      github: https://github.com/WpThemeDev/xidipity
- *    comments:
- *
- * @package WordPress
- * @subpackage Xidipity
- * @since 5.0.0
- *
- ***/ ?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset=" <?php bloginfo( 'charset' ); ?> ">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-    <div id="page" class="pl-4">
-        <a class="skip-link screen-reader-text" href="#content">
-            <?php esc_html_e( 'Skip to content', 'xidipity' ); ?>
-        </a>
-        <header id="masthead" class="site-header">
-            <div class="entry-header-wrapper">
-                <p class="site-title">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <?php bloginfo( 'name' ); ?>
-                    </a>
-                </p>
-                <p class="site-description mb-4">
-                    <?php bloginfo( 'description' ); ?>
-                </p>
-            </div>
-        </header>
-        <div id="content" class="site-content">
-            <h1><i class="fas fa-search-minus fg-pri-300 pr-4"></i><?php _e( '404 Error', 'xidipity' ); ?></h1>
-            <h5 class="fg-bas-700"><?php _e( 'Unable to locate requested page.', 'xidipity' ); ?></h5>
-        </div>
-    </div>
-</body>
+*        file: 404.php
+*       build: 90323.1
+* description: Template for displaying 404 image.
+*      github: https://github.com/WpThemeDev/xidipity
+*    comments:
+*
+* @package WordPress
+* @subpackage Xidipity
+* @since 5.0.0
+*
+***/
+echo '<!doctype html>' . "\n";
+?>
+<html <?php
+language_attributes(); ?> >
+<?php
+echo '<head>' . "\n";
+echo '<meta charset="' . get_bloginfo('charset') . '">' . "\n";
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n";
+echo '<link rel="profile" href="http://gmpg.org/xfn/11">' . "\n";
+/* load wordpress meta     ------------
+-- */
+do_action('wp_head');
+echo '</head>' . "\n";
+echo '<body class="' . implode(' ', get_body_class()) . '">' . "\n";
+echo '<div id="page" class="pl-4">' . "\n";
+echo '<a class="skip-link screen-reader-text" href="#content">' . __('Skip to content') . '</a>' . "\n";
+echo '</a>' . "\n";
+echo '<header id="masthead" class="site-header">' . "\n";
+echo '<div class="entry-header-wrapper bg-bas-050">' . "\n";
+echo '<p class="site-title">' . "\n";
+echo '<a href="' . esc_url(home_url('/')) . '" rel="home">' . "\n";
+echo get_bloginfo('name') . "\n";
+echo '</a>' . "\n";
+echo '</p>' . "\n";
+echo '<p class="site-description mb-4">' . "\n";
+echo get_bloginfo('description') . "\n";
+echo '</p>' . "\n";
+echo '</div>' . "\n";
+echo '</header>' . "\n";
+echo '<h1><i class="fas fa-search-minus fg-pri-300 pr-4"></i>' . __('404 Error', 'xidipity') . '</h1>' . "\n";
+echo '<h5 class="fg-bas-700">' . __('Unable to locate requested page.', 'xidipity') . '</h5>' . "\n";
+echo '</div>' . "\n";
+echo '</div>' . "\n";
+echo '</body>' . "\n";
+/*  # eof
+    404.php
+-------------------------------------*/
+?>
