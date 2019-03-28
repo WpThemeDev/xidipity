b2b8c95c9f4d7219186da08be26670f173fe1f43
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-03-28 13:37:20 -0400

    content-none.php - 90328.1 update
    Update to PHP syntax to K&R style, 4 space indentation.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,33 +1,39 @@
 <?php
-/**
- * The template part for displaying a message that posts cannot be found.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package xidipity
- */
+/*
+*        file: content-none.php
+*       build: 90325.1
+* description: The template for displaying search forms.
+*      github: https://github.com/WpThemeDev/xidipity
+*    comments:
+*
+* @package WordPress
+* @subpackage Xidipity
+* @since 5.0.0
+*
+***/
+echo '<section class="no-results not-found">' . "\n";
+echo '<header class="page-header">' . "\n";
+echo '<h1 class="page-title"><i class="fas fa-search-minus fg-pri-300 pr-4"></i>Search Results</h1>' . "\n";
+echo '</header>' . "\n";
+echo '<div class="page-content">' . "\n";
+
+if (is_home() && current_user_can('publish_posts')) {
+    echo '<p>' . printf(wp_kses(__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'xidipity') , array(
+        'a' => array(
+        'href' => array()
+        )
+    )) , esc_url(admin_url('post-new.php'))) . '</p>' . "\n";
+}
+elseif (is_search()) {
+    echo '<p>' . esc_html_e('Sorry, but nothing matched your search terms. Please try again with different keywords.', 'xidipity') . '</p>' . "\n";
+}
+else {
+    echo '<p>' . esc_html_e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'xidipity') . '</p>' . "\n";
+}
+
+echo '</div>' . "\n";
+echo '</section>' . "\n";
+/*  # eof
+content-none.php
+-------------------------------------*/
 ?>
-
-<section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'xidipity' ); ?></h1>
-	</header><!-- .page-header -->
-
-	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'xidipity' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'xidipity' ); ?></p>
-			<p><?php get_search_form(); ?></p>
-
-		<?php else : ?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'xidipity' ); ?></p>
-			<p><?php get_search_form(); ?></p>
-
-		<?php endif; ?>
-	</div><!-- .page-content -->
-</section>
