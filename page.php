24c297c4c062520c9f277a1dce4be4bdea660427
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-03-28 14:13:42 -0400

    page.php - 90328.1 update
    Update to PHP syntax to K&R style, 4 space indentation.

--- page.php
+++ page.php
@@ -1,40 +1,52 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package xidipity
- */
+/*
+*        file: page.php
+*       build: 90324.1
+* description: This is the template that displays all pages by default.
+*      github: https://github.com/WpThemeDev/xidipity
+*    comments:
+*
+* @package WordPress
+* @subpackage Xidipity
+* @since 5.0.0
+*
+***/
+/* display page header     ------------
+-- */
+get_header();
+echo '<div class="content-area-container">' . "\n";
+echo '<div id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
+echo '<main id="main" class="site-main">' . "\n";
+echo '<div id="post-wrapper" class="post-wrapper post-wrapper-single">' . "\n";
+/* run database query      ------------
+-- */
+if ($wp_query->have_posts()) {
+    while ($wp_query->have_posts()) {
+        the_post();
+        get_template_part('template-parts/content', 'page');
 
-get_header(); ?>
+        // If comments are open or we have at least one comment, load up the comment template
 
-    <div class="content-area-container">
-			<div id="primary" class="content-area <?php xidipity_layout_class( 'content' ); ?>">
-				<main id="main" class="site-main">
-					<div id="post-wrapper" class="post-wrapper post-wrapper-single">
-          <?php
-          //  $wp_query is global
-				  if ( $wp_query->have_posts() ) {
-					  while ( $wp_query->have_posts() ) : the_post();
-						  get_template_part( 'template-parts/content', 'page' );
-							// If comments are open or we have at least one comment, load up the comment template
-							if ( comments_open() || '0' != get_comments_number() ) :
-								comments_template();
-							endif;
-					  endwhile; // end of the loop.
-				  }
-					?>
-					</div><!-- .post-wrapper -->
-				</main><!-- #main -->
-			</div><!-- #primary -->
-			<?php get_sidebar(); ?>
-		</div><!-- # column wrapper -->
+        if (comments_open() || '0' != get_comments_number()) {
+            comments_template();
+        }
+    }
+}
 
-<?php
-// Restore original Post Data
+echo '</div>' . "\n";
+echo '</main>' . "\n";
+echo '</div>' . "\n";
+/* display sidebar         ------------
+-- */
+get_sidebar();
+echo '</div>' . "\n";
+/* reset post data         ------------
+-- */
 wp_reset_postdata();
-get_footer(); ?>
+/* display footer          ------------
+-- */
+get_footer();
+/*  # eof
+page.php
+-------------------------------------*/
+?>
