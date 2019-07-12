35b247523a6c2d2194e29aa731082479ab6c1bd3
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-07-12 17:08:37 -0400

    content-page.php - 90712.1 build
    Post & Page.php and dependencies system upgrade

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *        file: content-page.php
-*       build: 90629.1
+*       build: 90712.1
 * description: The template used for displaying page content in page.php
 *      github: https://github.com/WpThemeDev/xidipity
 *    comments:
@@ -11,15 +11,18 @@
 * @since 5.0.0
 *
 ***/
-echo '<!-- xwpt:90629.1/content-page.php    -->' . "\n";
-echo '<article id="post-' . get_the_ID() . '" class="' . implode(' ', get_post_class()) . '">' . "\n";
-echo '<div class="post-content-wrapper post-content-wrapper-single">' . "\n";
-echo '<div class="entry-data-wrapper entry-data-wrapper-single">' . "\n";
-echo '<div class="entry-header-wrapper">' . "\n";
+echo '<!-- xwpt:90712.1/content-page.php    -->' . "\n";
+//echo '<article id="post-' . get_the_ID() . '" class="' . implode(' ', get_post_class()) . '">' . "\n";
+echo '<article id="xwtFxRowFullItem" class="xwtAddShadow ' . implode(' ', get_post_class()) . '">' . "\n";
+//echo '<div class="post-content-wrapper post-content-wrapper-single">' . "\n";
+echo '<div class="xwtAddPadPage post-content-wrapper post-content-wrapper-single">' . "\n";
+
+//echo '<div class="entry-data-wrapper entry-data-wrapper-single">' . "\n";
+//echo '<div class="entry-header-wrapper">' . "\n";
 echo '<header class="entry-header">' . "\n";
 the_title('<h1 class="entry-title">', '</h1>');
 echo '</header>' . "\n";
-echo '</div>' . "\n";
+//echo '</div>' . "\n";
 echo '<div class="entry-content">' . "\n";
 
 /* yoast breadcrumbs       ------------
@@ -43,15 +46,15 @@ wp_link_pages(array(
 echo '</div>' . "\n";
 
 if ('' != get_edit_post_link()) {
-    echo '<footer class="entry-meta entry-meta-footer">' . "\n";
+    echo '<footer class="xwtContentFoot">' . "\n";
     xidipity_entry_footer();
     echo '</footer>' . "\n";
 }
 
-echo '</div>' . "\n";
+//echo '</div>' . "\n";
 echo '</div>' . "\n";
 echo '</article>' . "\n";
-echo '<!-- /xwpt:90629.1/content-page.php   -->' . "\n";
+echo '<!-- /xwpt:90712.1/content-page.php   -->' . "\n";
 /*  # eof
 content-page.php
 -------------------------------------*/
