a5e4454b98979960dfb157ce86149b658fe3f5c2
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-07-11 20:32:09 -0400

    sidebar.php - 90708.1 build
    Index.php and dependencies system upgrade

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *        file: sidebar.php
-*       build: 90325.1
+*       build: 90708.1
 * description: The Sidebar containing the main widget areas.
 *      github: https://github.com/WpThemeDev/xidipity
 *    comments:
@@ -14,13 +14,14 @@
 if ( ! xidipity_has_sidebar() ) {
   return;
 }
-echo '<!-- xwpt:90325.1/sidebar.php         -->' . "\n";
-echo '<div id="site-sidebar" class="' . xidipity_layout_class('sidebar') . '">' . "\n";
-echo '<div id="secondary">' . "\n";
+echo '<!-- xwpt:90708.1/sidebar.php         -->' . "\n";
+echo '<div id="xwtFxRowItem" class="_' . xidipity_layout_class('sidebar') . '">' . "\n";
+echo '<div id="xwtFxColItems">' . "\n";
+// see functions.php (~131)
 dynamic_sidebar( 'sidebar-1' );
 echo '</div>' . "\n";
 echo '</div>' . "\n";
-echo '<!-- /xwpt:90325.1/sidebar.php        -->' . "\n";
+echo '<!-- /xwpt:90708.1/sidebar.php        -->' . "\n";
 /*  # eof
 sidebar.php
 -------------------------------------*/
