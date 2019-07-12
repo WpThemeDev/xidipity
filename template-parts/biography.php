153b829c6c58f8dfdb83b5031a43ff6842bf165b
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-07-12 17:10:20 -0400

    biography.php - 90712.1 build
    Post & Page.php and dependencies system upgrade

--- template-parts/biography.php
+++ template-parts/biography.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *        file: biography.php
-*       build: 90327.1
+*       build: 90712.1
 * description: The template part for displaying an Author biography
 *      github: https://github.com/WpThemeDev/xidipity
 *    comments:
@@ -12,28 +12,34 @@
 * @since 5.0.0
 *
 ***/
-echo '<!-- xwpt:90327.1/biography.php       -->' . "\n";
-echo '<div class="post-navigation">' . "\n";
-echo '<div class="content-area-container">' . "\n";
-echo '<div id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
+echo '<!-- xwpt:90712.1/biography.php       -->' . "\n";
+//echo '<div class="post-navigation">' . "\n";
+echo '<div id="xwtFxRowFullItem" class="xwtAddShadow">' . "\n";
+//echo '<div class="content-area-container">' . "\n";
+echo '<div class="xwtAddPadPost">' . "\n";
+//echo '<div id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
 echo '<main id="main" class="site-main">' . "\n";
-echo '<div id="post-wrapper" class="post-wrapper post-wrapper-single">' . "\n";
-echo '<p>&nbsp;</p>' . "\n";
-echo '<!-- 90224.1 Template: miscellaneous / speciality / header / h4 -->' . "\n";
-echo '<div style="display: table; font-family: var(--font-sans); width: 100%;">' . "\n";
+// '<div id="post-wrapper" class="post-wrapper post-wrapper-single">' . "\n";
+//echo '<p>&nbsp;</p>' . "\n";
 $avatar = esc_url(get_avatar_url(get_the_author_meta('user_email') , 64));
-echo '<div style="background-color: var(--sys-content-bg-color); border-right: solid 3px var(--fg-sec-100); display: table-cell; padding: 8px 10px 0 10px; width: 84px; vertical-align: middle;"><img src="' . $avatar . '" class="rounded-full" alt="avatar" /></div>' . "\n";
-echo '<div style="background-color: var(--sys-content-bg-color); color: var(--fg-bas-900); display: table-cell; padding-left: 15px; width: calc(100% - 84px); vertical-align: top;"><h5>About: ' . get_the_author_meta('nickname') . '</h5><p class="text-sm">' . get_the_author_meta('description') . '</p></div>' . "\n";
+echo '<div class="table w-full">' . "\n";
+echo '<div class="table-row">' . "\n";
+echo '<div class="table-cell text-left pr-3 xwtAvatar">' . "\n";
+echo '<img class="h-auto w-full" src="' . $avatar . '" alt="Xidipity Theme Avatar" />' . "\n";
 echo '</div>' . "\n";
-echo '<!-- End Template -->' . "\n";
-echo '<p>&nbsp;</p>' . "\n";
+echo '<div class="table-cell align-top">' . "\n";
+echo '<h5>About: ' . get_the_author_meta('nickname') . '</h5>' . "\n";
+echo '<p class="text-sm">' . get_the_author_meta('description') . '</p>' . "\n";
 echo '</div>' . "\n";
+echo '</div>' . "\n";
+echo '</div>' . "\n";//echo '<p>&nbsp;</p>' . "\n";
+//echo '</div>' . "\n";
 echo '</main>' . "\n";
+//echo '</div>' . "\n";
 echo '</div>' . "\n";
 echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '<p>&nbsp;</p>' . "\n";
-echo '<!-- /xwpt:90327.1/biography.php      -->' . "\n";
+//echo '<p>&nbsp;</p>' . "\n";
+echo '<!-- /xwpt:90712.1/biography.php      -->' . "\n";
 /*  # eof
 biography.php
 -------------------------------------*/
