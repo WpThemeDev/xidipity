1b46c6b83f8535c7de120a926678813bf4bc6ecc
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-07-13 20:14:00 -0400

    archive.php - 90713.1 build
    Post & Page.php and dependencies system upgrade

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *        file: archive.php
-*       build: 90324.1
+*       build: 90713.1
 * description: Template for displaying posts assigned to category.
 *      github: https://github.com/WpThemeDev/xidipity
 *    comments: This file displays all categories searches.
@@ -14,17 +14,25 @@
 /* display page header     ------------
 -- */
 get_header();
-echo '<!-- xwpt:90324.1/archive.php         -->' . "\n";
-echo '<div class="content-area-container">' . "\n";
-echo '<section id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
-echo '<main id="main" class="site-main">' . "\n";
+echo '<!-- xwpt:90713.1/archive.php         -->' . "\n";
+//echo '<div class="content-area-container">' . "\n";
+//echo '<section id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
+//echo '<main id="main" class="site-main">' . "\n";
+echo '<main id="xwtFxRowItem" class="xwtFxRowItemOpts">' . "\n";
+echo '<div id="xwtFxRowItems" class="xpost-wrapper xpost-wrapper-archive">' . "\n";
 
 if (have_posts()) {
-    echo '<header class="page-header">' . "\n";
-    the_archive_title('<h1 class="page-title">', '</h1>');
+    //echo '<header class="xwtAddShadow xwtAddPadExcerpt">' . "\n";
+    //the_archive_title('<h2 class="xwtRecentTitle">', '</h2>');
+    //the_archive_description('<div class="taxonomy-description">', '</div>');
+    //echo '</header>' . "\n";
+    //echo '<div id="post-wrapper" class="post-wrapper post-wrapper-archive">' . "\n";
+    echo '<div id="xwtFxRowFullItem" class="xwtAddShadow">' . "\n";
+    echo '<header class="xwtAddPadExcerpt">' . "\n";
+    the_archive_title('<h2><i class="far fa-file-alt fg-bas-400 pr-2"></i>', '</h2>');
     the_archive_description('<div class="taxonomy-description">', '</div>');
     echo '</header>' . "\n";
-    echo '<div id="post-wrapper" class="post-wrapper post-wrapper-archive">' . "\n";
+    echo '</div>' . "\n";
     while (have_posts()) {
         the_post();
         /* Include the Post-Format-specific template for the content.
@@ -34,24 +42,20 @@ if (have_posts()) {
         get_template_part('template-parts/content', get_post_format());
     }
 
-    echo '</div>' . "\n";
-    $ppp = get_option('posts_per_page'); // posts per page
-    $cnt_posts = wp_count_posts();
-    $tot_posts = $cnt_posts->publish;
-    $max_pg = $tot_posts / $ppp;
-    xidipity_the_posts_pagination($max_pg);
+    //echo '</div>' . "\n";
+    xidipity_the_posts_pagination($pages_max);
 }
 else {
     get_template_part('template-parts/content', 'none');
 }
 
+echo '</div>' . "\n";
 echo '</main>' . "\n";
-echo '</section>' . "\n";
 /* display sidebar         ------------
 -- */
 get_sidebar();
 echo '</div>' . "\n";
-echo '<!-- /xwpt:90324.1/archive.php        -->' . "\n";
+echo '<!-- /xwpt:90713.1/archive.php        -->' . "\n";
 /* display footer          ------------
 -- */
 get_footer();
