c9e34496783a62001a6eadf7a774cc5d1e6fb4db
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-14 15:17:30 -0400

    page.php - build 90728.1
    Core theme updates

--- page.php
+++ page.php
@@ -1,56 +1,69 @@
 <?php
 /*
-*        file: page.php
-*       build: 90714.1
-* description: This is the template that displays all pages by default.
-*      github: https://github.com/WpThemeDev/xidipity
-*    comments:
-*
-* @package WordPress
-* @subpackage Xidipity
-* @since 5.0.0
-*
-***/
-/* display page header     ------------
--- */
+ *  Xidipity WordPress Theme
+ *
+ *  file:   page.php
+ *  build:  90728.1
+ *  descrp: Page template
+ *  ref:    https://github.com/WpThemeDev/xidipity
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+**/
+/*
+    system variables
+*/
+global $wp_query;
+/* current pagination number */
+$wp_paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+/* posts per page */
+$wp_ppp = get_option('posts_per_page');
+/*
+    local variables
+*/
+$v_cur_page = 0;
+$v_pages = 0;
+/*
+    display header
+*/
 get_header();
-echo '<!-- xwpt:90714.1/page.php            -->' . "\n";
-//echo '<div class="content-area-container">' . "\n";
-//echo '<div id="primary" class="content-area ' . xidipity_layout_class('content') . '">' . "\n";
+echo '<!-- xwpt: 90714.1/page.php            -->' . "\n";
 echo '<main id="xwtFxRowItem" class="xwtFxRowItemOpts">' . "\n";
 echo '<div id="xwtFxRowItems" class="xpost-wrapper xpost-wrapper-archive">' . "\n";
-//echo '<main id="main" class="site-main">' . "\n";
-//echo '<div id="post-wrapper" class="post-wrapper post-wrapper-single">' . "\n";
-/* run database query      ------------
--- */
 if ($wp_query->have_posts()) {
     while ($wp_query->have_posts()) {
         the_post();
         get_template_part('template-parts/content-page', 'page');
-
-        // If comments are open or we have at least one comment, load up the comment template
-
-        if (comments_open() || '0' != get_comments_number()) {
-            comments_template();
+        /*
+            pagination
+        */
+        $v_pages = $wp_query->max_num_pages;
+        if ($v_pages > 1)
+        {
+            $v_cur_page = max(1, get_query_var('paged'));
+            echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
         }
     }
 }
-
 echo '</div>' . "\n";
 echo '</main>' . "\n";
-//echo '</div>' . "\n";
-/* display sidebar         ------------
--- */
+/*
+    display sidebar
+*/
 get_sidebar();
 echo '</div>' . "\n";
-echo '<!-- /xwpt:90714.1/page.php           -->' . "\n";
-/* reset post data         ------------
--- */
+echo '<!-- /xwpt: 90714.1/page.php           -->' . "\n";
+/*
+    reset post data
+*/
 wp_reset_postdata();
-/* display footer          ------------
--- */
+/*
+    display footer
+*/
 get_footer();
-/*  # eof
-page.php
--------------------------------------*/
+/*
+    eof:page.php
+*/
 ?>
