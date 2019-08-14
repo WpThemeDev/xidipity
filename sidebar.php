9445af0f1b403597806bd18bb953dad8ba8d4f82
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-14 15:20:18 -0400

    sidebar.php - build 90728.1
    Core theme updates

--- sidebar.php
+++ sidebar.php
@@ -1,28 +1,31 @@
 <?php
 /*
-*        file: sidebar.php
-*       build: 90708.1
-* description: The Sidebar containing the main widget areas.
-*      github: https://github.com/WpThemeDev/xidipity
-*    comments:
-*
-* @package WordPress
-* @subpackage Xidipity
-* @since 5.0.0
-*
-***/
+ *  Xidipity WordPress Theme
+ *
+ *  file:   sidebar.php
+ *  build:  90728.1
+ *  descrp: sidebar (Flexbox column)
+ *  ref:    https://github.com/WpThemeDev/xidipity
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+**/
 if ( ! xidipity_has_sidebar() ) {
   return;
 }
-echo '<!-- xwpt:90708.1/sidebar.php         -->' . "\n";
+echo '<!-- xwpt: 90728.1/sidebar.php         -->' . "\n";
 echo '<div id="xwtFxRowItem" class="_' . xidipity_layout_class('sidebar') . '">' . "\n";
 echo '<div id="xwtFxColItems">' . "\n";
-// see functions.php (~131)
+/*
+    see functions.php (~131)
+*/
 dynamic_sidebar( 'sidebar-1' );
 echo '</div>' . "\n";
 echo '</div>' . "\n";
-echo '<!-- /xwpt:90708.1/sidebar.php        -->' . "\n";
-/*  # eof
-sidebar.php
--------------------------------------*/
+echo '<!-- /xwpt: 90728.1/sidebar.php        -->' . "\n";
+/*
+    eof: sidebar.php
+*/
 ?>
