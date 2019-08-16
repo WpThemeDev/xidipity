de6abb08028f533e3452b1851ba2ac1f8a39f652
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-16 10:44:54 -0400

    content-page.php - 90728.3
    Update format

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   content-page.php
- *  build:  90728.2
+ *  build:  90728.3
  *  descrp: content / page
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -11,58 +11,71 @@
  *  @subpackage Xidipity
  *  @since 0.9.0
  *
-**/
+ **/
 /*
     local variables
 */
 $v_cat = '';
 $v_meta_list = '';
-
-echo '<!-- xwpt: 90728.1/content-page.php    -->' . "\n";
-echo '<article id="xwtFxRowFullItem" class="xwtAddShadow ' . implode(' ', get_post_class()) . '">' . "\n";
-echo '<div class="xwtAddPadPage post-content-wrapper post-content-wrapper-single">' . "\n";
-/*
-    content title
-*/
-echo '<header id="xwtEntryHeader">' . "\n";
-the_title('<h1 class="xwtEntryTitle">', '</h1>');
-echo '</header>' . "\n";
-echo '<div class="entry-content">' . "\n";
-
 $wp_tmpl = get_option('current_page_template');
-if ($wp_tmpl !== 'misc')
-    {
+if ($wp_tmpl == 'naked')
+{
     /*
-        yoast breadcrumbs
-    */
-    if ( function_exists('yoast_breadcrumb') ) {
-      yoast_breadcrumb( '<p id="breadcrumbs" class="seo-pag-breadcrumbs">','</p>' );
-    }
-}
-/*
     content
     * set min height to keep meta @ bottom
-*/
-echo '<div class="xwtMinY-275">' . "\n";
-the_content();
-echo '</div>' . "\n";
-/*
-    page footer
-*/
-$v_meta_list = '';
-/*: edit :*/
-if (get_edit_post_link())
-{
-    $v_meta_list .= xidipity_icon_edit() . ',';
-    $v_meta_list .= '<a href="' . get_edit_post_link() . '">Edit</a>' . ',';
+    */
+    echo '<div class="xwtMinY-275">' . "\n";
+    the_content();
+    echo '</div>' . "\n";
 }
-/*: date :*/
-$v_meta_list .= xidipity_icon_date() . ',';
-$v_meta_list .= get_the_date() . ',';
-echo xidipity_metalinks(explode(',', $v_meta_list));
-echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '</article>' . "\n";
+else
+{
+    echo '<!-- xwpt: 90728.1/content-page.php    -->' . "\n";
+    echo '<article id="xwtFxRowFullItem" class="xwtAddShadow ' . implode(' ', get_post_class()) . '">' . "\n";
+    echo '<div class="xwtAddPadPage post-content-wrapper post-content-wrapper-single">' . "\n";
+    /*
+    content title
+    */
+    echo '<header id="xwtEntryHeader">' . "\n";
+    the_title('<h1 class="xwtEntryTitle">', '</h1>');
+    echo '</header>' . "\n";
+    echo '<div class="entry-content">' . "\n";
+    $wp_tmpl = get_option('current_page_template');
+    if ($wp_tmpl !== 'misc')
+    {
+        /*
+        yoast breadcrumbs
+        */
+        if (function_exists('yoast_breadcrumb'))
+        {
+            yoast_breadcrumb('<p id="breadcrumbs" class="seo-pag-breadcrumbs">', '</p>');
+        }
+    }
+    /*
+    content
+    * set min height to keep meta @ bottom
+    */
+    echo '<div class="xwtMinY-275">' . "\n";
+    the_content();
+    echo '</div>' . "\n";
+    /*
+    page footer
+    */
+    $v_meta_list = '';
+    /*: edit :*/
+    if (get_edit_post_link())
+    {
+        $v_meta_list .= xidipity_icon_edit() . ',';
+        $v_meta_list .= '<a href="' . get_edit_post_link() . '">Edit</a>' . ',';
+    }
+    /*: date :*/
+    $v_meta_list .= xidipity_icon_date() . ',';
+    $v_meta_list .= get_the_date() . ',';
+    echo xidipity_metalinks(explode(',', $v_meta_list));
+    echo '</div>' . "\n";
+    echo '</div>' . "\n";
+    echo '</article>' . "\n";
+}
 echo '<!-- /xwpt: 90728.1/content-page.php   -->' . "\n";
 /*
     eof:content-page.php
