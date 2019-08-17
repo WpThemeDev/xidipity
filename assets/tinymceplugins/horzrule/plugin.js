fda383f519d1507d6e4881a64c13ddcc9b58b457
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-17 19:36:35 -0400

    plugin.js - build 90817.1 build
    Core features update

--- assets/tinymceplugins/horzrule/plugin.js
+++ assets/tinymceplugins/horzrule/plugin.js
@@ -1,12 +1,16 @@
-/**
- * Plugin Name: TinyMCE Horizonal Rule Plugin
- * Plugin URI: http:www.xidipity.com
- * Build: 90624.1
- * Author: John Baer
- * Description: A TinyMCE Plugin to add horizonal rules to the MCE Visual Editor
- * License: GPL2
- */
-
+/*
+ *  Xidipity WordPress Theme
+ *
+ *  file:   plugin.js
+ *  build:  90817.1
+ *  descrp: horizonal rule plugin
+ *  ref:    https://www.tiny.cloud/
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+**/
 (function() {
     tinymce.PluginManager.add('hrule', function(editor, url) {
         editor.addButton('hrule', {
@@ -20,7 +24,7 @@
                     text: '• Single Wide',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<hr style="width:100%" />');
+                        editor.insertContent('<hr class="w-100%" />');
                     }
                 },
                 {
@@ -34,7 +38,7 @@
                     text: '• Double Wide',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<hr class="double" style="width:100%" />');
+                        editor.insertContent('<hr class="double w-100%" />');
                     }
                 },
                 {
@@ -48,21 +52,21 @@
                     text: '• Gradient Wide',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<hr class="gradient" style="width:100%" />');
+                        editor.insertContent('<hr class="gradient w-100%" />');
                     }
                 },
                 {
                     text: '• Emblem',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr /></div><div class="xhr-0x-chld2 text-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
+                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr /></div><div class="xhr-0x-chld2 txt-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
                     }
                 },
                 {
                     text: '• Emblem Wide',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr class="w-full" /></div><div class="xhr-0x-chld2 text-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
+                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr class="w-100%" /></div><div class="xhr-0x-chld2 txt-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
                     }
                 }
             ],
