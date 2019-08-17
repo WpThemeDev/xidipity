ab3534f95fabc16395de154ebdf1d91db9ca78d7
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-17 19:25:09 -0400

    plugin.js - build 90817.1 build
    Core features update

--- assets/tinymceplugins/excerpt/plugin.js
+++ assets/tinymceplugins/excerpt/plugin.js
@@ -1,12 +1,16 @@
-/**
- * Plugin Name: TinyMCE Excerpt Plugin
- * Plugin URI: http:www.xidipity.com
- * Build: 90115.2
- * Author: John Baer
- * Description: A TinyMCE Plugin to add excerpt template to the MCE Visual Editor
- * License: GPL2
- */
-
+/*
+ *  Xidipity WordPress Theme
+ *
+ *  file:   plugin.js
+ *  build:  90817.1
+ *  descrp: excerpt plugin
+ *  ref:    https://www.tiny.cloud/
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+**/
 (function() {
     tinymce.PluginManager.add('excerpt', function( editor, url ) {
         editor.addButton( 'excerpt', {
@@ -14,10 +18,7 @@
             icon: 'mce-excerpt',
             image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0%0D%0APSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0%0D%0ASDBWMHoiLz48cGF0aCBkPSJNMjAgMkg0Yy0xLjEgMC0yIC45LTIgMnYxOGw0LTRoMTRjMS4xIDAg%0D%0AMi0uOSAyLTJWNGMwLTEuMS0uOS0yLTItMnptMCAxNEg2bC0yIDJWNGgxNnYxMnoiLz48L3N2Zz4=',
             onclick: function() {
-              var html = '<!-- Template: excerpt -->' + String.fromCharCode(13) +
-              '<p class="excerpt-template">Place excerpt here ...<span class="hidden"><!--more--></span></p>' + String.fromCharCode(13) +
-              '<!-- End Template -->' + String.fromCharCode(13) +
-              '<p>&nbsp;</p>';
+              var html = '<p><!-- mce: embedded/excerpt --></p><div id="excerpt"><div class="excerpt-tr"><div class="excerpt-td">Place excerpt here ...<span class="excerpt-wp"><!--more--></span></div></div></div><p><!-- /mce: embedded/excerpt --></p>';
               editor.insertContent(html);
             }
         });
