96179c85f594f3ec4a2e1666270449fdfbd42ae2
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-18 16:14:35 -0400

    plugin.js - 90818.1 build
    Core features update

--- assets/tinymceplugins/horzrule/plugin.js
+++ assets/tinymceplugins/horzrule/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   plugin.js
- *  build:  90817.1
+ *  build:  90818.1
  *  descrp: horizonal rule plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -59,14 +59,14 @@
                     text: '• Emblem',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr /></div><div class="xhr-0x-chld2 txt-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
+                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr /></div><div class="xhr-0x-chld2 txt-center"><span class="px-1 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
                     }
                 },
                 {
                     text: '• Emblem Wide',
                     icon: false,
                     onclick: function() {
-                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr class="w-100%" /></div><div class="xhr-0x-chld2 txt-center"><span class="px-4 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
+                        editor.insertContent('<div class="xhr-0x"><div class="xhr-0x-chld1"><hr class="w-100%" /></div><div class="xhr-0x-chld2 txt-center"><span class="px-1 sys:bg-content"><i class="fas fa-square fg-bas-400">&#x200B;</i></span></div></div>');
                     }
                 }
             ],
