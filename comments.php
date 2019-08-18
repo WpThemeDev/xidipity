72cef37b48501e801319849c8799807f81c99599
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-18 16:06:42 -0400

    comments.php - 90818.1 build
    Core features update

--- comments.php
+++ comments.php
@@ -1,37 +1,43 @@
 <?php
 /*
-*        file: comments.php
-*       build: 90713.1
-* description: Template for displaying posts assigned to category.
-*      github: https://github.com/WpThemeDev/xidipity
-*    comments: Login required.
-*
-* @package WordPress
-* @subpackage Xidipity
-* @since 5.0.0
-*
-***/
-
+ *  Xidipity WordPress Theme
+ *
+ *  file:   comments.php
+ *  build:  90818.1
+ *  descrp: comment template
+ *  ref:    https://github.com/WpThemeDev/xidipity
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+**/
+/*
+    login required
+*/
 if (post_password_required()) {
     return;
 }
 
-echo '<!-- xwpt:90713.1/comments.php        -->' . "\n";
-//echo '<div id="xcomments" class="w-full xcomments-area">' . "\n";
+echo '<!-- xwpt: 90818.1/comments        -->' . "\n";
 echo '<div id="xwtFxRowFullItem" class="xwtAddShadow">' . "\n";
 
 if (have_comments()):
-    echo '<div class="xwtAddPadPost xcomments-area-wrapper">' . "\n";
+    echo '<div class="xwtAddPadPost">' . "\n";
     echo '<h2 class="comments-title">' . "\n";
     $comments_number = get_comments_number();
     if ('1' === $comments_number) {
+        /*
+            translators: %s: post title
+        */
         printf(
-        /* translators: %s: post title */
         esc_html(_x('One comment to &ldquo;%s&rdquo;', 'comments title', 'xidipity')) , get_the_title());
     }
     else {
+        /*
+            translators: 1: number of comments, 2: post title
+        */
         printf(
-        /* translators: 1: number of comments, 2: post title */
         esc_html(_nx('%1$s Comment to &ldquo;%2$s&rdquo;', '%1$s Comments to &ldquo;%2$s&rdquo;', $comments_number, 'comments title', 'xidipity')) , number_format_i18n($comments_number) , get_the_title());
     }
 
@@ -45,8 +51,9 @@ if (have_comments()):
         echo '</div>' . "\n";
         echo '</nav>' . "\n";
     endif;
-    /* comment ordered list    ------------
-    -- */
+    /*
+        comment ordered list
+    */
     echo '<ol class="comment-list">' . "\n";
     wp_list_comments(array(
         'style' => 'ol',
@@ -54,8 +61,9 @@ if (have_comments()):
         'avatar_size' => 68,
     ));
     echo '</ol>' . "\n";
-    /* comments                ------------
-    -- */
+    /*
+        comments
+    */
     if (get_comment_pages_count() > 1 && get_option('page_comments')):
         echo '<nav id="comment-nav-below" class="navigation comment-navigation comment-navigation-below" role="navigation">' . "\n";
         echo '<h1 class="screen-reader-text">' . esc_html_e('Comment navigation', 'xidipity') . '</h1>' . "\n";
@@ -67,26 +75,26 @@ if (have_comments()):
     endif;
     echo '</div>' . "\n";
 endif;
-/* closed comments         ------------
--- */
-
+/*
+    closed comments
+*/
 if (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type() , 'comments')):
     echo '<div class="xwtNoComment">' . "\n";
-    //echo '<div class="no-comments-wrapper">' . "\n";
-    echo '<p>' . __('<i class="far fa-comment-alt fg-pri-400 pr-2"></i>Comments are closed.', 'xidipity') . '</p>' . "\n";
+    echo '<p>' . __('<i class="far fa-comment-alt fg-pri-400 pr-0.5"></i>Comments are closed.', 'xidipity') . '</p>' . "\n";
     echo '</div>' . "\n";
 endif;
-/* display comment form    ------------
--- */
+/*
+    display comment form
+*/
 echo '<div class="xwtAddPadPost">' . "\n";
 comment_form(array(
-    'title_reply'=>'<i class="far fa-comments fg-pri-300 pr-2"></i>Got Something To Say?',
+    'title_reply'=>'<i class="far fa-comments fg-pri-300 pr-0.5"></i>Got Something To Say?',
     'logged_in_as' =>''
     ) );
 echo '</div>' . "\n";
 echo '</div>' . "\n";
-echo '<!-- /xwpt:90713.1/comments.php       -->' . "\n";
-/*  # eof
-    comments.php
--------------------------------------*/
+echo '<!-- /xwpt: 90818.1/comments       -->' . "\n";
+/*
+    eof:comments.php
+*/
 ?>
