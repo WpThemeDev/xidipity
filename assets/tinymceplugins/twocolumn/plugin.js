2b6e205f96a1a8a258ad1b6e2e99e5283ff83470
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-08-27 10:30:12 -0400

    plugin.js - 90824.1b build
    Core features update

--- assets/tinymceplugins/twocolumn/plugin.js
+++ assets/tinymceplugins/twocolumn/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   plugin.js
- *  build:  90817.1
+ *  build:  90824.1b
  *  descrp: two column plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -18,9 +18,12 @@
             icon: 'mce-column',
             image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB3aWR0aD0iMjQiIGhl%0D%0AaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIw%0D%0AMDAvc3ZnIj4KICA8cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDBWMHoiLz4KICA8cGF0%0D%0AaCBkPSJNIDMgMTUgTCAxMSAxNSBMIDExIDEzIEwgMyAxMyBMIDMgMTUgWiBNIDMgMTkgTCAxMSAx%0D%0AOSBMIDExIDE3IEwgMyAxNyBMIDMgMTkgWiBNIDMgMTEgTCAxMSAxMSBMIDExIDkgTCAzIDkgTCAz%0D%0AIDExIFogTSAzIDUgTCAzIDcgTCAxMSA3IEwgMTEgNSBMIDMgNSBaIE0gMTMgNSBMIDIxIDUgTCAy%0D%0AMSAxOSBMIDEzIDE5IEwgMTMgNSBaIiBzdHlsZT0iZmlsbDogcmdiKDMzLCAzMywgMzMpOyIvPgog%0D%0AIDxyZWN0IHg9IjEyLjk1IiB5PSI0Ljk5OCIgd2lkdGg9IjguMDQyIiBoZWlnaHQ9IjE0LjAwMyIg%0D%0Ac3R5bGU9ImZpbGw6IHJnYigxMTcsIDExNywgMTE3KTsiLz4KPC9zdmc+',
             onclick: function() {
-                var html = '<p><!-- mce: embedded/table/two column --></p><table id="twocol" class="twocolumn"><tbody><tr><td></td><td></td></tr></tbody></table><p><!-- /mce: embedded/table/two column --></p>';
+                var html = '<p><!-- xwt: table/two column --></p><table class="twocol" cellspacing="0"><tr><td class="twocol-td"><p>&nbsp;</p></td><td class="twocol-td"><p>&nbsp;</p></td></tr></table><p><!-- /xwt: table/two column --></p>';
                 editor.insertContent(html);
             }
         });
     });
 })();
+/*
+    eof: twocolumn/plugin.js
+*/
