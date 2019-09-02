550549d321b1244b62c3c505295855615029edbd
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-02 11:53:58 -0400

    footer.php - 90828.1a build
    Updated flexbox integration

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /*
 *        file: footer.php
-*       build: 90713.1
+*       build: 90828.1a
 * description: The template for displaying the footer
 *      github: https://github.com/WpThemeDev/xidipity
 *    comments:
@@ -11,25 +11,25 @@
 * @since 5.0.0
 *
 ***/
-echo '<!-- xwpt:90713.1/footer.php          -->' . "\n";
-echo '<footer id="xwtBkFoot" class="xwtBkFootOpts">' . "\n";
-//echo '<div class="xwtBkFootInfo">' . "\n";
-// echo '<div class="site-info-inside">' . "\n";
-// echo '<div class="credits">' . "\n";
+echo '<!-- xwpt: 90828.1a/footer/php              -->' . "\n";
+$wp_tmpl = get_option('current_page_template');
+if (abs(strpos('#,naked,no-sidebar', $wp_tmpl)) > 0)
+{
+    echo '<footer class="fx:nspg-footer fnt:size-smaller mar:vert-0.75 txt:center">' . "\n";
+}
+else
+{
+    echo '<footer class="fx:pg-item fnt:size-smaller mar:vert-0.75 txt:center">' . "\n";
+}
 do_action( 'xidipity_credits' );
-// echo '</div>' . "\n";
-// echo '</div>' . "\n";
-//echo '</div>' . "\n";
 echo '</footer>' . "\n";
-echo '<!-- /xwpt:90713.1/footer.php         -->' . "\n";
-// closing div from header
+echo '<!-- /xwpt: 90828.1a/footer/php             -->' . "\n";
 echo '</div>' . "\n";
-// echo '</div>' . "\n";
+/*: wordpress code :*/
 wp_footer();
 echo '</body>' . "\n";
 echo '</html>' . "\n";
-
-/*  # eof
-footer.php
--------------------------------------*/
+/*
+    eof: footer.php
+*/
 ?>
