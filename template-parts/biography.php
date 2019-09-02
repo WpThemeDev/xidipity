898562c22364da3ba353cf0506da9dc642796f8a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-02 12:19:35 -0400

    biography.php - 90828.1a build
    Updated flexbox integration

--- template-parts/biography.php
+++ template-parts/biography.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   biography.php
- *  build:  90824.1b
+ *  build:  90828.1a
  *  descrp: biography snippet
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -25,18 +25,16 @@ $wp_permalink = esc_url(get_avatar_url(get_the_author_meta('user_email') , 64));
 $v_biography  = '';
 $v_biography .= '<h5>About: <a href="' . get_author_posts_url( $wp_author_id, $wp_author ) . '">' . $wp_author . '</a></h5>';
 $v_biography .= '<p>' . $wp_bio . '</p>';
-$v_img = '<img src="' . $wp_permalink . '" alt="Xidipity Avatar" />';
+$v_img = '<img class="img:100%" src="' . $wp_permalink . '" alt="Xidipity Avatar" />';
 $v_meta_list = '';
-echo '<!-- xwpt: 90824.1b/biography.php       -->' . "\n";
-echo '<div id="xwtFxRowFullItem" class="xwtAddShadow">' . "\n";
-echo '<div class="xwtAddPadPost">' . "\n";
-echo '<div class="fx:row fx:nowrap fx:opt-000">' . "\n";
-echo '<div class="bio-item">' . $v_img . '</div>';
-echo '<div class="bio-item">' . $v_biography . '</div>';
+echo '<!-- xwpt: 90828.1a/biography/php           -->' . "\n";
+echo '<div class="fx:full-cn-item pad:all-1 fx:shadow">' . "\n";
+echo '<div class="fx:bio-container">' . "\n";
+echo '<div class="fx:bio-item">' . $v_img . '</div>';
+echo '<div class="fx:bio-item pad:left-0.5">' . $v_biography . '</div>';
 echo '</div>' . "\n";
 echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '<!-- /xwpt: 90824.1b/biography.php      -->' . "\n";
+echo '<!-- /xwpt: 90828.1a/biography/php          -->' . "\n";
 /*
     eof: biography.php
 */
