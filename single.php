82b6ab3ff40d6317590bfbd3718e7cc0c10adbf7
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-03 09:14:24 -0400

    single.php - 90901.1a build
    Add error message & update page template functionality

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   single.php
- *  build:  90828.1a
+ *  build:  90901.1a
  *  descrp: Single post template
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -29,48 +29,70 @@ $v_pages = 0;
     display header
 */
 get_header();
-echo '<!-- xwpt: 90828.1a/single/php              -->' . "\n";
+echo '<!-- xwpt: 90901.1a/single/php              -->' . "\n";
 echo '<main class="fx:pg-item">' . "\n";
 if ($wp_query->have_posts()) {
     the_post();
-    echo '<div class="fx:cn-container">' . "\n";
-    get_template_part('template-parts/content', 'post');
-    echo '</div>' . "\n";
-    /*
-        pagination
-    */
-    $v_pages = $wp_query->max_num_pages;
-    if ($v_pages > 1)
+    if (empty(locate_template('template-parts/biography.php')))
     {
-        echo '<!-- xwpt: 90828.1a/single/php/pagination   -->' . "\n";
-        $v_cur_page = max(1, get_query_var('paged'));
-        echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
-        echo '<!-- /xwpt: 90828.1a/single/php/pagination  -->' . "\n";
-    }
-    /*
-        author
-    */
-    if ('' !== get_the_author_meta('description'))
-    {
+        err_msg('missing file "template-parts/content-post.php"');
         echo '<div class="fx:cn-container">' . "\n";
-        get_template_part('template-parts/biography');
+        get_template_part('template-parts/content', 'none');
         echo '</div>' . "\n";
     }
-    /*
-        comments
-    */
-    if (comments_open() || '0' != get_comments_number())
+    else
     {
-        comments_template();
+        echo '<div class="fx:cn-container">' . "\n";
+        get_template_part('template-parts/content', 'post');
+        echo '</div>' . "\n";
+        /*
+            pagination
+        */
+        $v_pages = $wp_query->max_num_pages;
+        if ($v_pages > 1)
+        {
+            echo '<!-- xwpt: 90901.1a/single/php/pagination   -->' . "\n";
+            $v_cur_page = max(1, get_query_var('paged'));
+            echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
+            echo '<!-- /xwpt: 90901.1a/single/php/pagination  -->' . "\n";
+        }
+        /*
+            author
+        */
+        if ('' !== get_the_author_meta('description'))
+        {
+        if (empty(locate_template('template-parts/biography.php')))
+            {
+                err_msg('missing file "template-parts/biography.php"');
+                echo '<div class="fx:cn-container">' . "\n";
+                get_template_part('template-parts/content', 'none');
+                echo '</div>' . "\n";
+            }
+            else
+            {
+                echo '<div class="fx:cn-container">' . "\n";
+                get_template_part('template-parts/biography');
+                echo '</div>' . "\n";
+            }
+        }
+        /*
+            comments
+        */
+        if (comments_open() || '0' != get_comments_number())
+        {
+            comments_template();
+        }
     }
 }
 else
 {
+    err_msg('no available posts');
+    echo '<div class="fx:cn-container">' . "\n";
     get_template_part('template-parts/content', 'none');
     echo '</div>' . "\n";
 }
 echo '</main>' . "\n";
-echo '<!-- /xwpt: 90828.1a/single/php             -->' . "\n";
+echo '<!-- /xwpt: 90901.1a/single/php             -->' . "\n";
 /*
     display sidebar
 */
