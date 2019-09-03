ca9c0a7448a16d86086a686253622897dba1d93d
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-03 09:18:29 -0400

    content-none.php - 90901.1a build
    Add error message & update page template functionality

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   content-none.php
- *  build:  90828.1a
+ *  build:  90901.1a
  *  descrp: content none
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -16,13 +16,13 @@
     system variables
 */
 global $post;
-$wp_title = get_the_title($post);
-echo '<!-- xwpt: 90828.1a/content/none/php        -->' . "\n";
-echo '<article class="fx:full-cn-item fx:shadow">' . "\n";
-echo '<div class="pad:all-1">' . "\n";
+$wp_err = '';
+echo '<!-- xwpt: 90901.1a/content/none/php        -->' . "\n";
+echo '<article class="fx:full-cn-item pad:all-1 fx:shadow">' . "\n";
 /*
     no posts
 */
+echo '<!-- xwpt: 90901.1a/content/none/php/err    -->';
 if (is_home() && current_user_can('publish_posts'))
 {
     printf('<p>' . wp_kses(
@@ -32,10 +32,10 @@ if (is_home() && current_user_can('publish_posts'))
             'href' => array() ,
         ) ,
     )) . '</p>', esc_url(admin_url('post-new.php')));
+    echo '<!-- /xwpt: 90901.1a/content/none/php/err   -->';
 }
 elseif (is_search())
 {
-    echo '<!-- xwpt: 90828.1a/content/none/php/err    -->';
     echo '<div class="fx:an-container">';
     echo '<div class="fx:an-item"><svg width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' . "\n";
     echo '<path fill="none" d="M0 0h24v24H0V0z"/>' . "\n";
@@ -44,11 +44,9 @@ elseif (is_search())
     echo '<div class="fx:an-item"><h2>Search results</h2></div>';
     echo '</div>';
     echo '<p>The search criteria did not return any records.</p>' . "\n";
-    echo '<!-- /xwpt: 90828.1a/content/none/php/err   -->';
 }
 else
 {
-    echo '<!-- xwpt: 90828.1a/content/none/php/err    -->';
     echo '<div class="fx:an-container">';
     echo '<div class="fx:an-item"><svg width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' . "\n";
     echo '<path fill="none" d="M0 0h24v24H0V0z"/>' . "\n";
@@ -56,21 +54,25 @@ else
     echo '</svg></div>';
     echo '<div class="fx:an-item"><h2>Unkown page or post request</h2></div>';
     echo '</div>';
-    echo '<p>This page states the resource requested was not found. The reason may be due to a logic error or some other defect.</p>' . "\n";
-    echo '<!-- /xwpt: 90828.1a/content/none/php/err   -->';
-    if (!empty($wp_title))
+    echo '<p>&nbsp;</p>' . "\n";
+    $wp_err = err_msg();
+    if (empty($wp_err))
+    {
+        echo '<p>The resource requested was not found. The reason may be due to a logic error or some other defect.</p>' . "\n";
+    }
+    else
     {
         echo '<p>&nbsp;</p>' . "\n";
-        echo '<p>The resource in question is titled ' . $wp_title . '.</p>' . "\n";
+        echo '<p>The system reported the following error: <span class="fnt:weight-bolder">' . $wp_err . '</span></p>' . "\n";
     }
     echo '<p>&nbsp;</p>' . "\n";
-    echo '<p>Regardless of the circumstance, drop us a note on <a href="https://github.com/WpThemeDev/xidipity">GitHub</a> or send us an email at support@xidipity.com we will investigate the cause.</p>' . "\n";
+    echo '<p>Regardless of the circumstance, drop us a note on <a href="https://github.com/WpThemeDev/xidipity">GitHub</a> or send us an email at support@xidipity.com and we will investigate the cause.</p>' . "\n";
     echo '<p>&nbsp;</p>' . "\n";
     echo '<p>Thank you</p>' . "\n";
 }
-echo '</div>' . "\n";
+echo '<!-- /xwpt: 90901.1a/content/none/php/err   -->';
 echo '</article>' . "\n";
-echo '<!-- /xwpt: 90828.1a/content/none/php       -->' . "\n";
+echo '<!-- /xwpt: 90901.1a/content/none/php       -->' . "\n";
 /*
     eof: content-none.php
 */
