b34c2f6e1dc0af1b7624406d129dbed7a24abeac
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-02 21:56:20 -0400

    archive.php - 90901.1a build
    Add error message & update page template functionality

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   archive
- *  build:  90828.1a
+ *  build:  90901.1a
  *  descrp: Display archive excerpts
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -31,9 +31,17 @@ $v_cur_page = $wp_paged;
     display header
 */
 get_header();
-echo '<!-- xwpt: 90828.1a/archive/php             -->' . "\n";
+echo '<!-- xwpt: 90901.1a/archive/php             -->' . "\n";
 echo '<main class="fx:pg-item">' . "\n";
-if (have_posts())
+
+if (empty(locate_template('template-parts/content-excerpt.php')))
+{
+    err_msg('missing file content-excerpt.php');
+    echo '<div class="fx:cn-container">' . "\n";
+    get_template_part('template-parts/content', 'none');
+    echo '</div>' . "\n";
+}
+elseif (have_posts())
 {
     /*
         display category banner
@@ -48,7 +56,7 @@ if (have_posts())
     while (have_posts())
     {
         the_post();
-        get_template_part('template-parts/content', get_post_format());
+        get_template_part('template-parts/content', 'excerpt');
     }
     echo '</div>' . "\n";
     /*
@@ -57,10 +65,10 @@ if (have_posts())
     $v_pages = $wp_query->max_num_pages;
     if ($v_pages > 1)
     {
-        echo '<!-- xwpt: 90828.1a/archive/php/pagination  -->' . "\n";
+        echo '<!-- xwpt: 90901.1a/archive/php/pagination  -->' . "\n";
         $v_cur_page = max(1, get_query_var('paged'));
         echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
-        echo '<!-- /xwpt: 90828.1a/archive/php/pagination -->' . "\n";
+        echo '<!-- /xwpt: 90901.1a/archive/php/pagination -->' . "\n";
     }
 }
 else
@@ -74,7 +82,7 @@ echo '</main>' . "\n";
     display sidebar
 */
 get_sidebar();
-echo '<!-- /xwpt: 90828.1a/archive/php            -->' . "\n";
+echo '<!-- /xwpt: 90901.1a/archive/php            -->' . "\n";
 /*
     reset post data
 */
