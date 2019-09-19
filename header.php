1fc6860237775f75c924eed3c84640008147fa98
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-18 20:00:01 -0400

    header.php - 90915.1a build
    Updated flexbox core design

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   header.php
- *  build:  90905.1a
+ *  build:  90915.1a
  *  descrp: Header template
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -17,7 +17,7 @@ echo '<!doctype html>' . "\n";
 <html <?php
 language_attributes(); ?> >
 <?php
-echo '<!-- xwpt: 90905.1a/header/php              -->' . "\n";
+echo '<!-- xwpt: 90915.1a/header/php              -->' . "\n";
 echo '<head>' . "\n";
 echo '<meta charset="' . get_bloginfo('charset') . '">' . "\n";
 echo '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n";
@@ -28,9 +28,11 @@ echo '<link rel="profile" href="http://gmpg.org/xfn/11">' . "\n";
 do_action('wp_head');
 echo '</head>' . "\n";
 echo '<body class="' . implode(' ', get_body_class()) . '">' . "\n";
-echo '<div class="fx:pg-container">' . "\n";
-echo '<!-- xwpt: 90905.1a/header.php           -->' . "\n";
-echo '<header class="fx:pg-item">' . "\n";
+echo '<!-- xwpt: flexbox/page/container           -->' . "\n";
+echo '<div class="fx:pg-ct">' . "\n";
+echo '<!-- xwpt: 90915.1b/header/php              -->' . "\n";
+echo '<!-- xwpt: flexbox/page/container/item-1    -->' . "\n";
+echo '<header class="fx:pg-ct-itm fx:align-ct-itm-1">' . "\n";
 echo '<a class="skip-link screen-reader-text" href="#content">' . __('Skip to content') . '</a>' . "\n";
 echo '<div class="header-banner">' . "\n";
 /*
@@ -61,47 +63,53 @@ if ($description || is_customize_preview())
 }
 echo '</div>' . "\n";
 echo '</header>' . "\n";
-echo '<!-- /xwpt: 90905.1a/header/php             -->' . "\n";
+echo '<!-- /xwpt: 90915.1b/header/php             -->' . "\n";
 
 /*
     menu exceptions
 */
-echo '<!-- xwpt: 90905.1a/header/php/nav          -->' . "\n";
-echo '<nav class="fx:pg-item txt:center">' . "\n";
-if ( $wp_tmpl !== 'naked' )
+echo '<!-- xwpt: 90828.1a/header/php/nav          -->' . "\n";
+echo '<!-- xwpt: flexbox/page/container/item-2    -->' . "\n";
+if ( $wp_tmpl == 'naked' )
 {
-    /*
-        primary menu
-    */
-    $wp_page = get_page_by_path('table-of-contents');
-    $wp_menus = get_nav_menu_locations();
-    $wp_menu = (abs($wp_menus['primary']) !== 0);
-    if ($wp_menu || $wp_page)
+    echo '<nav class="fx:pg-ct-itm disp:none">' . "\n";
+}
+else
+{
+    echo '<nav class="fx:pg-ct-itm fx:align-ct-itm-2">' . "\n";
+}
+
+/*
+    primary menu
+*/
+$wp_page = get_page_by_path('table-of-contents');
+$wp_menus = get_nav_menu_locations();
+$wp_menu = (abs($wp_menus['primary']) !== 0);
+if ($wp_menu || $wp_page)
+{
+    if ($wp_page)
     {
-        if ($wp_page)
+        if ($wp_page->ID == get_queried_object_id())
         {
-            if ($wp_page->ID == get_queried_object_id())
-            {
-                echo '<a class="toc-btn" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
-            }
-            else
-            {
-                echo '<a class="toc-btn" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
-            }
+            echo '<a class="toc-btn" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
         else
         {
-            echo '<input type="checkbox" id="menu-toggle">' . "\n";
-            echo '<label for="menu-toggle"><i class="fas fa-bars"></i></label>' . "\n";
-            wp_nav_menu(array(
-                'theme_location' => 'primary',
-                'menu_id' => 'primary-menu',
-            ));
+            echo '<a class="toc-btn" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
+    }
+    else
+    {
+        echo '<input type="checkbox" id="menu-toggle">' . "\n";
+        echo '<label for="menu-toggle"><i class="fas fa-bars"></i></label>' . "\n";
+        wp_nav_menu(array(
+            'theme_location' => 'primary',
+            'menu_id' => 'primary-menu',
+        ));
     }
 }
 echo '</nav>' . "\n";
-echo '<!-- /xwpt: 90905.1a/header/php/nav         -->' . "\n";
+echo '<!-- /xwpt: 90915.1a/header/php/nav         -->' . "\n";
 /*
     eof: header.php
 */
