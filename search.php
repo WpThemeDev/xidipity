9e4a6d8adaefbf1eff2e4de8b8156f83f2d8d9d6
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 09:34:19 -0400

    search.php - 90915.1b build
    Code consolidation and addition of xidipity.cfg

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   search.php
- *  build:  90915.1a
+ *  build:  90915.1b
  *  descrp: Display archive excerpts
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -12,6 +12,11 @@
  *  @since 0.9.0
  *
  ****/
+/*
+    set page options
+*/
+disp_sidebar('yes');
+disp_menu('yes');
 /*
     system variables
 */
@@ -31,18 +36,10 @@ $v_cur_page = $wp_paged;
     display header
 */
 get_header();
-echo '<!-- xwpt: 90915.1a/search/php              -->' . "\n";
+echo '<!-- xwpt: 90915.1b/search/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-3    -->' . "\n";
 echo '<main class="fx:pg-ct-itm">' . "\n";
-if (empty(locate_template('template-parts/content-excerpt.php')))
-{
-    err_msg('missing file content-excerpt.php');
-    echo '<!-- xwpt: flexbox/content/container        -->' . "\n";
-    echo '<div class="fx:cn-ct">' . "\n";
-    get_template_part('template-parts/content', 'none');
-    echo '</div>' . "\n";
-}
-elseif (have_posts())
+if (have_posts())
 {
     /*
         display excerpt banner
@@ -70,7 +67,65 @@ elseif (have_posts())
     while (have_posts())
     {
         the_post();
-        get_template_part('template-parts/content', 'excerpt');
+        echo '<!-- xwpt: 90915.1b/content/blog/php        -->' . "\n";
+        echo '<!-- xwpt: flexbox/content/container/item   -->' . "\n";
+        echo '<article class="fx:cn-ct-itm fx:cn-ct-opt fx:basis-50% fx:shadow">' . "\n";
+        /*
+            featured image
+        */
+        $wp_img = get_the_post_thumbnail(null, 'FULL', array('class' => 'img:100%'));
+        if ($wp_img)
+        {
+            /*: post thumbnail :*/
+            echo $wp_img;
+            echo '<div class="pad:all-1">' . "\n";
+        }
+        else
+        {
+            echo '<div class="pad:all-1 ht:min-455px">' . "\n";
+        }
+        if ('post' == get_post_type())
+        {
+            if (is_sticky())
+            {
+                $v_icon = xidipity_icon_star();
+            }
+            else
+            {
+                $v_icon = xidipity_icon_cat();
+            }
+            $v_meta_list  = '';
+            $v_meta_list .=  $v_icon . ',';
+            $v_meta_list .=  xidipity_first_category() . ',';
+            echo xidipity_metalinks(explode(',', $v_meta_list));
+        }
+        echo '<header class="fx:cn-itm-hd">' . "\n";
+        the_title('<h1 class="fx:cn-itm-ti"><a href="' . esc_url(apply_filters('xidipity_the_permalink', get_permalink())) . '" rel="bookmark">', '</a></h1>');
+        echo '</header>' . "\n";
+        if ('post' == get_post_type())
+        {
+            $v_meta_list  = '';
+            $v_meta_list .=  xidipity_posted_on() . ',';
+            $v_meta_list .=  xidipity_posted_by() . ',';
+            echo xidipity_metalinks(explode(',', $v_meta_list));
+        }
+        if (xidipity_has_excerpt())
+        {
+            the_excerpt();
+        }
+        /*
+            readmore
+        */
+        $v_link = esc_url(apply_filters('xidipity_the_permalink', get_permalink()));
+        
+        $v_meta_list  = '';
+        $v_meta_list .=  xidipity_icon_rm() . ',';
+        $v_meta_list .=  '<a href="' . $v_link . '">Read more …</a>' . ',';
+        echo xidipity_metalinks(explode(',', $v_meta_list));
+        echo '<p>&nbsp;</p>' . "\n";
+        echo '</div>' . "\n";
+        echo '</article>' . "\n";
+        echo '<!-- /xwpt: 90915.1b/content/blog/php       -->' . "\n";
     }
     echo '</div>' . "\n";
     /*
@@ -79,10 +134,10 @@ elseif (have_posts())
     $v_pages = $wp_query->max_num_pages;
     if ($v_pages > 1)
     {
-        echo '<!-- xwpt: 90915.1a/search/php/pagination   -->' . "\n";
+        echo '<!-- xwpt: 90915.1b/search/php/pagination   -->' . "\n";
         $v_cur_page = max(1, get_query_var('paged'));
         echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
-        echo '<!-- /xwpt: 90915.1a/search/php/pagination  -->' . "\n";
+        echo '<!-- /xwpt: 90915.1b/search/php/pagination  -->' . "\n";
     }
 }
 else
@@ -93,7 +148,7 @@ else
     echo '</div>' . "\n";
 }
 echo '</main>' . "\n";
-echo '<!-- /xwpt: 90915.1a/search/php             -->' . "\n";
+echo '<!-- /xwpt: 90915.1b/search/php             -->' . "\n";
 /*
     display sidebar
 */
