33f101a08fdf590d15c61fb07748f1ecb5688ba7
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 08:01:35 -0400

    footer.php - 90915.1b build
    Code consolidation and addition of xidipity.cfg

--- footer.php
+++ footer.php
@@ -1,26 +1,26 @@
 <?php
+
 /*
-*        file: footer.php
-*       build: 90915.1a
-* description: The template for displaying the footer
-*      github: https://github.com/WpThemeDev/xidipity
-*    comments:
-*
-* @package WordPress
-* @subpackage Xidipity
-* @since 5.0.0
-*
-***/
+ *  Xidipity WordPress Theme
+ *
+ *  file:   footer.php
+ *  build:  90915.1b
+ *  descrp: Display site footer
+ *  ref:    https://github.com/WpThemeDev/xidipity
+ *
+ *  @package WordPress
+ *  @subpackage Xidipity
+ *  @since 0.9.0
+ *
+ ****/
 /*
-    get page template
-*/
-$wp_tmpl = page_tmpl();
-echo '<!-- xwpt: 90915.1a/footer/php              -->' . "\n";
+
+echo '<!-- xwpt: 90915.1b/footer/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
-echo '<footer class="fx:pg-ct-itm fnt:size-smaller fx:align-ct-itm-2 mar:vert-0.75">' . "\n";
+echo '<footer class="fx:pg-ct-itm fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
 do_action( 'xidipity_credits' );
 echo '</footer>' . "\n";
-echo '<!-- /xwpt: 90915.1a/footer/php             -->' . "\n";
+echo '<!-- /xwpt: 90915.1b/footer/php             -->' . "\n";
 /*: wordpress code :*/
 wp_footer();
 echo '</body>' . "\n";
