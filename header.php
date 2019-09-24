223091d9b104bbb6603bfc80caef8dd315197601
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 09:15:17 -0400

    header.php - 90915.1b build
    Code consolidation and addition of xidipity.cfg

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   header.php
- *  build:  90915.1a
+ *  build:  90915.1b
  *  descrp: Header template
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -17,7 +17,7 @@ echo '<!doctype html>' . "\n";
 <html <?php
 language_attributes(); ?> >
 <?php
-echo '<!-- xwpt: 90915.1a/header/php              -->' . "\n";
+echo '<!-- xwpt: 90915.1b/header/php              -->' . "\n";
 echo '<head>' . "\n";
 echo '<meta charset="' . get_bloginfo('charset') . '">' . "\n";
 echo '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n";
@@ -35,10 +35,6 @@ echo '<!-- xwpt: flexbox/page/container/item-1    -->' . "\n";
 echo '<header class="fx:pg-ct-itm fx:align-ct-itm-1">' . "\n";
 echo '<a class="skip-link screen-reader-text" href="#content">' . __('Skip to content') . '</a>' . "\n";
 echo '<div class="header-banner">' . "\n";
-/*
-    get page template
-*/
-$wp_tmpl = page_tmpl();
 /*
     custom logo
 */
@@ -70,46 +66,53 @@ echo '<!-- /xwpt: 90915.1b/header/php             -->' . "\n";
 */
 echo '<!-- xwpt: 90828.1a/header/php/nav          -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-2    -->' . "\n";
-if ( $wp_tmpl == 'naked' )
+if ( disp_menu() == 'no' )
 {
-    echo '<nav class="fx:pg-ct-itm disp:none">' . "\n";
+    echo '<div class="fx:pg-ct-itm disp:none">' . "\n";
 }
 else
 {
-    echo '<nav class="fx:pg-ct-itm fx:align-ct-itm-2">' . "\n";
+    echo '<div class="fx:pg-ct-itm">' . "\n";
 }
 
 /*
     primary menu
 */
-$wp_page = get_page_by_path('table-of-contents');
+$wp_page = get_page_by_path('xidipity-toc');
 $wp_menus = get_nav_menu_locations();
 $wp_menu = (abs($wp_menus['primary']) !== 0);
 if ($wp_menu || $wp_page)
 {
     if ($wp_page)
     {
+        /*: xidipity toc page :*/
         if ($wp_page->ID == get_queried_object_id())
         {
-            echo '<a class="toc-btn" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
+            echo '<a class="toc-btn txt:align-center" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
         else
         {
-            echo '<a class="toc-btn" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
+            echo '<a class="toc-btn txt:align-center" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
     }
     else
     {
-        echo '<input type="checkbox" id="menu-toggle">' . "\n";
-        echo '<label for="menu-toggle"><i class="fas fa-bars"></i></label>' . "\n";
+        /*: drop down menu :*/
+        echo '<div class="fx:mnu-wrapper">' . "\n";
+        echo '<nav role="navigation" id="nav">' . "\n";
+        echo '<input class="trigger" type="checkbox" id="mainNavButton">' . "\n";
+        echo '<label for="mainNavButton" onclick><i class="material-icons vert:align-bottom">menu</i></label>' . "\n";
         wp_nav_menu(array(
             'theme_location' => 'primary',
             'menu_id' => 'primary-menu',
+            'container' => ''
         ));
+        echo '</nav>' . "\n";
+        echo '</div>' . "\n";
     }
 }
-echo '</nav>' . "\n";
-echo '<!-- /xwpt: 90915.1a/header/php/nav         -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /xwpt: 90915.1b/header/php/nav         -->' . "\n";
 /*
     eof: header.php
 */
