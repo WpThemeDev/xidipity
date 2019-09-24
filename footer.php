0c3b17531d716e53efb44463b5a1525e50fb7177
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 17:12:42 -0400

    footer.php - 90920.1a build
    Bug fixes

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   footer.php
- *  build:  90915.1b
+ *  build:  90920.1a
  *  descrp: Display site footer
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -13,14 +13,12 @@
  *  @since 0.9.0
  *
  ****/
-/*
-
-echo '<!-- xwpt: 90915.1b/footer/php              -->' . "\n";
+echo '<!-- xwpt: 90920.1a/footer/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
 echo '<footer class="fx:pg-ct-itm fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
 do_action( 'xidipity_credits' );
 echo '</footer>' . "\n";
-echo '<!-- /xwpt: 90915.1b/footer/php             -->' . "\n";
+echo '<!-- /xwpt: 90920.1a/footer/php             -->' . "\n";
 /*: wordpress code :*/
 wp_footer();
 echo '</body>' . "\n";
