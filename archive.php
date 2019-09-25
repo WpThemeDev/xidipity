62c8a0f64224abf551809d8796ce8223c7f57a75
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 20:31:27 -0400

    archive.php - 90920.1c build
    Sidebar update

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   archive.php
- *  build:  90915.1b
+ *  build:  90920.1c
  *  descrp: Display archive excerpts
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -36,9 +36,9 @@ $v_cur_page = $wp_paged;
     display header
 */
 get_header();
-echo '<!-- xwpt: 90915.1b/archive/php             -->' . "\n";
+echo '<!-- xwpt: 90920.1c/archive/php             -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-3    -->' . "\n";
-echo '<main class="fx:pg-ct-itm">' . "\n";
+echo '<main class="fx:pg-ct-itm-sbr">' . "\n";
 
 if (have_posts())
 {
@@ -57,7 +57,7 @@ if (have_posts())
     while (have_posts())
     {
         the_post();
-        echo '<!-- xwpt: 90915.1b/content/blog/php        -->' . "\n";
+        echo '<!-- xwpt: 90920.1c/content/blog/php        -->' . "\n";
         echo '<!-- xwpt: flexbox/content/container/item   -->' . "\n";
         echo '<article class="fx:cn-ct-itm fx:cn-ct-opt fx:basis-50% fx:shadow">' . "\n";
         /*
@@ -115,7 +115,7 @@ if (have_posts())
         echo '<p>&nbsp;</p>' . "\n";
         echo '</div>' . "\n";
         echo '</article>' . "\n";
-        echo '<!-- /xwpt: 90915.1b/content/blog/php       -->' . "\n";
+        echo '<!-- /xwpt: 90920.1c/content/blog/php       -->' . "\n";
     }
     echo '</div>' . "\n";
     /*
@@ -124,10 +124,10 @@ if (have_posts())
     $v_pages = $wp_query->max_num_pages;
     if ($v_pages > 1)
     {
-        echo '<!-- xwpt: 90915.1b/archive/php/pagination  -->' . "\n";
+        echo '<!-- xwpt: 90920.1c/archive/php/pagination  -->' . "\n";
         $v_cur_page = max(1, get_query_var('paged'));
         echo xidipity_paginate_links(array('page'=>$v_cur_page,'pages'=>$v_pages)) . "\n";
-        echo '<!-- /xwpt: 90915.1b/archive/php/pagination -->' . "\n";
+        echo '<!-- /xwpt: 90920.1c/archive/php/pagination -->' . "\n";
     }
 }
 else
@@ -138,7 +138,7 @@ else
     echo '</div>' . "\n";
 }
 echo '</main>' . "\n";
-echo '<!-- /xwpt: 90915.1b/archive/php            -->' . "\n";
+echo '<!-- /xwpt: 90920.1c/archive/php            -->' . "\n";
 /*
     display sidebar
 */
