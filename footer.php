0ff7aac9a76fe8c616f2871a6f109840cc3a8faf
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-24 20:34:33 -0400

    footer.php - 90920.1c build
    Sidebar update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   footer.php
- *  build:  90920.1a
+ *  build:  90920.1c
  *  descrp: Display site footer
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -13,12 +13,12 @@
  *  @since 0.9.0
  *
  ****/
-echo '<!-- xwpt: 90920.1a/footer/php              -->' . "\n";
+echo '<!-- xwpt: 90920.1c/footer/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
-echo '<footer class="fx:pg-ct-itm fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
 do_action( 'xidipity_credits' );
 echo '</footer>' . "\n";
-echo '<!-- /xwpt: 90920.1a/footer/php             -->' . "\n";
+echo '<!-- /xwpt: 90920.1c/footer/php             -->' . "\n";
 /*: wordpress code :*/
 wp_footer();
 echo '</body>' . "\n";
