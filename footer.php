60a3440b9f0882e3dc275dd99be72540fda6ba75
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-25 09:21:36 -0400

    footer.php - 90920.1d build
    Left / right sidebar integration

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   footer.php
- *  build:  90920.1c
+ *  build:  90920.1d
  *  descrp: Display site footer
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -13,12 +13,19 @@
  *  @since 0.9.0
  *
  ****/
-echo '<!-- xwpt: 90920.1c/footer/php              -->' . "\n";
+echo '<!-- xwpt: 90920.1d/footer/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
-echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+if (align_sidebar() == 'left')
+{
+    echo '<footer class="fx:pg-ct-itm-sbl fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+}
+else
+{
+    echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+}
 do_action( 'xidipity_credits' );
 echo '</footer>' . "\n";
-echo '<!-- /xwpt: 90920.1c/footer/php             -->' . "\n";
+echo '<!-- /xwpt: 90920.1d/footer/php             -->' . "\n";
 /*: wordpress code :*/
 wp_footer();
 echo '</body>' . "\n";
