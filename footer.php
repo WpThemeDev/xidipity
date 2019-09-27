149afd30093dd49d3d01fe72b7de90370a674a1a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-09-27 17:26:20 -0400

    footer.php - 90925.1a build
    Custom header integration

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
 <?php
-
 /*
  *  Xidipity WordPress Theme
  *
  *  file:   footer.php
- *  build:  90920.1d
+ *  build:  90925.1a
  *  descrp: Display site footer
  *  ref:    https://github.com/WpThemeDev/xidipity
  *
@@ -15,15 +14,25 @@
  ****/
 echo '<!-- xwpt: 90920.1d/footer/php              -->' . "\n";
 echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
+/*
+    footer management
+*/
 if (align_sidebar() == 'left')
 {
-    echo '<footer class="fx:pg-ct-itm-sbl fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+    echo '<footer class="fx:pg-ct-itm-sbl fnt:size-smaller">' . "\n";
+    echo '<div class="fx:area-ct-' . ftr_align() . ' mar:vert-0.75 pad:horz-0.5">' . "\n";
+    echo '<div class="fx:area-ct-itm" style="text-align:' . ftr_align() . '">' . "\n";
 }
 else
 {
-    echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller fx:align-ct-itm-5 mar:vert-0.75">' . "\n";
+    echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller">' . "\n";
+    echo '<div class="fx:area-ct-' . ftr_align() . ' mar:vert-0.75 pad:horz-0.5">' . "\n";
+    echo '<div class="fx:area-ct-itm" style="text-align:' . ftr_align() . '">' . "\n";
 }
-do_action( 'xidipity_credits' );
+echo '<p>' . blog_copyright() . '</p>';
+echo '<p><a href="' . get_site_url() . '">Xidipity Theme</a> &sdot; Powered by <a href="https://wordpress.org">WordPress</a>';
+echo '</div>' . "\n";
+echo '</div>' . "\n";
 echo '</footer>' . "\n";
 echo '<!-- /xwpt: 90920.1d/footer/php             -->' . "\n";
 /*: wordpress code :*/
