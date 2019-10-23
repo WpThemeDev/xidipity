a1deba46a7feacd3f7e3519aef3f4e99ae24d8db
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-10-23 11:09:10 -0400

    excerpt/plugin.js - 91020.1a build
    Update image

--- assets/tinymceplugins/excerpt/plugin.js
+++ assets/tinymceplugins/excerpt/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   excerpt/plugin.js
- *  build:  90927.1b
+ *  build:  91020.1a
  *  descrp: excerpt plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -14,9 +14,9 @@
 (function() {
     tinymce.PluginManager.add('excerpt', function( editor, url ) {
         editor.addButton( 'excerpt', {
-            title: 'Excerpt',
+            title: 'Insert excerpt',
             icon: 'mce-excerpt',
-            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0%0D%0APSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0%0D%0ASDBWMHoiLz48cGF0aCBkPSJNMjAgMkg0Yy0xLjEgMC0yIC45LTIgMnYxOGw0LTRoMTRjMS4xIDAg%0D%0AMi0uOSAyLTJWNGMwLTEuMS0uOS0yLTItMnptMCAxNEg2bC0yIDJWNGgxNnYxMnoiLz48L3N2Zz4=',
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTIxIDNIM2MtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxOGMxLjEgMCAyLS45IDItMlY1YzAtMS4xLS45LTItMi0yem0wIDE2SDNWNWgxMHY0aDh2MTB6Ii8+PC9zdmc+',
             onclick: function() {
               var html = '<!-- xwpt: 90927.1b/mce/toolbar/excerpt     --><table id="excerpt" cellspacing="0"><tbody><tr><td class="excerpt:txt">Type excerpt here ...</td><td class="excerpt:tag"><!--more--></td></tr></tbody></table><!-- /xwpt: 90927.1b/mce/toolbar/excerpt    -->';
               editor.insertContent(html);
