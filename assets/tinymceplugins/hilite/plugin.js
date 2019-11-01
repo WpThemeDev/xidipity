3f5b7483ab725839d150f0a2ef525ae8e31f4091
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-10-31 20:47:38 -0400

    hilite/plugin.js - 91025.1a build
    Remove outer function

--- assets/tinymceplugins/hilite/plugin.js
+++ assets/tinymceplugins/hilite/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   hilite/plugin.js
- *  build:  91020.1a
+ *  build:  91025.1a
  *  descrp: hilite plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -10,19 +10,17 @@
  *  @subpackage Xidipity
  *  @since 0.9.0
  *
-**/
-(function() {
-    tinymce.PluginManager.add('hilite', function( editor, url ) {
-        editor.addButton( 'hilite', {
-            title: 'High lite',
-            icon: 'mce-hilite',
-            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMEgwdjI0aDI0eiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0xNy42NiA3LjkzTDEyIDIuMjcgNi4zNCA3LjkzYy0zLjEyIDMuMTItMy4xMiA4LjE5IDAgMTEuMzFDNy45IDIwLjggOS45NSAyMS41OCAxMiAyMS41OGMyLjA1IDAgNC4xLS43OCA1LjY2LTIuMzQgMy4xMi0zLjEyIDMuMTItOC4xOSAwLTExLjMxek0xMiAxOS41OWMtMS42IDAtMy4xMS0uNjItNC4yNC0xLjc2QzYuNjIgMTYuNjkgNiAxNS4xOSA2IDEzLjU5cy42Mi0zLjExIDEuNzYtNC4yNEwxMiA1LjF2MTQuNDl6Ii8+PC9zdmc+',
-            onclick: function() {
-                tinymce.execCommand('mceReplaceContent',false,'<span class="bg:highlight">{$selection}</span>');
-            }
-        });
+ **/
+tinymce.PluginManager.add('hilite', function (editor, url) {
+    editor.addButton('hilite', {
+        title: 'High lite',
+        icon: 'mce-hilite',
+        image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjQgMEgwdjI0aDI0eiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0xNy42NiA3LjkzTDEyIDIuMjcgNi4zNCA3LjkzYy0zLjEyIDMuMTItMy4xMiA4LjE5IDAgMTEuMzFDNy45IDIwLjggOS45NSAyMS41OCAxMiAyMS41OGMyLjA1IDAgNC4xLS43OCA1LjY2LTIuMzQgMy4xMi0zLjEyIDMuMTItOC4xOSAwLTExLjMxek0xMiAxOS41OWMtMS42IDAtMy4xMS0uNjItNC4yNC0xLjc2QzYuNjIgMTYuNjkgNiAxNS4xOSA2IDEzLjU5cy42Mi0zLjExIDEuNzYtNC4yNEwxMiA1LjF2MTQuNDl6Ii8+PC9zdmc+',
+        onclick: function () {
+            tinymce.execCommand('mceReplaceContent', false, '<span class="bg:highlight">{$selection}</span>');
+        }
     });
-})();
+});
 /*
     eof: hilite/plugin.js
 */
