a662aa5b9948b44190730a9075f7a639042ad995
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-11-01 09:09:31 -0400

    twocolumn/plugin.js - 91025.1a build
    Update focus

--- assets/tinymceplugins/twocolumn/plugin.js
+++ assets/tinymceplugins/twocolumn/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   twocolumn/plugin.js
- *  build:  91020.1a
+ *  build:  91025.1a
  *  descrp: two column plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -10,20 +10,22 @@
  *  @subpackage Xidipity
  *  @since 0.9.0
  *
-**/
-(function() {
-    tinymce.PluginManager.add('twocolumn', function( editor, url ) {
-        editor.addButton( 'twocolumn', {
-            title: 'Insert 2 column template',
-            icon: 'mce-column',
-            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNLTc0IDI5aDQ4djQ4aC00OFYyOXpNMCAwaDI0djI0SDBWMHptMCAwaDI0djI0SDBWMHoiLz48cGF0aCBkPSJNMTMgMTJoN3YxLjVoLTd6bTAtMi41aDdWMTFoLTd6bTAgNWg3VjE2aC03ek0yMSA0SDNjLTEuMSAwLTIgLjktMiAydjEzYzAgMS4xLjkgMiAyIDJoMThjMS4xIDAgMi0uOSAyLTJWNmMwLTEuMS0uOS0yLTItMnptMCAxNWgtOVY2aDl2MTN6Ii8+PC9zdmc+',
-            onclick: function() {
-                var html = '<!-- xwpt: 90927.1a/mce/toolbar/multi-col   --><table class="cols:auto"><tbody><tr><td><p>&nbsp;</p></td></tr></tbody></table><!-- /xwpt: 90927.1a/mce/toolbar/multi-col  -->';
-                editor.insertContent(html);
-            }
-        });
+ **/
+tinymce.PluginManager.add('twocolumn', function (editor, url) {
+    editor.addButton('twocolumn', {
+        title: 'Insert 2 column template',
+        icon: 'mce-column',
+        image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNLTc0IDI5aDQ4djQ4aC00OFYyOXpNMCAwaDI0djI0SDBWMHptMCAwaDI0djI0SDBWMHoiLz48cGF0aCBkPSJNMTMgMTJoN3YxLjVoLTd6bTAtMi41aDdWMTFoLTd6bTAgNWg3VjE2aC03ek0yMSA0SDNjLTEuMSAwLTIgLjktMiAydjEzYzAgMS4xLjkgMiAyIDJoMThjMS4xIDAgMi0uOSAyLTJWNmMwLTEuMS0uOS0yLTItMnptMCAxNWgtOVY2aDl2MTN6Ii8+PC9zdmc+',
+        onclick: function () {
+            var dom = editor.dom;
+            var uniqueID = dom.uniqueId();
+            var html = '<!-- xwpt: 90927.1a/mce/toolbar/multi-col   --><table class="cols:auto"><tbody><tr><td><p id="' + uniqueID + '">&nbsp;</p></td></tr></tbody></table><!-- /xwpt: 90927.1a/mce/toolbar/multi-col  -->';
+            editor.insertContent(html);
+            var newTwoColumn = dom.select('p#' + uniqueID)[0];
+            editor.selection.setCursorLocation(newTwoColumn);
+        }
     });
-})();
+});
 /*
     eof: twocolumn/plugin.js
 */
