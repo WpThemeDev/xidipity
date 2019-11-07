5b28a156560910dd6b86175d0448f168984159ac
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-11-06 19:34:51 -0500

    clrfmt/plugin.js - 91101.1a build
    Update title

--- assets/tinymceplugins/clrfmt/plugin.js
+++ assets/tinymceplugins/clrfmt/plugin.js
@@ -2,7 +2,7 @@
  *  Xidipity WordPress Theme
  *
  *  file:   clrfmt/plugin.js
- *  build:  91025.1a
+ *  build:  91101.1a
  *  descrp: clrfmt plugin
  *  ref:    https://www.tiny.cloud/
  *
@@ -13,7 +13,7 @@
 **/
 tinymce.PluginManager.add('clrfmt', function( editor, url ) {
     editor.addButton( 'clrfmt', {
-        title: 'clear format',
+        title: 'Clear Format',
         icon: 'mce-clrfmt',
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjAgOFY1SDYuMzlsMyAzaDEuODNsLS41NSAxLjI4IDIuMDkgMi4xTDE0LjIxIDh6TTMuNDEgNC44NkwyIDYuMjdsNi45NyA2Ljk3TDYuNSAxOWgzbDEuNTctMy42NkwxNi43MyAyMWwxLjQxLTEuNDF6Ii8+PC9zdmc+',
         cmd: 'removeformat'
