56ab36a85a094ebdc60406da3ab0c1dc57948b98
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-12-18 13:38:25 -0500

    [1] Update contents to new structure

--- assets/tinymceplugins/toggle-fullscreen/plugin.js
+++ assets/tinymceplugins/toggle-fullscreen/plugin.js
@@ -9,16 +9,16 @@
  * Function:        toggle tinymce full screen & standard view
  * File Name:       toggle-fullscreen/plugin.js
  * GitHub:          https://github.com/WpThemeDev/xidipity/
- * Build:           91211.1a
+ * Build:           91215.1a
  * Revision:        1
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
  *                  https://xidipity.com/documentation/reference/editor/toolbar/toggle-fullscreen/
  */
-tinymce.PluginManager.add('xscreen', function(editor, url) {
-    editor.addButton('xscreen', {
+tinymce.PluginManager.add('toggle_fullscreen', function(editor, url) {
+    editor.addButton('toggle_fullscreen', {
         title: 'Toggle Full Screen',
-        icon: 'mce-fullscreen',
+        icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTcgN0g3Yy0yLjc2IDAtNSAyLjI0LTUgNXMyLjI0IDUgNSA1aDEwYzIuNzYgMCA1LTIuMjQgNS01cy0yLjI0LTUtNS01ek03IDE1Yy0xLjY2IDAtMy0xLjM0LTMtM3MxLjM0LTMgMy0zIDMgMS4zNCAzIDMtMS4zNCAzLTMgM3oiLz48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+PC9zdmc+',
         onclick: function() {
             editor.execCommand('mceFullscreen');
@@ -26,5 +26,5 @@ tinymce.PluginManager.add('xscreen', function(editor, url) {
     });
 });
 /**
- *  eof: text-align/plugin.js
+ *  eof: toggle-fullscreen/plugin.js
  */
