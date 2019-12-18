f8df344fecbed4b813798f4ec1f45f799a591641
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-12-18 13:24:12 -0500

    [1] Update contents to new structure

--- assets/tinymceplugins/Rename apply-text-alignment/plugin.js
+++ assets/tinymceplugins/Rename apply-text-alignment/plugin.js
@@ -6,23 +6,23 @@
  * @copyright       2019 John Baer
  * @license         GPL-3.0-or-later
  *
- * Function:        align selected text
- * File Name:       text-align/plugin.js
+ * Function:        apply text alignment
+ * File Name:       apply-text-alignment/plugin.js
  * GitHub:          https://github.com/WpThemeDev/xidipity/
- * Build:           91211.1a
+ * Build:           91215.1a
  * Revision:        1
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
- *                  https://xidipity.com/documentation/reference/editor/toolbar/text-align/
+ *                  https://xidipity.com/documentation/reference/editor/toolbar/apply-text-alignment/
  */
-tinymce.PluginManager.add('txtalign', function(editor, url) {
-    editor.addButton('txtalign', {
+tinymce.PluginManager.add('apply_txt_align', function(editor, url) {
+    editor.addButton('apply_txt_align', {
         type: 'splitbutton',
-        title: 'Align Left',
-        icon: 'mce-alignleft',
+        title: 'Align Center',
+        icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0%0D%0APSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTUgMTVIM3YyaDEydi0yem0wLThI%0D%0AM3YyaDEyVjd6TTMgMTNoMTh2LTJIM3Yyem0wIDhoMTh2LTJIM3Yyek0zIDN2MmgxOFYzSDN6Ii8+%0D%0APHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPjwvc3ZnPg==',
         onclick: function() {
-            editor.execCommand('JustifyLeft');
+            editor.execCommand('JustifyCenter');
         },
         menu: [{
             icon: false,
@@ -48,9 +48,27 @@ tinymce.PluginManager.add('txtalign', function(editor, url) {
             onclick: function() {
                 editor.execCommand('JustifyFull');
             }
+        }, {
+            icon: false,
+            text: '•\xa0Indent >',
+            onclick: function() {
+                editor.execCommand('Indent');
+            }
+        }, {
+            icon: false,
+            text: '•\xa0Indent <',
+            onclick: function() {
+                editor.execCommand('Outdent');
+            }
+        }, {
+            icon: false,
+            text: '•\xa0_ Paragraph >',
+            onclick: function() {
+                tinymce.execCommand('mceReplaceContent', false, '<span style="display:block;text-indent:2.5rem;">{$selection}</span>');
+            }
         }],
     });
 });
 /**
- *  eof: text-align/plugin.js
+ *  eof: apply-text-alignment/plugin.js
  */
