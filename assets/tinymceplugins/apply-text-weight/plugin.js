5e585ec32ae9d71e3e5f433c7577b95743507339
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-12-18 13:35:05 -0500

    [1] Update contents to new structure

--- assets/tinymceplugins/apply-text-weight/plugin.js
+++ assets/tinymceplugins/apply-text-weight/plugin.js
@@ -7,22 +7,22 @@
  * @license         GPL-3.0-or-later
  *
  * Function:        wrap selected text with span tags with weight styles
- * File Name:       text-weight/plugin.js
+ * File Name:       apply-text-weight/plugin.js
  * GitHub:          https://github.com/WpThemeDev/xidipity/
- * Build:           91211.1a
+ * Build:           91215.1a
  * Revision:        1
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
- *                  https://xidipity.com/documentation/reference/editor/toolbar/text-weight/
+ *                  https://xidipity.com/documentation/reference/editor/toolbar/apply-text-weight/
  */
-tinymce.PluginManager.add('fntwgt', function (editor, url) {
-    editor.addButton('fntwgt', {
+tinymce.PluginManager.add('apply_txt_weight', function (editor, url) {
+    editor.addButton('apply_txt_weight', {
         type: 'splitbutton',
-        title: 'Semi Bold',
-        icon: 'mce-fntwgt',
+        title: 'Normal',
+        icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0%0D%0APSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTUuNiAxMC43OWMuOTctLjY3IDEu%0D%0ANjUtMS43NyAxLjY1LTIuNzkgMC0yLjI2LTEuNzUtNC00LTRIN3YxNGg3LjA0YzIuMDkgMCAzLjcx%0D%0ALTEuNyAzLjcxLTMuNzkgMC0xLjUyLS44Ni0yLjgyLTIuMTUtMy40MnpNMTAgNi41aDNjLjgzIDAg%0D%0AMS41LjY3IDEuNSAxLjVzLS42NyAxLjUtMS41IDEuNWgtM3YtM3ptMy41IDlIMTB2LTNoMy41Yy44%0D%0AMyAwIDEuNS42NyAxLjUgMS41cy0uNjcgMS41LTEuNSAxLjV6Ii8+PHBhdGggZD0iTTAgMGgyNHYy%0D%0ANEgweiIgZmlsbD0ibm9uZSIvPjwvc3ZnPg==',
         onclick: function () {
-            editor.formatter.toggle('wgt600');
+            editor.formatter.toggle('wgt400');
         },
         menu: [{
                 icon: false,
@@ -91,5 +91,5 @@ tinymce.PluginManager.add('fntwgt', function (editor, url) {
     });
 });
 /**
- *  eof: text-weight/plugin.js
+ *  eof: apply-text-weight/plugin.js
  */
