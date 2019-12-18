3d170fc7eadfcbb8b9e7a475cb606abf4f40939b
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-12-18 11:42:35 -0500

    [1] Update contents to new structure

--- assets/tinymceplugins/clear-format/plugin.js
+++ assets/tinymceplugins/clear-format/plugin.js
@@ -9,18 +9,18 @@
  * Function:        clear format of selected text
  * File Name:       clear-format/plugin.js
  * GitHub:          https://github.com/WpThemeDev/xidipity/
- * Build:           91211.1a
+ * Build:           91215.1a
  * Revision:        1
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
  *                  https://xidipity.com/documentation/reference/editor/toolbar/clear-format/
  */
-tinymce.PluginManager.add('clrfmt', function( editor, url ) {
-    editor.addButton( 'clrfmt', {
+tinymce.PluginManager.add('clear_format', function( editor, url ) {
+    editor.addButton( 'clear_format', {
         title: 'Clear Format',
-        icon: 'mce-clrfmt',
+        icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjAgOFY1SDYuMzlsMyAzaDEuODNsLS41NSAxLjI4IDIuMDkgMi4xTDE0LjIxIDh6TTMuNDEgNC44NkwyIDYuMjdsNi45NyA2Ljk3TDYuNSAxOWgzbDEuNTctMy42NkwxNi43MyAyMWwxLjQxLTEuNDF6Ii8+PC9zdmc+',
-        cmd: 'removeformat'
+        cmd: 'RemoveFormat'
     });
 });
 /**
