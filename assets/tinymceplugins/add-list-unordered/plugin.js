538a49dfa53f53eb9360e21be351c411ecd80be2
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2019-12-18 11:32:17 -0500

    [1] Update contents to new structure

--- assets/tinymceplugins/add-list-unordered/plugin.js
+++ assets/tinymceplugins/add-list-unordered/plugin.js
@@ -6,20 +6,20 @@
  * @copyright       2019 John Baer
  * @license         GPL-3.0-or-later
  *
- * Function:        tinymce unordered toolbar button
+ * Function:        add unordered list
  * File Name:       add-list-unordered/plugin.js
  * GitHub:          https://github.com/WpThemeDev/xidipity/
- * Build:           91211.1a
+ * Build:           91215.1a
  * Revision:        1
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
  *                  https://xidipity.com/documentation/reference/editor/toolbar/add-list-unordered/
  */
-tinymce.PluginManager.add('ullist', function (editor, url) {
-    editor.addButton('ullist', {
+tinymce.PluginManager.add('add_lst_unorder', function (editor, url) {
+    editor.addButton('add_lst_unorder', {
         type: 'splitbutton',
         title: 'Unordered lists',
-        icon: 'mce-ullist',
+        icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNNCAxMC41Yy0uODMgMC0xLjUuNjctMS41IDEuNXMuNjcgMS41IDEuNSAxLjUgMS41LS42NyAxLjUtMS41LS42Ny0xLjUtMS41LTEuNXptMC02Yy0uODMgMC0xLjUuNjctMS41IDEuNVMzLjE3IDcuNSA0IDcuNSA1LjUgNi44MyA1LjUgNiA0LjgzIDQuNSA0IDQuNXptMCAxMmMtLjgzIDAtMS41LjY4LTEuNSAxLjVzLjY4IDEuNSAxLjUgMS41IDEuNS0uNjggMS41LTEuNS0uNjctMS41LTEuNS0xLjV6TTcgMTloMTR2LTJIN3Yyem0wLTZoMTR2LTJIN3Yyem0wLTh2MmgxNFY1SDd6Ii8+PHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgyNHYyNEgwVjB6Ii8+PC9zdmc+',
         onclick: function () {
             var dom = editor.dom;
