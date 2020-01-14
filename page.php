e0e493c7b88e88b8fd89c2f01786a25a4faa61d0
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-01-13 19:57:15 -0500

    1. update pad:all-? classes

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
  *
  * File Name:       page.php
  * Function:        display page
- * Build:           200104-1
+ * Build:           200107-1
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -79,7 +79,7 @@ if ($wp_query->have_posts()) {
         content
         */
         echo '<!-- xwpt: flexbox/content/wrapper          -->' . "\n";
-        echo '<div class="fx:cn-itm-wrapper pad:all-1">' . "\n";
+        echo '<div class="fx:cn-itm-wrapper pad:1">' . "\n";
         the_content();
         echo '</div>' . "\n";
         /*
@@ -136,7 +136,7 @@ get_footer();
 
 /*
  * EOF:     page.php
- * Build:   200104-1
+ * Build:   200107-1
  *
  */
 ?>
