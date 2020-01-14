b33ff89c6c72207e36c75e96e9071e7aa584e828
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-01-13 19:38:19 -0500

    1. update excerpt

--- assets/tinymceplugins/add-misc-opts/plugin.js
+++ assets/tinymceplugins/add-misc-opts/plugin.js
@@ -3,7 +3,7 @@
  *
  * File Name:       add-misc-opts/plugin.js
  * Function:        tinymce misc opts plugin
- * Build:           200104-1
+ * Build:           200107-1
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -18,37 +18,37 @@
  */
 
 tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
-    editor.addButton('add_misc_opts', {
-        type: 'splitbutton',
-        title: 'Block Quote',
-        icon: false,
-        image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNNSAxN2gzbDItNFY3SDR2NmgzbC0yIDR6bTEwIDBoM2wyLTRWN2gtNnY2aDNsLTIgNHoiLz48L3N2Zz4=',
-        onclick: function() {
-            editor.execCommand('mceBlockQuote');
-        },
-        menu: [{
-            icon: false,
-            text: '•\xa0Block Quote',
-            onclick: function() {
-                editor.execCommand('mceBlockQuote');
-            }
-        }, {
-            icon: false,
-            text: '•\xa0Excerpt',
-            onclick: function() {
-              var dom = editor.dom;
-              var uniqueID = dom.uniqueId();                                
-              var html = '<!-- xwpt: 90927.1b/mce/toolbar/excerpt     --><table class="exc dsp:none!web" cellspacing="0"><tbody><tr><td class="exc:content" id="' + uniqueID + '"></td><td class="exc:tag"><!--more--></td></tr></tbody></table><!-- /xwpt: 90927.1b/mce/toolbar/excerpt    -->';
-              editor.insertContent(html);
-              var newExcerpt = dom.select('p#' + uniqueID)[0];
-              editor.selection.setCursorLocation(newExcerpt);
-            }
-        }],
-    });
+	editor.addButton('add_misc_opts', {
+		type: 'splitbutton',
+		title: 'Block Quote',
+		icon: false,
+		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNNSAxN2gzbDItNFY3SDR2NmgzbC0yIDR6bTEwIDBoM2wyLTRWN2gtNnY2aDNsLTIgNHoiLz48L3N2Zz4=',
+		onclick: function() {
+			editor.execCommand('mceBlockQuote');
+		},
+		menu: [{
+			icon: false,
+			text: '•\xa0Block Quote',
+			onclick: function() {
+				editor.execCommand('mceBlockQuote');
+			}
+		}, {
+			icon: false,
+			text: '•\xa0Excerpt',
+			onclick: function() {
+				var dom = editor.dom;
+				var uniqueID = dom.uniqueId();
+				var html = '<!-- xwpt: 90927.1b/mce/toolbar/excerpt     --><table class="exc web[dsp:none]" cellspacing="0"><tbody><tr><td class="exc:content" id="' + uniqueID + '"></td><td class="exc:tag"><!--more--></td></tr></tbody></table><!-- /xwpt: 90927.1b/mce/toolbar/excerpt    -->';
+				editor.insertContent(html);
+				var newExcerpt = dom.select('p#' + uniqueID)[0];
+				editor.selection.setCursorLocation(newExcerpt);
+			}
+		}],
+	});
 });
 
 /*
  * EOF:     add-misc-opts/plugin.js
- * Build:   200104-1
+ * Build:   200107-1
  *
  */
