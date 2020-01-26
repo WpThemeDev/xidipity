9523e70662c1594a0f3580832b276c89c038d80a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-01-26 16:20:20 -0500

    1. update mce tint class

--- assets/tinymceplugins/add-vertical-space/plugin.js
+++ assets/tinymceplugins/add-vertical-space/plugin.js
@@ -3,7 +3,7 @@
  *
  * File Name:       add-vertical-space/plugin.js
  * Function:        tinymce vertical space plugin
- * Build:           200104-1
+ * Build:           200126-1
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -11,8 +11,8 @@
  * @author          John Baer
  * @copyright       2019-2020 John Baer
  * @license         GPL-3.0-or-later
- * @version      	1.0
- * @since      		0.9
+ * @version         1.0
+ * @since           0.9
  * @link            https://www.tiny.cloud/docs/
  *
  */
@@ -20,47 +20,47 @@
 tinymce.PluginManager.add('add_vert_space', function(editor, url) {
     editor.addButton('add_vert_space', {
         title: '\xBD Vertical Space',
-    icon: false,
+        icon: false,
         image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAg%0D%0AMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTSAz%0D%0AIDE5IEwgMjEgMTkgTCAyMSAxMyBMIDMgMTMgTCAzIDE5IFogTSAzIDExIEwgMjEgMTEgTCAyMSA5%0D%0AIEwgMyA5IEwgMyAxMSBaIE0gMyA1IEwgMyA3IEwgMjEgNyBMIDIxIDUgTCAzIDUgWiIgc3R5bGU9%0D%0AImZpbGw6IHJnYigzMywgMzMsIDMzKTsiLz4KPC9zdmc+',
         type: 'splitbutton',
         onclick: function() {
-            editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 50%); font-size: calc(var(--dft-font-size) * 50%);">&nbsp;</p>');
+            editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 50%); font-size: calc(var(--dft-font-size) * 50%);">&nbsp;</p>');
         },
         menu: [{
             text: '•\xa0\xBD Line',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 50%); font-size: calc(var(--dft-font-size) * 50%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 50%); font-size: calc(var(--dft-font-size) * 50%);">&nbsp;</p>');
             }
         }, {
             text: '•\xa0\xBE Line',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 75%); font-size: calc(var(--dft-font-size) * 75%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 75%); font-size: calc(var(--dft-font-size) * 75%);">&nbsp;</p>');
             }
         }, {
             text: '•\xa01\xBD Lines',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 150%); font-size: calc(var(--dft-font-size) * 150%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 150%); font-size: calc(var(--dft-font-size) * 150%);">&nbsp;</p>');
             }
         }, {
             text: '•\xa02 Lines',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 200%); font-size: calc(var(--dft-font-size) * 200%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 200%); font-size: calc(var(--dft-font-size) * 200%);">&nbsp;</p>');
             }
         }, {
             text: '•\xa02\xBD Lines',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 250%); font-size: calc(var(--dft-font-size) * 250%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 250%); font-size: calc(var(--dft-font-size) * 250%);">&nbsp;</p>');
             }
         }, {
             text: '•\xa03 Lines',
             icon: false,
             onclick: function() {
-                editor.insertContent('<p class="dsp:tint!mce" style="line-height: calc(var(--dft-line-height) * 300%); font-size: calc(var(--dft-font-size) * 300%);">&nbsp;</p>');
+                editor.insertContent('<p class="mce[dsp:tint]" style="line-height: calc(var(--dft-line-height) * 300%); font-size: calc(var(--dft-font-size) * 300%);">&nbsp;</p>');
             }
         }]
     });
@@ -68,6 +68,6 @@ tinymce.PluginManager.add('add_vert_space', function(editor, url) {
 
 /*
  * EOF:     add-vertical-space/plugin.js
- * Build:   200104-1
+ * Build:   200126-1
  *
  */
