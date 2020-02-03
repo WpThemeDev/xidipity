42fd80eb0c20c7c762945f342cadb8f0e792f634
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-02-02 20:58:14 -0500

    1.  update mar to + syntax
    @ 28, 29, 34, 35

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *
  * File Name:       footer.php
  * Function:        display page footer
- * Build:           200205-1
+ * Build:           200206
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -12,7 +12,7 @@
  * @author          John Baer
  * @copyright       2019-2020 John Baer
  * @license         GPL-3.0-or-later
- * @version         5.0
+ * @version         1.0
  * @since           0.9
  * @link            https://developer.wordpress.org/themes/basics/
  *
@@ -25,14 +25,14 @@ echo '<!-- xwpt: flexbox/page/container/item-5    -->' . "\n";
 */
 if (XWT_SIDEBAR_ALIGN == 'left')
 {
-    echo '<footer class="fx:pg-ct-itm-sbl fnt:size-smaller mar:bottom-1">' . "\n";
-    echo '<div class="fx:area-ct-' . XWT_FTR_ALIGN . ' mar:vert-0.75">' . "\n";
+    echo '<footer class="fx:pg-ct-itm-sbl fnt:size-smaller mar:bottom+1">' . "\n";
+    echo '<div class="fx:area-ct-' . XWT_FTR_ALIGN . ' mar:vert+0.75">' . "\n";
     echo '<div class="fx:area-itm-' . XWT_FTR_ALIGN .  '">' . "\n";
 }
 else
 {
-    echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller mar:bottom-1">' . "\n";
-    echo '<div class="fx:area-ct-' . XWT_FTR_ALIGN . ' mar:vert-0.75">' . "\n";
+    echo '<footer class="fx:pg-ct-itm-sbr fnt:size-smaller mar:bottom+1">' . "\n";
+    echo '<div class="fx:area-ct-' . XWT_FTR_ALIGN . ' mar:vert+0.75">' . "\n";
     echo '<div class="fx:area-itm-' . XWT_FTR_ALIGN .  '">' . "\n";
 }
 echo '<p>' . blog_copyright() . '</p>';
@@ -48,7 +48,7 @@ echo '</html>' . "\n";
 
 /*
  * EOF:     footer.php
- * Build:   200205-1
+ * Build:   200206
  *
  */
 ?>
