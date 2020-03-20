87c36ae5866c2551abdec1179ceca93ca7a14e05
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-03-20 09:56:17 -0400

    Update page.php
    1. update content wrapper

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
  *
  * File Name:       page.php
  * Function:        display page
- * Build:           200206
+ * Build:           200318
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -79,17 +79,20 @@ if ($wp_query->have_posts()) {
         content
         */
         echo '<!-- xwpt: flexbox/content/wrapper          -->' . "\n";
-        echo '<div class="fx:cn-itm-wrapper pad:+1">' . "\n";
+        echo '<div class="cn:flex">' . "\n";
+        echo '<div class="cn:block">' . "\n";
         the_content();
         echo '</div>' . "\n";
+        echo '</div>' . "\n";
+        echo '<!-- /xwpt: flexbox/content/wrapper         -->' . "\n";
         /*
             page footer
         */
-	    /*: edit :*/
-	    if (get_edit_post_link())
-	    {
-	        $v_meta_list .= dsp_edit(get_edit_post_link()) . '|';
-	    }
+        /*: edit :*/
+        if (get_edit_post_link())
+        {
+            $v_meta_list .= dsp_edit(get_edit_post_link()) . '|';
+        }
         /*: date :*/
         $v_meta_list .= dsp_date(get_the_date()) . '|';
         echo '<div class="pad:left+1 fnt:size-smaller prt(dsp:none)">' . "\n";
@@ -132,7 +135,7 @@ get_footer();
 
 /*
  * EOF:     page.php
- * Build:   200206
+ * Build:   200318
  *
  */
 ?>
