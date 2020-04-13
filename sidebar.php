137c8788f3dd7e9a537577438905d6c1f1327c8a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-12 20:33:11 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
  *
  * File Name:       sidebar.php
  * Function:        display sidebar
- * Build:           200107-1
+ * Build:           200322
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -12,55 +12,32 @@
  * @author          John Baer
  * @copyright       2019-2020 John Baer
  * @license         GPL-3.0-or-later
- * @version			1.0
- * @since			0.9
+ * @version      1.0
+ * @since      0.9
  * @link            https://developer.wordpress.org/themes/basics/
  *
  */
 
 /*
-    get page options
+***/
+echo '<!--  file:sidebar.php -->' . "\n";
+/***
 */
-echo '<!-- xwpt: 90915.1a/sidebar/php             -->' . "\n";
-echo '<!-- xwpt: flexbox/page/container/item-4    -->' . "\n";
+echo '<!--  fi:2/SECTION -->' . "\n";
+echo '<section class="fxd:2 fxe:6 ht:min10 wd:100% md)wd:20 prt[dsp:none]">' . "\n";
+
 /*
-    logic check
+***
+    * dynamic_sidebar
+***
 */
-if (disp_sidebar() == 'no')
-{
-    if (XWT_SIDEBAR_ALIGN == 'left')
-    {
-        echo '<div class="fx:pg-ct-itm-sbl dsp:none">' . "\n";
-    }
-    else
-    {
-        echo '<div class="fx:pg-ct-itm-sbr dsp:none">' . "\n";
-    }
-}
-else
-{
-    if (XWT_SIDEBAR_ALIGN == 'left')
-    {
-        echo '<div class="fx:pg-ct-itm-sbl">' . "\n";
-    }
-    else
-    {
-        echo '<div class="fx:pg-ct-itm-sbr">' . "\n";
-    }
-}
-echo '<!-- xwpt: flexbox/sidebar/container        -->' . "\n";
-echo '<div class="fx:sb-ct fx:sb-ct-opt">' . "\n";
-/*: hook for print.css to hide sidebar :*/
-echo '<div id="sidebar">' . "\n";
 dynamic_sidebar( 'sidebar-1' );
-echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '<!-- /xwpt: 90915.1a/sidebar/php            -->' . "\n";
+echo '</section>' . "\n";
+echo '<!-- /fi:2/SECTION -->' . "\n";
 
 /*
  * EOF:     sidebar.php
- * Build:   200104-1
+ * Build:   200322
  *
  */
 ?>
