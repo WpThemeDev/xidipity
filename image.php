889b4459a1d2623eb29fa7c04b029f466f75bead
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-12 20:22:21 -0400

    Update image.php

--- image.php
+++ image.php
@@ -4,7 +4,7 @@
  *
  * File Name:       image.php
  * Function:        display media library image
- * Build:           200206
+ * Build:           200322
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -19,145 +19,196 @@
  */
 
 /*
-    system variables
+***
+    * variables defaults
+***
 */
-$wp_attm = get_queried_object();
-$wp_attm_permalink = get_permalink( $wp_attm->post_parent );
-$wp_attm_title = get_post( $wp_attm->post_parent )->post_title;
-$wp_attm_title_lnk = '<a href="' . $wp_attm_permalink  . '"  title="' . $wp_attm_title . '">' . $wp_attm_title . '</a>';
+$aspect_ratio = '';
+$ratio = 0;
+$seo = 'Xidipity WordPress Theme Media Library Image';
 $wp_metadata = wp_get_attachment_metadata();
+
 /*
-    local variables
+***
+    * display menus
+***
 */
-$v_aspect_ratio = '';
-$v_meta_list = '';
-$v_ratio = 0;
-$v_title = '';
+disp_menu('yes');
+
 /*
-    display page header
+***
+    * function: get_header
+    * descript: display page header
+    * ref: developer.wordpress.org/reference/functions/get_header/
+***
 */
 get_header();
-echo '<!-- xwpt: 90920.1d/image/php               -->' . "\n";
-echo '<!-- xwpt: flexbox/page/container/item-3    -->' . "\n";
+
+/*
+***/
+echo '<!--  file:page.php -->' . "\n";
+/***
+
+*/
+echo '<!--  fi:3/HTML -->' . "\n";
+echo '<div class="fxd:2 fxe:6 wd:100%">' . "\n";
+echo '<!--  fc:MAIN -->' . "\n";
+
+/*
+***
+    * align sidebar
+***
+*/
 if (XWT_SIDEBAR_ALIGN == 'left')
 {
-    echo '<main class="fx:pg-ct-itm-sbl">' . "\n";
+    echo '<main class="fx:c md)fx:r-rev fxa:1 fxb:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+    echo '<!--  fi:1/SECTION -->' . "\n";
+    echo '<section class="fxd:2 fxe:6 mar:bottom+0.5 md)mar:left+0.5 wd:100%">' . "\n";
 }
 else
 {
-    echo '<main class="fx:pg-ct-itm-sbr">' . "\n";
+    echo '<main class="fx:c md)fx:r fxa:1 fxb:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+    echo '<!--  fi:1/SECTION -->' . "\n";
+    echo '<section class="fxd:2 fxe:6 mar:bottom+0.5 md)mar:right+0.5 wd:100%">' . "\n";
 }
-echo '<!-- xwpt: flexbox/content/container        -->' . "\n";
-echo '<div class="fx:cn-ct">' . "\n";
-/*: get page title :*/
-$v_title = get_the_title();
-if (strpos($v_title, '.') > 1)
+
+echo '<!--  ct:ARTICLE -->' . "\n";
+echo '<article class="box:shadow bg:content fg:content dsp:block pad:hrz+1 ht:min10 wd:100%">' . "\n";
+
+if ($wp_query->have_posts())
 {
-    $v_title = substr($v_title, 0, strpos($v_title, '.'));
-}
-/*: has attm :*/
-if (have_posts())
-{
-    $v_ratio = round(absint($wp_metadata['height']) / absint($wp_metadata['width']) , 4);
-    switch ($v_ratio)
+
+    echo '<!--  ct:HEADER -->' . "\n";
+    echo '<header class="wd:100%">' . "\n";
+
+    /*
+    ***
+        * function: the_title
+        * descript: display page title
+        * ref: developer.wordpress.org/reference/functions/the_title/
+    ***
+    */
+    the_title('<div class="pg:title">Media: ', '</div>');
+    echo '</header>' . "\n";
+    echo '<!-- /ct:HEADER -->' . "\n";
+    echo '<!--  ct:BODY -->' . "\n";
+    echo '<div class="bg:content ht:min10 mar:bottom+0.5 wd:100%">' . "\n";
+
+    /*
+    ***
+        * display image
+    ***
+    */
+    $caption = wp_get_attachment_caption(get_the_ID());
+    $content = get_the_content();
+    echo '<div class="mar:vrt+0.5">' . "\n";
+    echo '<!--  fc:MEDIA -->' . "\n";
+    echo '<div class="fx:r fxa:3 fxb:1 fxc:3 wd:100%">' . "\n";
+    echo wp_get_attachment_image(get_the_ID() , 'full', false, array('alt'=>$seo));
+    echo '</div>' . "\n";
+    echo '<!-- /fc:MEDIA -->' . "\n";
+    if (!empty($caption))
+    {
+        echo '<div class="aln:text-center fnt:size-smaller mar:bottom+0.25 wd:100%">' . wp_get_attachment_caption(get_the_ID()) . '</div>' . "\n";
+    }
+    if (!empty($content))
+    {
+    echo get_the_content() . "\n";
+    }
+    echo '</div>' . "\n";
+    echo '</div>' . "\n";
+    echo '<!-- /ct:BODY -->' . "\n";
+
+    /*
+    ***
+        * page footer
+    ***
+    */
+    /* file date */
+    $footer_items = '';
+    $footer_items = get_the_date(get_option('date_format'),get_the_ID()) . '|';
+    /*: dimensions :*/
+    $footer_items .= 'Dimensions: ' . absint($wp_metadata['height']) . 'x' . absint($wp_metadata['width']) . ' pixels |';
+    $ratio = round(absint($wp_metadata['height']) / absint($wp_metadata['width']) , 4);
+    switch ($ratio)
     {
         case 1:
-            $v_aspect_ratio = '1x1';
+            $aspect_ratio = '1x1';
         break;
         case 0.75:
-            $v_aspect_ratio = '4x3';
+            $aspect_ratio = '4x3';
         break;
         case 0.6667:
-            $v_aspect_ratio = '6x4 (classic film)';
+            $aspect_ratio = '6x4 (classic film)';
         break;
         case 0.7146:
-            $v_aspect_ratio = '7x5';
+            $aspect_ratio = '7x5';
         break;
         case 0.625:
-            $v_aspect_ratio = '16x10';
+            $aspect_ratio = '16x10';
         break;
         case 0.5625:
-            $v_aspect_ratio = '16x9 (high definition)';
+            $aspect_ratio = '16x9 (high definition)';
         break;
         case 0.4281:
-            $v_aspect_ratio = '21x9 (cinemascope)';
+            $aspect_ratio = '21x9 (cinemascope)';
         break;
         default:
-            $v_aspect_ratio = '';
+            $aspect_ratio = '';
     }
-    echo '<!-- xwpt: flexbox/content/container/item   -->' . "\n";
-    echo '<article class="fx:cn-ct-itm fx:ct-itm-opt fx:shadow mar:right+0.5 pad:+2 wd:100%">' . "\n";
-    echo '<h1 class="wcag:fg-grey9">' . $v_title . '</h1>' . "\n";
-    echo '<p><span class="fnt:weight-normal">Dimensions:</span> ' . absint($wp_metadata['width']) . '&times;' . absint($wp_metadata['height']) . 'px' . "\n";
-    if (!empty($v_aspect_ratio))
+    if (!empty($aspect_ratio))
     {
-        echo '<p><span class="fnt:weight-normal">Aspect Ratio:</span> ' . $v_aspect_ratio . '</p>' . "\n";
+        $footer_items .= 'Aspect Ratio: ' . $aspect_ratio . '|';
     }
-    echo '<p>&nbsp;</p>' . "\n";
-    echo '<div class="wd:100%">' . "\n";
-    echo '<figure class="entry-attm wp-block-image">' . "\n";
-    /*
-        Filter the default Xidipity image attm size
-        @param string $image_size Image size. Default = 'large'
-    */
-    $image_size = apply_filters('Xidipity_attm_size', 'full');
-    $wp_image = wp_get_attachment_image(get_the_ID() , $image_size);
-    echo wp_get_attachment_image(get_the_ID() , $image_size) . "\n";
-    echo '<figcaption class="wp-caption-text">' . wp_get_attachment_caption(get_the_ID()) . '</figcaption>' . "\n";
-    echo '</figure>' . "\n";
-    echo get_the_content() . "\n";
-    echo '</div>' . "\n";
-    echo '<div class="wd:100%">' . "\n";
-    if ($wp_metadata)
-    {
-        $v_meta_list .= dsp_view(esc_url(wp_get_attachment_url())) . '|';
-    }
-    /*
-        show on login
-    */
-    if (get_edit_post_link())
-    {
-        $v_meta_list .= dsp_edit(get_edit_post_link()) . '|';
-    }
-    /*
-        attm parent
-    */
-    if (!empty($wp_attm_title)) {
-
-        $v_meta_list .= '<span class="pad:right+0.5">Published in:</span>';
-        $v_meta_list .= $wp_attm_title_lnk . '|';
-    }
-    if (!empty($v_meta_list))
-    {
-        echo '<div class="pad:left+1 fnt:size-smaller prt(dsp:none)">' . "\n";
-        echo xidipity_metalinks(explode('|', $v_meta_list)) . "\n";
-        echo '</div>' . "\n";
-    }
-    echo '</div>' . "\n";
-    echo '</article>' . "\n";
-    /*
-        If comments are open or we have at least one comment, load up the comment template.
-    */
-    if (comments_open() || get_comments_number())
-    {
-        comments_template();
-    }
+    echo '<!--  ct:FOOTER -->' . "\n";
+    echo '<footer class="pad:left+1 fnt:size-smaller prt[dsp:none]">' . "\n";
+    echo xidipity_metalinks(explode('|', $footer_items)) . "\n";
+    echo '</footer>' . "\n";
+    echo '<!-- /ct:FOOTER -->' . "\n";
 }
-echo '</div>' . "\n";
-echo '</main>' . "\n";
-echo '<!-- /xwpt: 90920.1d/image/php              -->' . "\n";
+else
+{
+    get_template_part('template-parts/content', 'none');
+}
+echo '</article>' . "\n";
+echo '<!-- /ct:ARTICLE -->' . "\n";
+echo '</section>' . "\n";
+echo '<!-- /fi:1/SECTION 1 -->' . "\n";
+
 /*
-    display sidebar
+***
+    * function: get_sidebar
+    * descript: display sidebar
+    * ref: developer.wordpress.org/reference/functions/get_sidebar/
+***
 */
 get_sidebar();
+echo '</main>' . "\n";
+echo '<!-- /fc:MAIN -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /fi:3/HTML -->' . "\n";
+
 /*
-    reset post data
+***
+    * function: get_footer
+    * descript: display page footer
+    * ref: developer.wordpress.org/reference/functions/get_footer/
+***
 */
 get_footer();
 
 /*
+***
+    * function: wp_reset_postdata
+    * descript: reset database query
+    * ref: developer.wordpress.org/reference/functions/wp_reset_postdata/
+***
+*/
+wp_reset_postdata();
+
+/*
  * EOF:     image.php
- * Build:   200206
+ * Build:   200322
  *
  */
 ?>
