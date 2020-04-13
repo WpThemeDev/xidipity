03ff08bb37bde2d7db33f647975c8c8b596a62f1
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-12 20:21:01 -0400

    Update header.php

--- header.php
+++ header.php
@@ -2,61 +2,95 @@
 /*
  * WordPress Xidipity Theme PHP File
  *
- * File Name:       header.php
- * Function:        display page header
- * Build:           200315
- * GitHub:          https://github.com/WpThemeDev/xidipity/
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
+ *  File Name:      header.php
+ *  Function:       display page header
+ *  Build:          200322
+ *  GitHub:         github.com/WpThemeDev/xidipity/
+ *  License URI:    gnu.org/licenses/gpl-3.0.txt
+ *  Author:         John Baer
+ *  Copyright:      2019-2020 John Baer
+ *  License:        GPL-3.0-or-later
+ *  Ref:            developer.wordpress.org/themes/basics/
  *
- * @package         xidipity
- * @author          John Baer
- * @copyright       2019-2020 John Baer
- * @license         GPL-3.0-or-later
- * @version         1.0
- * @since           0.9
- * @link            https://developer.wordpress.org/themes/basics/
+ *  Comments:       page/flex/container is the root
+ *                      - header components are children of page/flex/item 1
+ *                      - menu components are children of page/flex/item 2
  *
- */
-
+**/
 echo '<!doctype html>' . "\n";
 ?>
 <html <?php
 language_attributes(); ?> >
 <?php
-echo '<!-- xwpt: 91001.1a/header/php              -->' . "\n";
 echo '<head>' . "\n";
 echo '<meta charset="' . get_bloginfo('charset') . '">' . "\n";
 echo '<meta name="viewport" content="width=device-width, initial-scale=1">' . "\n";
 echo '<link rel="profile" href="http://gmpg.org/xfn/11">' . "\n";
+
 /*
-    load wordpress meta
+***
+    * functon: wp_head
+    * dsc: load wordpress meta
+    * ver: 200322
+    * fnt: core wordpress functionality
+    * ref:
+***
 */
 do_action('wp_head');
 
+/*
+***
+    * functon: favicon image
+    * dsc: set site favorite icon
+    * ver: 200322
+    * fnt: set site favorite icon from xidipity-cfg
+    * ref:
+***
+*/
+if (XWT_FAV_ICO !== 'none')
+{
+	echo '<link rel="icon" href="' . XWT_FAV_ICO . '" sizes="32x32" />' . "\n";
+	echo '<link rel="pple-touch-icon-precomposed" href="' . XWT_FAV_ICO . '" sizes="32x32" />' . "\n";
+	echo '<link rel="icon" href="' . XWT_FAV_ICO . '" sizes="192x192" />' . "\n";
+	echo '<meta name="msapplication-TileImage" content="' . XWT_FAV_ICO . '" />' . "\n";
+}
+else
+{
+    echo '<!--  err:###MISSING FAV-ICON DECLARATION### -->' . "\n";
+}
+
+/*
+***
+    * functon: hdr image
+    * dsc: header background image
+    * ver: 200322
+    * fnt: set background image url
+    * ref:
+***
+*/
 if (XWT_HDR_IMG !== 'none')
 {
-    echo '<!-- xwpt: header/background/image          -->' . "\n";
+    echo '<!--  img:HEADER/BACKGROUND -->' . "\n";
     echo '<style type="text/css">' . "\n";
-    echo '.fx\:ct-itm-1-opt {' . "\n";
+    echo '.hdr\:bg-img {' . "\n";
     echo 'background-image: url("' . XWT_HDR_IMG . '");' . "\n";
-    echo 'background-repeat: no-repeat;' . "\n";
-    echo 'background-position: center center;' . "\n";
-    echo '-webkit-background-size: cover;' . "\n";
-    echo '-moz-background-size: cover;' . "\n";
-    echo '-o-background-size: cover;' . "\n";
-    echo 'background-size: cover;' . "\n";
-    echo 'height: auto;' . "\n";
-    echo 'width: 100%;' . "\n";
     echo '}' . "\n";
     echo '</style>' . "\n";
-    echo '<!-- /xwpt: header/background/image         -->' . "\n";
+    echo '<!-- /img:HEADER/BACKGROUND -->' . "\n";
 }
+
 /*
-    emoji support
+***
+    * functon: emoji
+    * dsc: emoji style
+    * ver: 200322
+    * fnt: create emoji styles
+    * ref:
+***
 */
+echo '<!--  emoji:HEADER/STYLE -->' . "\n";
 if (XWT_EMOJI_DSP == 'yes')
 {
-    echo '<!-- xwpt: wp/emoji/image                   -->' . "\n";
     echo '<style type="text/css">' . "\n";
     echo 'img.wp-smiley,' . "\n";
     echo 'img.emoji {' . "\n";
@@ -71,104 +105,143 @@ if (XWT_EMOJI_DSP == 'yes')
     echo 'padding: 0 !important;' . "\n";
     echo '}' . "\n";
     echo '</style>' . "\n";
-    echo '<!-- /xwpt: wp/emoji/image                  -->' . "\n";
 }
 else
 {
-    echo '<!-- xwpt: wp/emoji/image                   -->' . "\n";
     echo '<style type="text/css">' . "\n";
     echo 'img.wp-smiley,' . "\n";
     echo 'img.emoji {' . "\n";
     echo 'display: none !important;' . "\n";
     echo '}' . "\n";
     echo '</style>' . "\n";
-    echo '<!-- /xwpt: wp/emoji/image                  -->' . "\n";
 }
+echo '<!-- /emoji:HEADER/STYLE -->' . "\n";
 echo '</head>' . "\n";
 echo '<body class="' . implode(' ', get_body_class()) . '">' . "\n";
-echo '<!-- xwpt: flexbox/page/container           -->' . "\n";
-echo '<div class="fx:pg-ct">' . "\n";
-echo '<!-- xwpt: 91001.1a/header/php              -->' . "\n";
-echo '<!-- xwpt: flexbox/page/container/item-1    -->' . "\n";
-if (XWT_SIDEBAR_ALIGN == 'left')
-{
-    echo '<header class="fx:pg-ct-itm-sbl fx:ct-itm-1-align">' . "\n";
-}
-else
-{
-    echo '<header class="fx:pg-ct-itm-sbr fx:ct-itm-1-align">' . "\n";
+/*
+***/
+echo '<!--  file:header.php -->' . "\n";
+/***
+*/
+/*
+***
+    * open htm/flex/container here / close in footer
+***
+*/
+echo '<!--  fc:HTML -->' . "\n";
+echo '<div class="fx:c fxa:1 fxb:1 fxc:1 pad:hrz+0.5 vw:100">' . "\n";
+/*
+***
+    * element: header
+    * dsc: page header
+    * bld: 200322
+    * fnt: display header as configured
+    * ref:
+***
+*/
+echo '<!--  fi:1/HTML -->' . "\n";
+echo '<div class="fxd:2 fxe:6 wd:100%">' . "\n";
+echo '<!--  fc:HEADER -->' . "\n";
+if (XWT_HDR_ALIGN == 'left') {
+    echo '<header class="fx:r fxa:1 fxc:3 hdr:bg-img wd:100%" style="min-height:' . XWT_HDR_HGT . ';">' . "\n";
+} elseif (XWT_HDR_ALIGN == 'right') {
+    echo '<header class="fx:r fxa:2 fxc:3 hdr:bg-img wd:100%" style="min-height:' . XWT_HDR_HGT . ';">' . "\n";
+} else {
+    echo '<header class="fx:r fxa:3 fxc:3 hdr:bg-img wd:100%" style="min-height:' . XWT_HDR_HGT . ';">' . "\n";
 }
 echo '<a class="skip-link screen-reader-text" href="#content">' . __('Skip to content') . '</a>' . "\n";
-echo '<div class="fx:ct-itm-1-opt" style="min-height: ' . XWT_HDR_HGT . '">' . "\n";
-
-/*
-    header management
-*/
-echo '<div class="fx:area-ct-' . XWT_HDR_ALIGN . '">' . "\n";
+echo '<!--  ct:HEADER -->' . "\n";
 if (XWT_HDR_LOGO == 'none')
 {
-    echo '<div class="fx:area-itm-' . XWT_HDR_ALIGN .  '">' . "\n";
     if (is_front_page() || is_home())
     {
-        echo '<p class="site-title">' . get_bloginfo('name') . '</p>' . "\n";
+        if (XWT_HDR_ALIGN == 'left') {
+            echo '<div class="aln:text-left dsp:block">' . "\n";
+            echo '<p class="hdr:title">' . get_bloginfo('name') . '</p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        } elseif (XWT_HDR_ALIGN == 'right') {
+            echo '<div class="aln:text-right dsp:block">' . "\n";
+            echo '<p class="hdr:title">' . get_bloginfo('name') . '</p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        } else {
+            echo '<div class="aln:text-center dsp:block">' . "\n";
+            echo '<p class="hdr:title">' . get_bloginfo('name') . '</p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        }
     }
     else
     {
-        echo '<p><a class="site-title" href="' . esc_url(home_url('/')) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></p>' . "\n";
+        if (XWT_HDR_ALIGN == 'left') {
+            echo '<div class="aln:text-left dsp:block">' . "\n";
+            echo '<p><a class="hdr:title" href="' . esc_url(home_url('/')) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        } elseif (XWT_HDR_ALIGN == 'right') {
+            echo '<div class="aln:text-right dsp:block">' . "\n";
+            echo '<p><a class="hdr:title" href="' . esc_url(home_url('/')) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        } else {
+            echo '<div class="aln:text-center dsp:block">' . "\n";
+            echo '<p><a class="hdr:title" href="' . esc_url(home_url('/')) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></p>' . "\n";
+            echo '<p class="hdr:desc">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+            echo '</div>' . "\n";
+        }
     }
-    echo '<p class="site-description">' . get_bloginfo( 'description' ) . '</p>' . "\n";
+}
+else
+{
+    echo '<div class="dsp:block">' . "\n";
+    if (is_front_page() || is_home())
+    {
+        echo '<img src="' . XWT_HDR_LOGO . '" alt="' . get_bloginfo('name') . ' Theme">' . "\n";
+    }
+    else
+    {
+        echo '<a href="' . esc_url(home_url('/')) . '" rel="home"><img src="' . XWT_HDR_LOGO . '" alt="' . get_bloginfo('name') . '"></a>' . "\n";
+    }
     echo '</div>' . "\n";
 }
-else
-{
-    if (is_front_page() || is_home())
-    {
-        echo '<img class="fx:area-ct-itm" src="' . XWT_HDR_LOGO . '" alt="' . get_bloginfo('name') . ' Theme">' . "\n";
-    }
-    else
-    {
-        echo '<a href="' . esc_url(home_url('/')) . '" rel="home"><img class="fx:area-ct-itm" src="' . XWT_HDR_LOGO . '" alt="' . get_bloginfo('name') . '"></a>' . "\n";
-    }
-}
-echo '</div>' . "\n";
-echo '</div>' . "\n";
+echo '<!-- /ct:HEADER -->' . "\n";
 echo '</header>' . "\n";
-echo '<!-- /xwpt: 91001.1a/header/php             -->' . "\n";
+echo '<!-- /fc:HEADER -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /fi:1/HTML -->' . "\n";
 
 /*
-    menu exceptions
-*/
-echo '<!-- xwpt: 90828.1a/header/php/nav          -->' . "\n";
-echo '<!-- xwpt: flexbox/page/container/item-2    -->' . "\n";
-if ( disp_menu() == 'no' )
-{
-    if (XWT_SIDEBAR_ALIGN == 'left')
-    {
-        echo '<div class="fx:pg-ct-itm-sbl dsp:none">' . "\n";
-    }
-    else
-    {
-        echo '<div class="fx:pg-ct-itm-sbr dsp:none">' . "\n";
-    }
-}
-else
-{
-    if (XWT_SIDEBAR_ALIGN == 'left')
-    {
-        echo '<div class="fx:pg-ct-itm-sbl">' . "\n";
-    }
-    else
-    {
-        echo '<div class="fx:pg-ct-itm-sbr">' . "\n";
-    }
-}
-
-/*
-    primary menu
+***
+    * element: menu
+    * dsc: page menu
+    * ver: 200322
+    * fnt: display menu as configured
+    * ref: primary menu
+***
 */
 $wp_page = get_page_by_path('xidipity-toc');
 $wp_menus = get_nav_menu_locations();
 $wp_menu = (abs($wp_menus['primary']) !== 0);
+echo '<!--  fi:2/HTML -->' . "\n";
+if (disp_menu() == 'no')
+{
+    echo '<div class="dsp:none fxd:2 fxe:6 wd:100%">' . "\n";
+}
+else
+{
+    echo '<div class="fxd:2 fxe:6 wd:100% prt[dsp:none]">' . "\n";
+}
+echo '<!--  fc:NAV -->' . "\n";
+if (XWT_MNU_ALIGN == 'left') {
+    echo '<nav class="fx:r fxa:1 fxc:3 ht:auto sm)mar:vrt+0.25 wd:100%">' . "\n";
+} elseif (XWT_MNU_ALIGN == 'right') {
+    echo '<nav class="fx:r fxa:2 fxc:3 ht:auto sm)mar:vrt+0.25 wd:100%">' . "\n";
+} else {
+    echo '<nav class="fx:r fxa:3 fxc:3 ht:auto sm)mar:vrt+0.25 wd:100%">' . "\n";
+}
+echo '<!--  ct:NAV -->' . "\n";
+echo '<div role="navigation" id="nav" class="dsp:block" style="width: ' . XWT_MENU_WIDTH . '">' . "\n";
 if ($wp_menu || $wp_page)
 {
     if ($wp_page)
@@ -176,41 +249,34 @@ if ($wp_menu || $wp_page)
         /*: xidipity toc page :*/
         if ($wp_page->ID == get_queried_object_id())
         {
-            echo '<div class="mnu:hb-' . XWT_MNU_ALIGN . '">' . "\n";
-            echo '<a class="toc-btn" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
-            echo '</div>' . "\n";
+            echo '<a class="toc:btn led:wide" href="' . home_url('/') . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
         else
         {
-            echo '<div class="mnu:hb-' . XWT_MNU_ALIGN . '">' . "\n";
-            echo '<a class="toc-btn" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
-            echo '</div>' . "\n";
+            echo '<a class="toc:btn led:wide" href="' . get_permalink($wp_page->ID) . '"><i class="fas fa-bars"></i></a>' . "\n";
         }
     }
     else
     {
-        /*: drop down menu :*/
-        echo '<div class="fx:area-ct-' . XWT_MNU_ALIGN . '">' . "\n";
-        echo '<div class="fx:area-itm-' . XWT_MNU_ALIGN . ' fx:mnu-' . XWT_MENU_WIDTH . '">' . "\n";
-        echo '<nav role="navigation" id="nav">' . "\n";
-        echo '<input class="trigger" type="checkbox" id="mainNavButton">' . "\n";
-        echo '<label for="mainNavButton" onclick><i class="fas fa-bars"></i></label>' . "\n";
+        echo '<input class="trigger" type="checkbox" id="mainNavButton" name="hm">' . "\n";
+        echo '<label for="mainNavButton" class="led:wide toc:hamburger" onclick><i class="fas fa-bars"></i></label>' . "\n";
         wp_nav_menu(array(
             'theme_location' => 'primary',
             'menu_id' => 'primary-menu',
             'container' => ''
         ));
-        echo '</nav>' . "\n";
-        echo '</div>' . "\n";
-        echo '</div>' . "\n";
     }
 }
 echo '</div>' . "\n";
-echo '<!-- /xwpt: 91001.1a/header/php/nav         -->' . "\n";
+echo '<!-- /ct:NAV -->' . "\n";
+echo '</nav>' . "\n";
+echo '<!-- /fc:NAV -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /fi:2/HTML -->' . "\n";
 
 /*
  * EOF:     header.php
- * Build:   200315
+ * Build:   200322
  *
  */
 ?>
