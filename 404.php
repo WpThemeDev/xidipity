e602f4e5df6d82da192135ae5fc3c586e1a54edb
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-12 20:09:34 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
  *
  * File Name:       404.php
  * Function:        display 404 page
- * Build:           200128-1
+ * Build:           200322
  * GitHub:          https://github.com/WpThemeDev/xidipity/
  * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
  *
@@ -19,59 +19,106 @@
  */
 
 /*
-    set page options
+***
+    * display menus
+***
 */
-disp_sidebar('yes');
-disp_menu('no');
+disp_menu('yes');
+
 /*
-    system variables
-*/
-global $wp_query;
-/*: current pagination number :*/
-$wp_paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-/*: posts per page :*/
-$wp_ppp = get_option('posts_per_page');
-/*
-    local variables
-*/
-$v_cur_page = 0;
-$v_meta_list = '';
-$v_pages = 0;
-/*
-    display header
+***
+    * function: get_header
+    * descript: display page header
+    * ref: developer.wordpress.org/reference/functions/get_header/
+***
 */
 get_header();
-echo '<!-- xwpt: 90927.1a/page/php                -->' . "\n";
-echo '<!-- xwpt: flexbox/page/container/item-3    -->' . "\n";
+
+/*
+***/
+echo '<!--  file:page.php -->' . "\n";
+/***
+
+*/
+echo '<!--  fi:3/HTML -->' . "\n";
+echo '<div class="fxd:2 fxe:6 wd:100%">' . "\n";
+echo '<!--  fc:MAIN -->' . "\n";
+
+/*
+***
+    * align sidebar
+***
+*/
 if (XWT_SIDEBAR_ALIGN == 'left')
 {
-    echo '<main class="fx:pg-ct-itm-sbl">' . "\n";
+    echo '<main class="fx:c md)fx:r-rev fxa:1 fxb:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+    echo '<!--  fi:1/SECTION -->' . "\n";
+    echo '<section class="fxd:2 fxe:6 mar:bottom+0.5 md)mar:left+0.5 wd:100%">' . "\n";
 }
 else
 {
-    echo '<main class="fx:pg-ct-itm-sbr">' . "\n";
+    echo '<main class="fx:c md)fx:r fxa:1 fxb:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+    echo '<!--  fi:1/SECTION -->' . "\n";
+    echo '<section class="fxd:2 fxe:6 mar:bottom+0.5 md)mar:right+0.5 wd:100%">' . "\n";
 }
-echo '<div class="fx:cn-ct">' . "\n";
-get_template_part('template-parts/content', 'none');
-echo '</div>' . "\n";
-echo '</main>' . "\n";
-echo '<!-- /xwpt: 90927.1a/page/php               -->' . "\n";
+
+echo '<!--  ct:ARTICLE -->' . "\n";
+echo '<article class="box:shadow bg:content fg:content dsp:block pad:hrz+1 ht:min10 wd:100%">' . "\n";
+
+echo '<!--  ct:BODY -->' . "\n";
+echo '<div class="bg:content ht:min10 mar:bottom+0.5 wd:100%">' . "\n";
+
 /*
-    display sidebar
+***
+    * function: the_content none
+    * descript: display 404 page
+    * ref:
+***
+*/
+get_template_part('template-parts/content', 'none');
+
+echo '</div>' . "\n";
+echo '<!-- /ct:BODY -->' . "\n";
+
+echo '</article>' . "\n";
+echo '<!-- /ct:ARTICLE -->' . "\n";
+echo '</section>' . "\n";
+echo '<!-- /fi:1/SECTION 1 -->' . "\n";
+
+/*
+***
+    * function: get_sidebar
+    * descript: display sidebar
+    * ref: developer.wordpress.org/reference/functions/get_sidebar/
+***
 */
 get_sidebar();
+echo '</main>' . "\n";
+echo '<!-- /fc:MAIN -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /fi:3/HTML -->' . "\n";
+
 /*
-    reset post data
-*/
-wp_reset_postdata();
-/*
-    display footer
+***
+    * function: get_footer
+    * descript: display page footer
+    * ref: developer.wordpress.org/reference/functions/get_footer/
+***
 */
 get_footer();
 
 /*
+***
+    * function: wp_reset_postdata
+    * descript: reset database query
+    * ref: developer.wordpress.org/reference/functions/wp_reset_postdata/
+***
+*/
+wp_reset_postdata();
+
+/*
  * EOF:     404.php
- * Build:   200128-1
+ * Build:   200322
  *
  */
 ?>
