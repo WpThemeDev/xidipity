4f1b1f04e29d5159d03f27161d4819a70850d613
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-24 11:08:57 -0400

    Update content-none.php
    svg update

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -22,27 +22,29 @@
 echo '<!--  file:content-none.php -->' . "\n";
 /***
 */
-echo '<!--  fc:NONE -->' . "\n";
-echo '<div class="fx:c sm)fx:r fxa:1 fxb:1 fxc:1 mar:vrt+0.5">' . "\n";
-echo '<!--  fi:LOGO -->' . "\n";
-echo '<div class="fxd:3 wd:100% pad:vrt+0.5 sm)wd:25%">' . "\n";
-echo xidipity_404_logo() . "\n";
+echo '<!--  fc:PG-ERR -->' . "\n";
+echo '<div class="fx:rw sm)fx:r fxa:1 fxb:1 fxc:1 bg:bas-100 cnr:arch-small mar:vrt+0.5 pad:vrt+1">' . "\n";
+echo '<!--  fc:LOGO -->' . "\n";
+echo '<div class="fx:r fxa:3 fxb:1 fxc:3 wd:100% sm)wd:6">' . "\n";
+echo '<svg class="ht:5 wd:5" viewBox="0 0 24 24">
+<path fill="var(--hue-pri-dark)" d="M15,15.5A2.5,2.5 0 0,1 12.5,18A2.5,2.5 0 0,1 10,15.5V13.75A0.75,0.75 0 0,1 10.75,13A0.75,0.75 0 0,1 11.5,13.75V15.5A1,1 0 0,0 12.5,16.5A1,1 0 0,0 13.5,15.5V11.89C12.63,11.61 12,10.87 12,10C12,8.9 13,8 14.25,8C15.5,8 16.5,8.9 16.5,10C16.5,10.87 15.87,11.61 15,11.89V15.5M8.25,8C9.5,8 10.5,8.9 10.5,10C10.5,10.87 9.87,11.61 9,11.89V17.25A3.25,3.25 0 0,0 12.25,20.5A3.25,3.25 0 0,0 15.5,17.25V13.75A0.75,0.75 0 0,1 16.25,13A0.75,0.75 0 0,1 17,13.75V17.25A4.75,4.75 0 0,1 12.25,22A4.75,4.75 0 0,1 7.5,17.25V11.89C6.63,11.61 6,10.87 6,10C6,8.9 7,8 8.25,8M10.06,6.13L9.63,7.59C9.22,7.37 8.75,7.25 8.25,7.25C7.34,7.25 6.53,7.65 6.03,8.27L4.83,7.37C5.46,6.57 6.41,6 7.5,5.81V5.75A3.75,3.75 0 0,1 11.25,2A3.75,3.75 0 0,1 15,5.75V5.81C16.09,6 17.04,6.57 17.67,7.37L16.47,8.27C15.97,7.65 15.16,7.25 14.25,7.25C13.75,7.25 13.28,7.37 12.87,7.59L12.44,6.13C12.77,6 13.13,5.87 13.5,5.81V5.75C13.5,4.5 12.5,3.5 11.25,3.5C10,3.5 9,4.5 9,5.75V5.81C9.37,5.87 9.73,6 10.06,6.13M14.25,9.25C13.7,9.25 13.25,9.59 13.25,10C13.25,10.41 13.7,10.75 14.25,10.75C14.8,10.75 15.25,10.41 15.25,10C15.25,9.59 14.8,9.25 14.25,9.25M8.25,9.25C7.7,9.25 7.25,9.59 7.25,10C7.25,10.41 7.7,10.75 8.25,10.75C8.8,10.75 9.25,10.41 9.25,10C9.25,9.59 8.8,9.25 8.25,9.25Z" /></svg>' . "\n";
 echo '</div>' . "\n";
-echo '<!-- /fi:LOGO -->' . "\n";
-echo '<!--  fi:MESSAGE -->' . "\n";
-echo '<div class="fxd:1 fxe:6 wd:100% sm)pad:+1 sm)wd:75%">' . "\n";
-echo '<h2>System Page Error</h2>' . "\n";
-echo '<p>The page you are looking for can not be found.</p>' . "\n";
+echo '<!-- /fc:LOGO -->' . "\n";
+echo '<!--  fc:MESSAGE -->' . "\n";
+echo '<div class="fx:rw fxa:1 fxb:1 fxc:1 wd:100%">' . "\n";
+echo '<h2 class="aln:text-center sm)aln:text-left wd:100%">Page Error</h2>' . "\n";
+echo '<p class="aln:text-center sm)aln:text-left wd:100%">There was an error loading the requested page.</p>' . "\n";
+echo '<!--  fc:BUTTON -->' . "\n";
+echo '<div class="fx:r fxa:3 sm)fxa:1 fxb:1 fxc:1 wd:100%">' . "\n";
 echo '<div class="aln:text-center sm)aln:text-left mar:top+1">' . "\n";
-echo '<button onclick="goBack()">Return to previous page</button>' . "\n";
+echo '<button onclick="javascript:history.back()">Return to previous page</button>' . "\n";
 echo '</div>' . "\n";
-echo '<script>' . "\n";
-echo 'function goBack() {window.history.back();}' . "\n";
-echo '</script>' . "\n";
 echo '</div>' . "\n";
-echo '<!--  fi:MESSAGE -->' . "\n";
+echo '<!-- /fc:BUTTON -->' . "\n";
 echo '</div>' . "\n";
-echo '<!-- /fc:NONE -->' . "\n";
+echo '<!--  fc:MESSAGE -->' . "\n";
+echo '</div>' . "\n";
+echo '<!-- /fc:PG-ERR -->' . "\n";
 
 /*
  * EOF:     template-parts/content-none.php
