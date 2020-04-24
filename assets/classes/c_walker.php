501456e3d5c5dc4f604177f9b333d478b495c823
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-23 20:41:32 -0400

    Create c_walker.php

---
+++ assets/classes/c_walker.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *  name: c_walker
+ *  build: 200422
+ *  description: category walker extension to support font awesome icons
+ *  doc: developer.wordpress.org/reference/classes/walker/
+ *
+ */
+if ( ! class_exists( 'c_walker' ) )
+{
+    class c_walker extends Walker
+    {
+        public $tree_type = 'category';
+        public $db_fields = array(
+            'parent' => 'parent',
+            'id' => 'term_id'
+        );
+        public function start_lvl(&$output, $depth = 0, $args = array())
+        {
+            $output .= '<ul class="' . $args['class'] . '">' . "\n";
+        }
+        public function end_lvl(&$output, $depth = 0, $args = array())
+        {
+            $output .= '</ul>' . "\n";
+        }
+        public function start_el(&$output, $category, $depth = 0, $args = array() , $current_object_id = 0)
+        {
+            $output .= '<li>' . $args['link_before'] . $category->name . "\n";
+        }
+        public function end_el(&$output, $category, $depth = 0, $args = array())
+        {
+            $output .= $args['link_after'] . '</li>' . "\n";
+        }
+    }
+}
+
+/*
+ * EOF:     c_walker.php
+ * Build:   200422
+ *
+ */
+?>
