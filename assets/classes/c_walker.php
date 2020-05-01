006aea6d6e2bde717741ad7b66e529294613e6ff
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-04-30 20:44:26 -0400

    Update c_walker.php
    - update cat name to cat link

--- assets/classes/c_walker.php
+++ assets/classes/c_walker.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *  name: c_walker
- *  build: 200422
+ *  build: 200501
  *  description: category walker extension to support font awesome icons
  *  doc: developer.wordpress.org/reference/classes/walker/
  *
@@ -25,18 +25,18 @@ if ( ! class_exists( 'c_walker' ) )
         }
         public function start_el(&$output, $category, $depth = 0, $args = array() , $current_object_id = 0)
         {
-            $output .= '<li>' . $args['link_before'] . $category->name . "\n";
+            $output .= '<li><a href="' . get_category_link( $category->term_id ) . '">' . $args['link_before'] . $category->name . "\n";
         }
         public function end_el(&$output, $category, $depth = 0, $args = array())
         {
-            $output .= $args['link_after'] . '</li>' . "\n";
+            $output .= $args['link_after'] . '</a></li>' . "\n";
         }
     }
 }
 
 /*
  * EOF:     c_walker.php
- * Build:   200422
+ * Build:   200501
  *
  */
 ?>
