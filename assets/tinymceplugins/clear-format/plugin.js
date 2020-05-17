57c196c22dc24afbc7581e2c72ee6e5600ceb0e3
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-17 14:12:14 -0400

    Update plugin.js
    - update header comments / footer ID

--- assets/tinymceplugins/clear-format/plugin.js
+++ assets/tinymceplugins/clear-format/plugin.js
@@ -1,21 +1,13 @@
-/*
- * WordPress Xidipity Theme JS File
- *
- * File Name:       clear-format/plugin.js
- * Function:        tinymce set format to default plugin
- * Build:           200104-1
- * GitHub:          https://github.com/WpThemeDev/xidipity/
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
- *
- * @package         xidipity
- * @author          John Baer
- * @copyright       2019-2020 John Baer
- * @license         GPL-3.0-or-later
- * @version         1.0
- * @since           0.9
- * @link            https://www.tiny.cloud/docs/
- *
- */
+/**
+    * WordPress Xidipity Theme
+    * Tinymce clear-format plugin 
+    *
+    * ###:  plugin.js
+    * bld:  24200520
+    * src:  github.com/WpThemeDev/xidipity/
+    * (C)   2019-2020 John Baer
+    *
+*/
 
 tinymce.PluginManager.add('clear_format', function( editor, url ) {
     editor.addButton( 'clear_format', {
@@ -27,7 +19,5 @@ tinymce.PluginManager.add('clear_format', function( editor, url ) {
 });
 
 /*
- * EOF:     clear-format/plugin.js
- * Build:   200104-1
- *
+ * EOF: clear-format / plugin.js / 24200520
  */
