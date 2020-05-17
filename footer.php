d7ad67fc2ddaea95ca41596164eaf908c392d149
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-17 09:40:03 -0400

    Update footer.php
    - update header comments / footer ID / bas classes

--- footer.php
+++ footer.php
@@ -1,22 +1,14 @@
 <?php
-/*
- * WordPress Xidipity Theme PHP File
- *
- * File Name:       footer.php
- * Function:        display page footer
- * Build:           200415
- * GitHub:          github.com/WpThemeDev/xidipity/
- * License URI:     www.gnu.org/licenses/gpl-3.0.txt
- *
- * @package         xidipity
- * @author          John Baer
- * @copyright       2019-2020 John Baer
- * @license         GPL-3.0-or-later
- * @version         1.0
- * @since           0.9
- * @link            developer.wordpress.org/themes/basics/
- *
- */
+/**
+    * WordPress Xidipity Theme
+    * The template for displaying page footer
+    *
+    * ###:  footer.php
+    * bld:  24200520
+    * src:  github.com/WpThemeDev/xidipity/
+    * (C)   2019-2020 John Baer
+    *
+*/
 
 /*
 ***/
@@ -70,8 +62,6 @@ echo '</body>' . "\n";
 echo '</html>' . "\n";
 
 /*
- * EOF:     footer.php
- * Build:   200415
- *
+ * EOF: footer.php / 24200520
  */
 ?>
