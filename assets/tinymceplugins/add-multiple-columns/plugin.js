62aa7bcf2efe941927c3733227ec2b2cb463ca8a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-17 12:45:16 -0400

    Update plugin.js
    - update header comments / footer ID

--- assets/tinymceplugins/add-multiple-columns/plugin.js
+++ assets/tinymceplugins/add-multiple-columns/plugin.js
@@ -1,21 +1,13 @@
-/*
- * WordPress Xidipity Theme JS File
- *
- * File Name:       add-multiple-columns/plugin.js
- * Function:        tinymce multi column plugin
- * Build:           200104-1
- * GitHub:          https://github.com/WpThemeDev/xidipity/
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
- *
- * @package         xidipity
- * @author          John Baer
- * @copyright       2019-2020 John Baer
- * @license         GPL-3.0-or-later
- * @version      	1.0
- * @since      		0.9
- * @link            https://www.tiny.cloud/docs/
- *
- */
+/**
+    * WordPress Xidipity Theme
+    * Tinymce add-multiple-columns plugin 
+    *
+    * ###:  plugin.js
+    * bld:  24200520
+    * src:  github.com/WpThemeDev/xidipity/
+    * (C)   2019-2020 John Baer
+    *
+*/
 
 tinymce.PluginManager.add('add_multi_cols', function (editor, url) {
     editor.addButton('add_multi_cols', {
@@ -101,7 +93,5 @@ tinymce.PluginManager.add('add_multi_cols', function (editor, url) {
 });
 
 /*
- * EOF:     add-multiple-columns/plugin.js
- * Build:   200104-1
- *
+ * EOF: add-multiple-columns / plugin.js / 24200520
  */
