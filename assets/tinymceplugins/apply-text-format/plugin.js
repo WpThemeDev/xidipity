f7e54ba8e696750549e1db91fe2e517128b0355b
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-20 20:49:30 -0400

    Update plugin.js
    - update underline

--- assets/tinymceplugins/apply-text-format/plugin.js
+++ assets/tinymceplugins/apply-text-format/plugin.js
@@ -1,21 +1,13 @@
-/*
- * WordPress Xidipity Theme JS File
- *
- * File Name:       apply-text-format/plugin.js
- * Function:        tinymce text format plugin
- * Build:           200104-1
- * GitHub:          https://github.com/WpThemeDev/xidipity/
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
- *
- * @package         xidipity
- * @author          John Baer
- * @copyright       2019-2020 John Baer
- * @license         GPL-3.0-or-later
- * @version      	1.0
- * @since      		0.9
- * @link            https://www.tiny.cloud/docs/
- *
- */
+/**
+    * WordPress Xidipity Theme
+    * Tinymce apply-text-format plugin
+    *
+    * ###:  plugin.js
+    * bld:  24200520
+    * src:  github.com/WpThemeDev/xidipity/
+    * (C)   2019-2020 John Baer
+    *
+*/
 
 tinymce.PluginManager.add('apply_txt_formats', function(editor, url) {
     editor.addButton('apply_txt_formats', {
@@ -24,13 +16,13 @@ tinymce.PluginManager.add('apply_txt_formats', function(editor, url) {
     icon: false,
         image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PGRlZnM+PHBhdGggaWQ9ImEiIGQ9Ik0yNCAyNEgwVjBoMjR2MjR6Ii8+PC9kZWZzPjxjbGlwUGF0aCBpZD0iYiI+PHVzZSB4bGluazpocmVmPSIjYSIgb3ZlcmZsb3c9InZpc2libGUiLz48L2NsaXBQYXRoPjxwYXRoIGNsaXAtcGF0aD0idXJsKCNiKSIgZD0iTTIuNSA0djNoNXYxMmgzVjdoNVY0aC0xM3ptMTkgNWgtOXYzaDN2N2gzdi03aDNWOXoiLz48L3N2Zz4=',
         onclick: function() {
-            editor.execCommand('underline');
+            editor.execCommand('mceReplaceContent', false, '<u>{$selection}</u>');
         },
         menu: [{
             icon: false,
             text: '•\xa0Underline',
             onclick: function() {
-                editor.execCommand('underline');
+                editor.execCommand('mceReplaceContent', false, '<u>{$selection}</u>');
             }
         }, {
             icon: false,
@@ -52,6 +44,12 @@ tinymce.PluginManager.add('apply_txt_formats', function(editor, url) {
             }
         }, {
             icon: false,
+            text: '•\xa0Strong',
+            onclick: function() {
+                editor.execCommand('mceReplaceContent', false, '<strong>{$selection}</strong>');
+            }
+        }, {
+            icon: false,
             text: '•\xa0Drop Shadow',
             onclick: function() {
                 tinymce.execCommand('mceReplaceContent', false, '<span class="fnt:shadow-medium">{$selection}</span>');
@@ -59,6 +57,7 @@ tinymce.PluginManager.add('apply_txt_formats', function(editor, url) {
         }, ],
     });
 });
-/**
- *  eof: apply-text-format/plugin.js
+
+/*
+ * EOF: apply-text-format / plugin.js / 24200520
  */
