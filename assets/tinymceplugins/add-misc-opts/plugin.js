1dd9d942b50c0dfec099f4fbe545c8e2c5d3900b
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-23 18:41:56 -0400

    Update plugin.js
    - update content frame

--- assets/tinymceplugins/add-misc-opts/plugin.js
+++ assets/tinymceplugins/add-misc-opts/plugin.js
@@ -3,7 +3,7 @@
     * Tinymce add-misc-opts plugin 
     *
     * ###:  plugin.js
-    * bld:  24200520
+    * bld:  24200527
     * src:  github.com/WpThemeDev/xidipity/
     * (C)   2019-2020 John Baer
     *
@@ -26,9 +26,9 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
             }
         }, {
             icon: false,
-            text: '•\xa0Frame Text',
+            text: '•\xa0Content Frame',
             onclick: function () {
-                tinymce.execCommand('mceReplaceContent', false, '<table class="wd:100%"><tr><td class="txt:frame">{$selection}</td></tr></table>');
+                tinymce.execCommand('mceReplaceContent', false, '<!--  xwp:EDITOR/CONTENT/FRAME --><table class="frame"><tr><td>{$selection}</td></tr></table><!-- /xwp:EDITOR/CONTENT/FRAME -->');
             }
         }, {
             icon: false,
@@ -36,7 +36,7 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
             onclick: function() {
                 var dom = editor.dom;
                 var uniqueID = dom.uniqueId();
-                var html = '<table class="bdr:collapse mar:top-0 mar:bottom+0.5 wd:100% web[dsp:none]"><tr><td class="bdr:solid-thin bdr:bas-300 bg:tint cnr:arch-small fnt:size-small pad:+0.5" id="' + uniqueID + '"></td><td class="mce[dsp:none]"><!--more--></td></tr></table>';
+                var html = '<!--  xwp:EDITOR/EXCERPT --><table class="bdr:collapse mar:top-0 mar:bottom+0.5 wd:100% web[dsp:none]"><tr><td class="bdr:solid-thin bdr:bas-300 bg:tint cnr:arch-small fnt:size-small pad:+0.5" id="' + uniqueID + '"></td><td class="mce[dsp:none]"><!--more--></td></tr></table><!-- /xwp:EDITOR/EXCERPT -->';
                 editor.insertContent(html);
                 var newExcerpt = dom.select('p#' + uniqueID)[0];
                 editor.selection.setCursorLocation(newExcerpt);
@@ -52,5 +52,5 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
 });
 
 /*
- * EOF: add-misc-opts / plugin.js / 24200520
+ * EOF: add-misc-opts / plugin.js / 24200527
  */
