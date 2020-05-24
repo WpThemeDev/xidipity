8919300e16d99a00cd705ef1591da66993139591
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-23 20:39:48 -0400

    Update plugin.js
    - update tinymce reference

--- assets/tinymceplugins/apply-text-highlight/plugin.js
+++ assets/tinymceplugins/apply-text-highlight/plugin.js
@@ -1,9 +1,9 @@
 /**
     * WordPress Xidipity Theme
-    * Tinymce apply-text-highlight plugin 
+    * Tinymce apply-text-highlight plugin
     *
     * ###:  plugin.js
-    * bld:  24200520
+    * bld:  24200527
     * src:  github.com/WpThemeDev/xidipity/
     * (C)   2019-2020 John Baer
     *
@@ -15,11 +15,11 @@ tinymce.PluginManager.add('apply_txt_hilight', function (editor, url) {
         icon: false,
         image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE4LjUsMS4xNUMxNy45NywxLjE1IDE3LjQ2LDEuMzQgMTcuMDcsMS43M0wxMS4yNiw3LjU1TDE2LjkxLDEzLjJMMjIuNzMsNy4zOUMyMy41LDYuNjEgMjMuNSw1LjM1IDIyLjczLDQuNTZMMTkuODksMS43M0MxOS41LDEuMzQgMTksMS4xNSAxOC41LDEuMTVNMTAuMyw4LjVMNC4zNCwxNC40NkMzLjU2LDE1LjI0IDMuNTYsMTYuNSA0LjM2LDE3LjMxQzMuMTQsMTguNTQgMS45LDE5Ljc3IDAuNjcsMjFINi4zM0w3LjE5LDIwLjE0QzcuOTcsMjAuOSA5LjIyLDIwLjg5IDEwLDIwLjEyTDE1Ljk1LDE0LjE2IiAvPjwvc3ZnPg==',
         onclick: function () {
-            tinymce.execCommand('mceReplaceContent', false, '<ins>{$selection}</ins>');
+            editor.execCommand('mceReplaceContent', false, '<ins>{$selection}</ins>');
         }
     });
 });
 
 /*
- * EOF: apply-text-highlight / plugin.js / 24200520
+ * EOF: apply-text-highlight / plugin.js / 24200527
  */
