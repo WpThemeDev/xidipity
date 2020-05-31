6b62072e4e49429722c332015f62d5f25fd3b591
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-05-31 11:08:31 -0400

    Update plugin.js
    - add icons
    - add code option

--- assets/tinymceplugins/add-misc-opts/plugin.js
+++ assets/tinymceplugins/add-misc-opts/plugin.js
@@ -3,7 +3,7 @@
     * Tinymce add-misc-opts plugin
     *
     * ###:  plugin.js
-    * bld:  24200527
+    * bld:  24200531
     * src:  github.com/WpThemeDev/xidipity/
     * (C)   2019-2020 John Baer
     *
@@ -19,14 +19,16 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
             editor.execCommand('mceBlockQuote');
         },
         menu: [{
-            icon: false,
-            text: '•\xa0Block Quote',
+            icon: true,
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNNSAxN2gzbDItNFY3SDR2NmgzbC0yIDR6bTEwIDBoM2wyLTRWN2gtNnY2aDNsLTIgNHoiLz48L3N2Zz4=',
+            text: '\xa0Block Quote',
             onclick: function() {
                 editor.execCommand('mceBlockQuote');
             }
         }, {
             icon: false,
-            text: '•\xa0Content Frame',
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMyA1aDJWM2MtMS4xIDAtMiAuOS0yIDJ6bTAgOGgydi0ySDN2MnptNCA4aDJ2LTJIN3Yyek0zIDloMlY3SDN2MnptMTAtNmgtMnYyaDJWM3ptNiAwdjJoMmMwLTEuMS0uOS0yLTItMnpNNSAyMXYtMkgzYzAgMS4xLjkgMiAyIDJ6bS0yLTRoMnYtMkgzdjJ6TTkgM0g3djJoMlYzem0yIDE4aDJ2LTJoLTJ2MnptOC04aDJ2LTJoLTJ2MnptMCA4YzEuMSAwIDItLjkgMi0yaC0ydjJ6bTAtMTJoMlY3aC0ydjJ6bTAgOGgydi0yaC0ydjJ6bS00IDRoMnYtMmgtMnYyem0wLTE2aDJWM2gtMnYyek03IDE3aDEwVjdIN3YxMHptMi04aDZ2Nkg5Vjl6Ii8+PC9zdmc+',
+            text: '\xa0Content Frame',
             onclick: function () {
                 var seltxt = editor.selection.getContent({format : 'text'});
                 if (seltxt.length > 0)
@@ -37,24 +39,45 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
                         var uniqueID = dom.uniqueId();
                         var html = '<!--  xwp:EDITOR/CONTENT/FRAME --><table class="frame"><tr><td id="' + uniqueID + '">&nbsp;</td></tr></table><!-- /xwp:EDITOR/CONTENT/FRAME -->';
                         editor.insertContent(html);
-                        var newTag = dom.select('p#' + uniqueID)[0];
+                        var newTag = dom.select('td#' + uniqueID)[0];
                         editor.selection.setCursorLocation(newTag);
                     }
             }
         }, {
-            icon: false,
-            text: '•\xa0Excerpt',
+
+            icon: true,
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMFYweiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik05LjQgMTYuNkw0LjggMTJsNC42LTQuNkw4IDZsLTYgNiA2IDYgMS40LTEuNHptNS4yIDBsNC42LTQuNi00LjYtNC42TDE2IDZsNiA2LTYgNi0xLjQtMS40eiIvPjwvc3ZnPg==',
+            text: '\xa0Code',
+            onclick: function () {
+                var seltxt = editor.selection.getContent({format : 'text'});
+                if (seltxt.length > 0)
+                    {
+                        editor.execCommand('mceReplaceContent', false, '<code>{$selection}</code>');
+                    } else {
+                        var dom = editor.dom;
+                        var uniqueID = dom.uniqueId();
+                        var html = '<code id="' + uniqueID + '">&nbsp;</code>';
+                        editor.insertContent(html);
+                        var newTag = dom.select('code#' + uniqueID)[0];
+                        editor.selection.setCursorLocation(newTag);
+                    }
+            }
+        }, {
+            icon: true,
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMFYweiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0zIDE4aDEydi0ySDN2MnpNMyA2djJoMThWNkgzem0wIDdoMTh2LTJIM3YyeiIvPjwvc3ZnPg==',
+            text: '\xa0Insert Excerpt',
             onclick: function() {
                 var dom = editor.dom;
                 var uniqueID = dom.uniqueId();
                 var html = '<!--  xwp:EDITOR/EXCERPT --><table class="bdr:collapse mar:top-0 mar:bottom+0.5 wd:100% web[dsp:none]"><tr><td class="bdr:solid-thin bdr:bas-300 bg:tint cnr:arch-small fnt:size-small pad:+0.5" id="' + uniqueID + '"></td><td class="mce[dsp:none]"><!--more--></td></tr></table><!-- /xwp:EDITOR/EXCERPT -->';
                 editor.insertContent(html);
-                var newExcerpt = dom.select('p#' + uniqueID)[0];
+                var newExcerpt = dom.select('td#' + uniqueID)[0];
                 editor.selection.setCursorLocation(newExcerpt);
             }
         }, {
-            icon: false,
-            text: '•\xa0Acronym',
+            icon: true,
+            image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNNSAxN3YyaDE0di0ySDV6bTQuNS00LjJoNWwuOSAyLjJoMi4xTDEyLjc1IDRoLTEuNUw2LjUgMTVoMi4xbC45LTIuMnpNMTIgNS45OEwxMy44NyAxMWgtMy43NEwxMiA1Ljk4eiIvPjwvc3ZnPg==',
+            text: '\xa0Acronym',
             onclick: function () {
                 editor.execCommand('mceReplaceContent', false, '<abbr>{$selection}</abbr>');
             }
@@ -63,5 +86,5 @@ tinymce.PluginManager.add('add_misc_opts', function(editor, url) {
 });
 
 /*
- * EOF: add-misc-opts / plugin.js / 24200527
+ * EOF: add-misc-opts / plugin.js / 24200531
  */
