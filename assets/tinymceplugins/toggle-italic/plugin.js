887599801decbd6ea82376a8c1cff932c0d3e202
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-06-09 18:12:44 -0400

    Update plugin.js
    - https://beautifier.io/

--- assets/tinymceplugins/toggle-italic/plugin.js
+++ assets/tinymceplugins/toggle-italic/plugin.js
@@ -1,23 +1,23 @@
 /**
-    * WordPress Xidipity Theme
-    * Tinymce toggle-italic plugin 
-    *
-    * ###:  plugin.js
-    * bld:  24200520
-    * src:  github.com/WpThemeDev/xidipity/
-    * (C)   2019-2020 John Baer
-    *
-*/
-
-tinymce.PluginManager.add('toggle_italic', function( editor, url ) {
-    editor.addButton( 'toggle_italic', {
-        title: 'Toggle Italic',
-        icon: false,
-        image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTAgNHYzaDIuMjFsLTMuNDIgOEg2djNoOHYtM2gtMi4yMWwzLjQyLThIMThWNGgtOHoiLz48L3N2Zz4=',
-        cmd: 'Italic'
-    });
+ * WordPress Xidipity Theme
+ * Tinymce toggle-italic plugin 
+ *
+ * ###:  plugin.js
+ * bld:  27200615
+ * src:  github.com/WpThemeDev/xidipity/
+ * (C)   2019-2020 John Baer
+ *
+ */
+tinymce.PluginManager.add('toggle_italic', function(editor) {
+	'use strict';
+	editor.addButton('toggle_italic', {
+		title: 'Toggle Italic',
+		icon: false,
+		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTAgNHYzaDIuMjFsLTMuNDIgOEg2djNoOHYtM2gtMi4yMWwzLjQyLThIMThWNGgtOHoiLz48L3N2Zz4=',
+		cmd: 'Italic'
+	});
 });
 
 /*
- * EOF: toggle-italic / plugin.js / 24200520
+ * EOF: toggle-italic / plugin.js / 27200615
  */
