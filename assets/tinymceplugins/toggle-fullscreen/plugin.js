a8b725ed59eace1877763b53903be725f05e4b59
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-06-09 18:26:43 -0400

    Update plugin.js
    - update cmd

--- assets/tinymceplugins/toggle-fullscreen/plugin.js
+++ assets/tinymceplugins/toggle-fullscreen/plugin.js
@@ -15,7 +15,7 @@ tinymce.PluginManager.add('toggle_fullscreen', function(editor) {
 		icon: false,
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTcgN0g3Yy0yLjc2IDAtNSAyLjI0LTUgNXMyLjI0IDUgNSA1aDEwYzIuNzYgMCA1LTIuMjQgNS01cy0yLjI0LTUtNS01ek03IDE1Yy0xLjY2IDAtMy0xLjM0LTMtM3MxLjM0LTMgMy0zIDMgMS4zNCAzIDMtMS4zNCAzLTMgM3oiLz48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+PC9zdmc+',
 		onclick: function() {
-			editor.execCommand('mceFullscreen');
+			cmd: 'mceFullscreen';
 		}
 	});
 });
