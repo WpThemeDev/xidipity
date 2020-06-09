b265062812e852f5310e00d71c349e1f6fd2b4a8
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-06-09 17:53:28 -0400

    Update plugin.js
    - https://beautifier.io/

--- assets/tinymceplugins/add-vertical-space/plugin.js
+++ assets/tinymceplugins/add-vertical-space/plugin.js
@@ -1,69 +1,69 @@
 /**
-    * WordPress Xidipity Theme
-    * Tinymce add-vertical-space plugin 
-    *
-    * ###:  plugin.js
-    * bld:  24200520
-    * src:  github.com/WpThemeDev/xidipity/
-    * (C)   2019-2020 John Baer
-    *
-*/
-
-tinymce.PluginManager.add('add_vert_space', function(editor, url) {
-    editor.addButton('add_vert_space', {
-        title: '\xBD Vertical Space',
-        icon: false,
-        image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAg%0D%0AMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTSAz%0D%0AIDE5IEwgMjEgMTkgTCAyMSAxMyBMIDMgMTMgTCAzIDE5IFogTSAzIDExIEwgMjEgMTEgTCAyMSA5%0D%0AIEwgMyA5IEwgMyAxMSBaIE0gMyA1IEwgMyA3IEwgMjEgNyBMIDIxIDUgTCAzIDUgWiIgc3R5bGU9%0D%0AImZpbGw6IHJnYigzMywgMzMsIDMzKTsiLz4KPC9zdmc+',
-        type: 'splitbutton',
-        onclick: function() {
-            editor.insertContent('<p class="vs:1/2">&nbsp;</p>');
-        },
-        menu: [{
-            text: '•\xa0\xBD Line',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:1/2">&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa0\xBE Line',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:3/4">&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa01 Line',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p>&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa01\xBD Lines',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:1-1/2">&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa02 Lines',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:2">&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa02\xBD Lines',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:2-1/2">&nbsp;</p>');
-            }
-        }, {
-            text: '•\xa03 Lines',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('<p class="vs:3">&nbsp;</p>');
-            }
-        }]
-    });
+ * WordPress Xidipity Theme
+ * Tinymce add-vertical-space plugin 
+ *
+ * ###:  plugin.js
+ * bld:  27200615
+ * src:  github.com/WpThemeDev/xidipity/
+ * (C)   2019-2020 John Baer
+ *
+ */
+tinymce.PluginManager.add('add_vert_space', function(editor) {
+	'use strict';
+	editor.addButton('add_vert_space', {
+		title: '\xBD Vertical Space',
+		icon: false,
+		image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAg%0D%0AMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTSAz%0D%0AIDE5IEwgMjEgMTkgTCAyMSAxMyBMIDMgMTMgTCAzIDE5IFogTSAzIDExIEwgMjEgMTEgTCAyMSA5%0D%0AIEwgMyA5IEwgMyAxMSBaIE0gMyA1IEwgMyA3IEwgMjEgNyBMIDIxIDUgTCAzIDUgWiIgc3R5bGU9%0D%0AImZpbGw6IHJnYigzMywgMzMsIDMzKTsiLz4KPC9zdmc+',
+		type: 'splitbutton',
+		onclick: function() {
+			editor.insertContent('<p class="vs:1/2">&nbsp;</p>');
+		},
+		menu: [{
+			text: '•\xa0\xBD Line',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:1/2">&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa0\xBE Line',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:3/4">&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa01 Line',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p>&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa01\xBD Lines',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:1-1/2">&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa02 Lines',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:2">&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa02\xBD Lines',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:2-1/2">&nbsp;</p>');
+			}
+		}, {
+			text: '•\xa03 Lines',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('<p class="vs:3">&nbsp;</p>');
+			}
+		}]
+	});
 });
 
 /*
- * EOF: add-vertical-space / plugin.js / 24200520
+ * EOF: add-vertical-space / plugin.js / 27200615
  */
