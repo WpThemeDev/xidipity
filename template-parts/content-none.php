6c50b72b93985b4ff761414c2fde974f87c944ba
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-06 19:17:25 -0400

    Update content-none.php
    - compress code

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -16,18 +16,7 @@ echo '<!--  file:content-none.php -->' . "\n";
 /***
 */
 echo '<div class="bkg:bas+2 ln mar:top+0.5 mar:bottom+0.25">&#8203;</div>' . "\n";
-echo '<div class="fx:r fxa:1 fxc:3 bdr:left-solid-thick bdr:pri-2 bkg:tint-bas-1 cnr:arch-small">' . "\n";
-echo '<div class="fnt:size-7x-large pad:hrz+0.5"><i class="icon:attendant_solid"></i></div>' . "\n";
-echo '<div class="pad:right+0.5 pad:vrt+0.5 txt:bas-3">' . "\n";
-echo '<p class="fnt:family-serif fnt:size-2x-large sm)fnt:size-3x-large fnt:weight-medium wd:100%">Page Error</p>' . "\n";
-echo '<p>There was an error loading the requested page.</p>' . "\n";
-echo '<div class="mar:vrt+0.5 pad:vrt+0.5">' . "\n";
-echo '<!--  28200715:BUTTON -->' . "\n";
-echo '<button class="aln:text-center fnt:size-x-small sm)fnt-size-medium" onclick="javascript:history.back()">Previous page</button>' . "\n";
-echo '<!-- /28200715:BUTTON -->' . "\n";
-echo '</div>' . "\n";
-echo '</div>' . "\n";
-echo '</div>' . "\n";
+echo '<div class="fx:r fxa:1 fxc:3 bdr:left-solid-thick bdr:pri-2 bkg:tint-bas-1 cnr:arch-small"><div class="fnt:size-7x-large pad:hrz+0.5"><i class="icon:attendant_solid"></i></div><div class="pad:right+0.5 pad:vrt+0.5 txt:bas-3"><p class="fnt:family-serif fnt:size-2x-large sm)fnt:size-3x-large fnt:weight-medium wd:100%">Page Error</p><p>There was an error loading the requested page.</p><div class="mar:vrt+0.5 pad:vrt+0.5"><!--  28200715:BUTTON --><button class="aln:text-center fnt:size-x-small sm)fnt-size-medium" onclick="javascript:history.back()">Previous page</button><!-- /28200715:BUTTON --></div></div></div>' . "\n";
 echo '<div class="bkg:bas+2 ln mar:vrt+0.25">&#8203;</div>' . "\n";
 
 /*
