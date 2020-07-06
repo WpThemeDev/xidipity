03d5b93cdc8bd5ecac80d6e59c5a57119b24f441
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-05 20:36:02 -0400

    Update plugin.js
    - update icons

--- assets/tinymceplugins/add-vertical-space/plugin.js
+++ assets/tinymceplugins/add-vertical-space/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce add-vertical-space plugin 
  *
  * ###:  plugin.js
- * bld:  27200615
+ * bld:  28200715
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -14,7 +14,7 @@ tinymce.PluginManager.add('add_vert_space', function(editor) {
 		type: 'splitbutton',
 		title: 'Vertical Space',
 		icon: false,
-		image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE0LDhIMTFWMTRINlY4SDNMOC41LDJMMTQsOE0xNS41LDIyTDIxLDE2SDE4VjEwSDEzVjE2SDEwTDE1LjUsMjJaIiAvPjwvc3ZnPg==',
+		image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2ZXJzaW9uPSIxLjEiIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxwYXRoIGQ9Ik0gMTMuOTg1IDIgTCAxOS45NzYgOCBMIDE5Ljk3NiAxMi4xNyBDIDE5LjQ3NyAxMi4wNiAxOC45NzggMTIgMTguNDc4IDEyIEwgMTcuOTc5IDEyIEwgMTcuOTc5IDkgTCAxMi45ODcgOSBMIDEyLjk4NyA0IEwgNS45OTcgNCBMIDUuOTk3IDIwIEwgMTIuMTU4IDIwIEMgMTIuMzI4IDIwLjcyIDEyLjYxNyAyMS4zOSAxMi45ODcgMjIgTCA1Ljk5NyAyMiBDIDQuODk5IDIyIDQgMjEuMSA0IDIwIEwgNCA0IEMgNCAyLjkgNC44OTkgMiA1Ljk5NyAyIEwgMTMuOTg1IDIgWiIgc3R5bGU9IiIvPgogIDxwYXRoIGQ9Ik0gMTcuNzAyIDE0LjEyNCBMIDE2LjAzNSAxNC4xMjQgTCAxNi4wMzUgMTcuNDU3IEwgMTMuMjU2IDE3LjQ1NyBMIDEzLjI1NiAxNC4xMjQgTCAxMS41OSAxNC4xMjQgTCAxNC42NDUgMTAuNzkgTCAxNy43MDIgMTQuMTI0IE0gMTguNTM1IDIxLjkwMiBMIDIxLjU5IDE4LjU2OCBMIDE5LjkyMyAxOC41NjggTCAxOS45MjMgMTUuMjM1IEwgMTcuMTQ1IDE1LjIzNSBMIDE3LjE0NSAxOC41NjggTCAxNS40NzggMTguNTY4IEwgMTguNTM1IDIxLjkwMiBaIi8+Cjwvc3ZnPg==',
 		menu: [{
 			text: '•\xa0\xBD Line',
 			icon: false,
@@ -62,5 +62,5 @@ tinymce.PluginManager.add('add_vert_space', function(editor) {
 });
 
 /*
- * EOF: add-vertical-space / plugin.js / 27200615
+ * EOF: add-vertical-space / plugin.js / 28200715
  */
