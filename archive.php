90ce9991733863b81dcfaa8b55d8c587fc9c2e04
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-16 19:05:05 -0400

    Update and rename page-templates/archive.php to archive.php

---
+++ archive.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * WordPress Xidipity Theme
+ * The template for displaying archive blog posts
+ *
+ * ###:  archive.php
+ * bld:  28200715
+ * src:  github.com/WpThemeDev/xidipity/
+ * (C)   2019-2020 John Baer
+ *
+ */
+/*
+ *** setup database query
+*/
+global $wp_query;
+$wp_data = $wp_query;
+/*
+ *** set page options
+*/
+dsp_menu('yes');
+sup_msg('I was unable to load the archive page template.');
+/*
+ *** set pagination variables
+*/
+/*: current pagination number :*/
+$wp_paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+/*: posts per page :*/
+$wp_ppp = get_option('posts_per_page');
+/*: total pages :*/
+$wp_total = $wp_query->max_num_pages;
+/*
+ *** developer.wordpress.org/reference/functions/get_header/
+*/
+get_header();
+echo '<cmt name="begin">ARCHIVE/PHP</cmt>' . "\n";
+echo '<div class="fxd:3 fxe:2 fb:100%">' . "\n";
+/*
+ *** align sidebar
+*/
+if (XTY_SIDEBAR_ALIGN == 'left')
+{
+	echo '<main class="fx:rw md)fx:r-rev fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:left+0.5">' . "\n";
+}
+else
+{
+	echo '<main class="fx:rw md)fx:r fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:right+0.5">' . "\n";
+}
+/*
+ *** template-parts/content-precis/php ***
+*/
+include (locate_template('template-parts/content-precis.php', false, false));
+/*
+ *** developer.wordpress.org/reference/functions/get_sidebar/
+*/
+get_sidebar();
+echo '</main>' . "\n";
+echo '</div>' . "\n";
+echo '<cmt name="end">ARCHIVE/PHP</cmt>' . "\n";
+/*
+ *** developer.wordpress.org/reference/functions/get_footer/
+*/
+get_footer();
+/*
+ *** developer.wordpress.org/reference/functions/wp_reset_postdata/
+*/
+wp_reset_postdata();
+/*
+ * EOF: archive.php / 28200715
+*/
+?>
