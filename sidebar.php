d59a8308823f276081e6285cc94204d9276dc838
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-16 19:48:22 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,20 @@
 <?php
 /**
-    * WordPress Xidipity Theme
-    * The template for displaying sidebar
-    *
-    * ###:  sidebar.php
-    * bld:  24200520
-    * src:  github.com/WpThemeDev/xidipity/
-    * (C)   2019-2020 John Baer
-    *
-*/
-
-/*
-***/
-echo '<!--  file:sidebar.php -->' . "\n";
-/***
-*/
-
-/*
-***
-    * dynamic_sidebar
-***
-*/
-echo '<!--  fi:3/1/2/HTML -->' . "\n";
-echo '<section class="aln:text-left fxd:4 fxe:6 wd:0 fb:100% md)fb:20 prt[dsp:none]">' . "\n";
-dynamic_sidebar( 'sidebar-1' );
-echo '</section>' . "\n";
-echo '<!-- /fi:3/1/2/HTML -->' . "\n";
-
-/*
- * EOF: sidebar.php / 24200520
+ * WordPress Xidipity Theme
+ * The template for displaying sidebar
+ *
+ * ###:  sidebar.php
+ * bld:  28200715
+ * src:  github.com/WpThemeDev/xidipity/
+ * (C)   2019-2020 John Baer
+ *
  */
+echo '<cmt name="begin">SIDEBAR/PHP</cmt>' . "\n";
+echo '<section class="aln:text-left fxd:4 fxe:6 wd:0 fb:100% md)fb:20 prt[dsp:none]">' . "\n";
+dynamic_sidebar('sidebar-1');
+echo '</section>' . "\n";
+echo '<cmt name="end">SIDEBAR/PHP</cmt>' . "\n";
+/*
+ * EOF: sidebar.php / 28200715
+*/
 ?>
