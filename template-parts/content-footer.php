791f4fed097e949a903de764933231f22a588bea
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-17 07:21:00 -0400

    Update content-footer.php

--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -1,34 +1,33 @@
 <?php
 /**
-    * WordPress Xidipity Theme
-    * The template for displaying missing page error
-    *
-    * ###:  content-footer.php
-    * bld:  28200715
-    * src:  github.com/WpThemeDev/xidipity/
-    * (C)   2019-2020 John Baer
-    *
-*/
-/*
-***/
-echo '<!--  file:content-footer.php -->' . "\n";
-/***
-*/
-$footer_items = '';
-if (current_user_can( 'edit_pages' ))
+ * WordPress Xidipity Theme
+ * The template for displaying content footer
+ *
+ * ###:  content-footer.php
+ * bld:  28200715
+ * src:  github.com/WpThemeDev/xidipity/
+ * (C)   2019-2020 John Baer
+ *
+ */
+/*: current user :*/
+$current_user = xty_user();
+$data_item = array();
+if (!empty($current_user))
 {
-    $footer_items .= '<span class="fnt:size-larger pad:right+0.5"><i class="icon:doc_edit_solid"></i></span>' . '|';
-    $footer_items .= '<a href="' . get_edit_post_link() . '">Edit</a>' . '|';
-    $footer_items .= '^' . '|';
+	$data_item['ico_1'] = '<i class="icon:user_solid"></i>';
+	$data_item['txt_1'] = __($current_user);
+	$data_item['raw_1'] = '<div class="txt:bas pad:hrz+0.5">&#65372;</div>';
 }
 /*: today's date :*/
-$footer_items .= '<span class="fnt:size-larger pad:right+0.5"><i class="icon:calendar_today_outline"></i></span>' . '|';
-$footer_items .= git_date() . '|';
-echo '<!--  28200715:FOOTER -->' . "\n";
-echo git_content_footer(explode('|', $footer_items)) . "\n";
-echo '<!-- /28200715:FOOTER -->' . "\n";
-
+$data_item['ico_2'] = '<i class="icon:calendar_today_outline"></i>';
+$data_item['txt_2'] = xty_date();
+echo '<cmt name="begin">TEMPLATE-PARTS/CONTENT-FOOTER/PHP</cmt>' . "\n";
+echo '<div class="mar:vrt+0.25">' . "\n";
+echo '<div class="bkg:bas+2 ln"></div>' . "\n";
+echo xty_dsp_meta($data_item) . "\n";
+echo '</div>' . "\n";
+echo '<cmt name="end">TEMPLATE-PARTS/CONTENT-FOOTER/PHP</cmt>' . "\n";
 /*
  * EOF: content-footer.php / 27200615
- */
+*/
 ?>
