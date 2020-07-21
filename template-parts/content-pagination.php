ca3253716eeb4192ccaee14d0dd4920ccd778adf
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-21 14:41:15 -0400

    Update content-pagination.php

--- template-parts/content-pagination.php
+++ template-parts/content-pagination.php
@@ -3,18 +3,17 @@
  * WordPress Xidipity Theme
  * The template for displaying pagination controls
  *
- * ###:  content-pagination.php
- * bld:  28200715
+ * ###:  template-parts/content-pagination.php
+ * bld:  28200801
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
  */
-$current_page = max(1, get_query_var('paged'));
 $args = array(
 	'base' => get_pagenum_link(1) . '%_%',
-	'format' => '/page/%#%',
-	'total' => abs($wp_total) ,
-	'current' => abs($current_page) ,
+	'format' => '/page/%#%/',
+	'total' => $wp_total ,
+	'current' => $wp_paged ,
 	'show_all' => false,
 	'end_size' => 1,
 	'mid_size' => 2,
@@ -26,13 +25,13 @@ $args = array(
 	'add_fragment' => '',
 	'before_page_number' => '',
 	'after_page_number' => ''
-);
+);	
 echo '<cmt name="begin">TEMPLATE-PARTS/PAGINATION/PHP</cmt>' . "\n";
 echo '<div class="fx:r fxa:4 fxb:6 fxc:3">' . "\n";
 echo paginate_links($args);
 echo '</div>' . "\n";
 echo '<cmt name="end">TEMPLATE-PARTS/PAGINATION/PHP</cmt>' . "\n";
 /*
- * EOF: content-pagination.php / 28200715
+ * EOF: template-parts/content-pagination.php / 28200801
 */
 ?>
