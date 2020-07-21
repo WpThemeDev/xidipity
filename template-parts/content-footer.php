a36bb2ca90ff40f490a6387203f99dd379f3a9bd
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-21 14:36:01 -0400

    Update content-footer.php

--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -3,8 +3,8 @@
  * WordPress Xidipity Theme
  * The template for displaying content footer
  *
- * ###:  content-footer.php
- * bld:  28200715
+ * ###:  template-parts/content-footer.php
+ * bld:  28200801
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -16,7 +16,7 @@ if (!empty($current_user))
 {
 	$data_item['ico_1'] = '<i class="icon:user_solid"></i>';
 	$data_item['txt_1'] = __($current_user);
-	$data_item['raw_1'] = '<div class="txt:bas pad:hrz+0.5">&#65372;</div>';
+	$data_item['div_1'] = '&#65372;';
 }
 /*: today's date :*/
 $data_item['ico_2'] = '<i class="icon:calendar_today_outline"></i>';
@@ -28,6 +28,6 @@ echo xty_dsp_meta($data_item) . "\n";
 echo '</div>' . "\n";
 echo '<cmt name="end">TEMPLATE-PARTS/CONTENT-FOOTER/PHP</cmt>' . "\n";
 /*
- * EOF: content-footer.php / 27200615
+ * EOF: template-parts/content-footer.php / 28200801
 */
 ?>
