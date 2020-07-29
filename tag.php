f6ddb0ae25f0eb66f173f679b4307b94b79247f0
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-29 19:42:22 -0400

    Update tag.php

--- tag.php
+++ tag.php
@@ -17,14 +17,14 @@ $wp_data = $wp_query;
 /*
  *** set pagination variables
 */
-$wp_paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-$wp_ppp = get_option('posts_per_page');
-$wp_total = $wp_data->max_num_pages;
+$per_paged = get_option('posts_per_page');
+$cur_paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$tot_paged = $wp_data->max_num_pages;
 /*
  *** set page options
 */
 xty('mnu-dsp','yes');
-xty('msg','The catgory is missing or I was unable to load the tag page template.');
+xty('msg','Tag/php was unable to load the requested template.');
 /*
  *** developer.wordpress.org/reference/functions/get_header/
 */
@@ -45,7 +45,7 @@ else
 	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:right+0.5">' . "\n";
 }
 /*
- *** template-parts/content-precis/php ***
+ *** template-parts/content-precis/php
 */
 include (locate_template('template-parts/content-precis.php', false, false));
 /*
