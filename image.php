96ed251041528f5413cb4076817847d0f0d80e6c
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-07-29 19:37:07 -0400

    Update image.php

--- image.php
+++ image.php
@@ -13,10 +13,10 @@
  *** set page options
 */
 $aspect_ratio = '';
-$ratio        = 0;
-$seo          = 'Xidipity WordPress Theme Media Library Image';
-$wp_metadata  = wp_get_attachment_metadata();
-xty('mnu-dsp','yes');
+$ratio = 0;
+$seo = 'Xidipity WordPress Theme Media Library Image';
+$wp_metadata = wp_get_attachment_metadata();
+xty('mnu-dsp', 'yes');
 /*
  *** developer.wordpress.org/reference/functions/get_header/
 */
@@ -62,9 +62,9 @@ if ($wp_query->have_posts())
 	/*
 	 *** display image
 	*/
-	$caption      = wp_get_attachment_caption(get_the_ID());
+	$caption = wp_get_attachment_caption(get_the_ID());
 	$content_post = get_post(get_the_ID());
-	$content      = $content_post->post_content;
+	$content = $content_post->post_content;
 	echo '<div class="mar:vrt+2">' . "\n";
 	echo '<div class="fx:r fxa:3 fxb:1 fxc:3 wd:100%">' . "\n";
 	echo wp_get_attachment_image(get_the_ID() , 'full', false, array(
@@ -84,79 +84,39 @@ if ($wp_query->have_posts())
 	echo '</div>' . "\n";
 	echo '</div>' . "\n";
 	echo '<cmt name="end">PAGE/BODY/IMAGE</cmt>' . "\n";
-	echo '<div class="bkg:bas+2 ln mar:vrt+0.25 mce[dsp:none]"></div>' . "\n";
-	/*
-	 *** page footer
-	*/
-	/* file date */
-	$data_item = array();
-	$data_item['ico_1'] .= '<i class="icon:page_previous_solid"></i>';
-	$data_item['txt_1'] .= '<a href="javascript:history.back()">Go Back</a>';
-	$data_item['div_1'] .= '&#65372;';
-	$data_item['raw_1'] .= '<div class="fx:break sm)dsp:none"></div>';
-	/*: date :*/
-	$data_item['ico_2'] .= '<i class="icon:calendar_today_outline"></i>';
-	$data_item['txt_2'] .= get_the_date(get_option('date_format') , get_the_ID());
-	$data_item['div_2'] .= '&#65372;';
-	$data_item['raw_2'] .= '<div class="fx:break sm)dsp:none"></div>';
-	/*: dimensions :*/
-	$data_item['ico_3'] .= '<i class="icon:dimension_solid"></i>';
-	$data_item['txt_3'] .= absint($wp_metadata['width']) . 'x' . absint($wp_metadata['height']) . ' pixels';
-	$ratio        = round(absint($wp_metadata['height']) / absint($wp_metadata['width']) , 4);
-	switch ($ratio)
-	{
-		case 1:
-			$aspect_ratio = '1x1';
-		break;
-		case 0.75:
-			$aspect_ratio = '4x3';
-		break;
-		case 0.6667:
-			$aspect_ratio = '6x4 (classic film)';
-		break;
-		case 0.7146:
-			$aspect_ratio = '7x5';
-		break;
-		case 0.625:
-			$aspect_ratio = '16x10';
-		break;
-		case 0.5625:
-			$aspect_ratio = '16x9 (high definition)';
-		break;
-		case 0.4281:
-			$aspect_ratio = '21x9 (cinemascope)';
-		break;
-		default:
-			$aspect_ratio = '';
-	}
-	if (!empty($aspect_ratio))
-	{
-		$data_item['div_3'] .= '&#65372;';
-		$data_item['raw_3'] .= '<div class="fx:break sm)dsp:none"></div>';
-		$data_item['ico_4'] .= '<i class="icon:aspect_ratio_outline"></i>';
-		$data_item['txt_4'] .= $aspect_ratio;
-	}
-	$url = wp_get_attachment_url();
-	if (!empty($url))
-	{
-		$data_item['div_4'] .= '&#65372;';
-		$data_item['raw_4'] .= '<div class="fx:break sm)dsp:none"></div>';
-		$data_item['txt_5'] .= 'File Type: ' . substr($url, -3);
-	}
-	echo '<cmt name="begin">PAGE/BODY/CONTENT/FOOTER</cmt>' . "\n";
-	echo '<footer class="pad:left+0.5 fnt:size-smaller prt[dsp:none]">' . "\n";
-	echo '<div class="fnt:size-medium fnt:size-smaller pad:bottom+0.25">' . "\n";
-	echo xty_dsp_meta($data_item) . "\n";
-	echo '</div>' . "\n";
-	echo '</footer>' . "\n";
-	echo '<cmt name="end">PAGE/BODY/CONTENT/FOOTER</cmt>' . "\n";
 }
 else
 {
 	/*
-	 *** call support agent ***
+	 *** inc/templage-tags/xty_support_agent
 	*/
 	echo xty_support_agent('I was unable to load the image page template.');
+}
+/*
+ *** inc/templage-tags/xty_content_footer
+*/
+if (empty(xty_user()))
+{
+	echo xty_content_footer(array(
+		'back',
+		'today',
+		'img_size',
+		'img_ratio',
+		'print',
+		'file_type'
+	));
+}
+else
+{
+	echo xty_content_footer(array(
+		'back',
+		'user',
+		'today',
+		'img_size',
+		'img_ratio',
+		'print',
+		'file_type'
+	));
 }
 echo '</article>' . "\n";
 echo '</section>' . "\n";
