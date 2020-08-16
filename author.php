d94687c736c16d5e14a750a3be4780eee7b01ae8
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-16 19:41:33 -0400

    Update author.php

--- author.php
+++ author.php
@@ -4,7 +4,7 @@
  * The template for displaying author info
  *
  * ###:  author.php
- * bld:  28200801
+ * bld:  29200815
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -31,12 +31,12 @@ echo '<div class="fxd:3 fxe:2 fb:100%">' . "\n";
 if (xty('sb-aln') == 'left')
 {
 	echo '<main class="fx:rw md)fx:r-rev fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
-	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:left+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5 md)mar:lt+0.5">' . "\n";
 }
 else
 {
 	echo '<main class="fx:rw md)fx:r fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
-	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:right+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5 md)mar:rt+0.5">' . "\n";
 }
 echo '<article class="box:shadow bkg:content txt:content dsp:block pad:hrz+1 ht:min10 wd:100%">' . "\n";
 if (have_posts())
@@ -50,7 +50,7 @@ if (have_posts())
 	$avatar_link = esc_url(get_avatar_url(get_the_author_meta('user_email')));
 	if (empty($avatar_link))
 	{
-		$avatar__img = '<div class="fnt:size-4x-large pad:right+0.5"><i class="icon:user_solid"></i></div>';
+		$avatar__img = '<div class="fnt:size-4x-large pad:rt+0.5"><i class="icon:user_solid"></i></div>';
 	}
 	else
 	{
@@ -60,23 +60,23 @@ if (have_posts())
 	 *** developer.wordpress.org/reference/functions/the_title/
 	*/
 	echo '<cmt name="begin">PAGE/BODY/HEADER</cmt>' . "\n";
-	echo '<header class="mar:bottom+1 wd:100%">' . "\n";
+	echo '<header class="mar:bt+1 wd:100%">' . "\n";
 	echo '<div class="pg:title">Author: ' . __(get_the_author_meta('nickname')) . '</div>';
-	echo '<div class="bkg:bas+2 ln mar:bottom+0.75">&#8203;</div>' . "\n";
+	echo '<div class="bkg:bas+2 ln mar:bt+0.75">&#8203;</div>' . "\n";
 	echo '</header>' . "\n";
 	echo '<cmt name="end">PAGE/BODY/HEADER</cmt>' . "\n";
 	echo '<cmt name="begin">PAGE/BODY/CONTENT</cmt>' . "\n";
-	echo '<div class="bkg:content ht:min10 mar:bottom+0.5 wd:100%">' . "\n";
+	echo '<div class="bkg:content ht:min10 mar:bt+0.5 wd:100%">' . "\n";
 	/*
 	 *** content
 	*/
 	echo '<div class="fx:rw fxa:1 fxb:1 fxc:1 wd:100%">' . "\n";
 	echo '<section class="fxd:2 fxe:6 wd:100%">' . "\n";
 	echo '<div class="fx:c md)fx:r fxa:1 fxb:1 fxc:3 md)fxc:1 wd:100%">' . "\n";
-	echo '<div class="bkg:content fx:r fxa:3 fxb:1 fxc:3 pad:bottom+1 md)pad:bottom+0 wd:100% md)wd:10%">' . "\n";
+	echo '<div class="bkg:content fx:r fxa:3 fxb:1 fxc:3 pad:bt+1 md)pad:bt+0 wd:100% md)wd:10%">' . "\n";
 	echo $avatar__img;
 	echo '</div>' . "\n";
-	echo '<div class="bkg:content fxd:3 fxe:6 wd:100% md)pad:left+2 md)wd:90%">' . "\n";
+	echo '<div class="bkg:content fxd:3 fxe:6 wd:100% md)pad:lt+2 md)wd:90%">' . "\n";
 	echo $biography;
 	$qry_prms = array(
 		'author' => get_the_author_meta('ID') ,
@@ -88,7 +88,7 @@ if (have_posts())
 		'suppress_filters' => true
 	);
 	$resume_query = new WP_Query($qry_prms);
-	echo '<h4 class="mar:top+1">Recent Posts:</h4>' . "\n";
+	echo '<h4 class="mar:tp+1">Recent Posts:</h4>' . "\n";
 	echo '<ol class="li:qtr-spaced">' . "\n";
 	if ($resume_query->have_posts())
 	{
@@ -157,6 +157,6 @@ get_footer();
 */
 wp_reset_postdata();
 /*
- * EOF: author.php / 28200801
+ * EOF: author.php / 29200815
 */
 ?>
