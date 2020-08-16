dc2ae23725e88cfdbed26a324b347c1217d30fb4
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-16 19:56:39 -0400

    Update search.php

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
  * The template for displaying searchs results
  *
  * ###:  search.php
- * bld:  28200801
+ * bld:  29200815
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -65,12 +65,12 @@ echo '<div class="fxd:3 fxe:2 fb:100%">' . "\n";
 if (xty('sb-aln') == 'left')
 {
 	echo '<main class="fx:rw md)fx:r-rev fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
-	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:left+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5 md)mar:lt+0.5">' . "\n";
 }
 else
 {
 	echo '<main class="fx:rw md)fx:r fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
-	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5 md)mar:right+0.5">' . "\n";
+	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5 md)mar:rt+0.5">' . "\n";
 }
 /*
  *** template-parts/content-precis/php
@@ -92,6 +92,6 @@ get_footer();
 */
 wp_reset_postdata();
 /*
- * EOF: search.php / 28200801
+ * EOF: search.php / 29200815
 */
 ?>
