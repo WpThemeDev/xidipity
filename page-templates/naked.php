c51f79f5f95d57890415d7d1ed4a28033773a10a
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-16 20:08:11 -0400

    Update naked.php

--- page-templates/naked.php
+++ page-templates/naked.php
@@ -6,7 +6,7 @@
     * The template for displaying page without menu / page title / sidebar
     *
     * ###:  page-templates/naked.php
-    * bld:  28200801
+    * bld:  29200815
     * src:  github.com/WpThemeDev/xidipity/
     * (C)   2019-2020 John Baer
     *
@@ -27,14 +27,14 @@ echo '<cmt>### PAGE-TEMPLATE/NAKED/PHP ###</cmt>' . "\n";
 echo '<cmt name="begin">PAGE/BODY</cmt>' . "\n";
 echo '<div class="fxd:3 fxe:2 fb:100%">' . "\n";
 echo '<main class="fx:rw md)fx:r-rev fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
-echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bottom+0.5">' . "\n";
+echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5">' . "\n";
 echo '<article class="box:shadow bkg:content txt:content dsp:block pad:hrz+1 ht:min10 wd:100%">' . "\n";
 if (have_posts())
 {
 	the_post();
 	echo '<cmt name="end">PAGE/BODY/HEADER</cmt>' . "\n";
 	echo '<cmt name="begin">PAGE/BODY/CONTENT</cmt>' . "\n";
-	echo '<div class="bkg:content ht:min10 mar:bottom+0.5 wd:100%">' . "\n";
+	echo '<div class="bkg:content ht:min10 mar:bt+0.5 wd:100%">' . "\n";
 	echo xty_dsp_content();
 	echo '</div>' . "\n";
 	echo '<cmt name="end">PAGE/BODY/CONTENT</cmt>' . "\n";
@@ -60,6 +60,6 @@ get_footer();
 */
 wp_reset_postdata();
 /*
- * EOF: page-templates/naked.php / 28200801
+ * EOF: page-templates/naked.php / 29200815
  */
 ?>
