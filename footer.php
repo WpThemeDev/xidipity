298fd9d00bce4eeb75dfe3b449a6c72a3454c798
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-18 13:47:30 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  * The template for displaying page footer
  *
  * ###:  footer.php
- * bld:  28200801
+ * bld:  29200815
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -13,21 +13,18 @@ echo '<cmt name="begin">FOOTER/PHP</cmt>' . "\n";
 echo '<div class="fxd:1 fxe:2 fb:100%">' . "\n";
 if (xty('ftr-aln') == 'left')
 {
-	echo '<footer class="fx:r fxa:1 fxc:3 ht:min6 wd:100%">' . "\n";
-	echo '<!--  ct:FOOTER -->' . "\n";
-	echo '<div class="aln:text-left dsp:block fnt:size-smaller">' . "\n";
+	echo '<footer class="fx:r fxa:1 fxc:3 ht:min6 fb:100%">' . "\n";
+	echo '<div class="aln:txt-lt dsp:block fnt:size-smaller">' . "\n";
 }
 elseif (xty('ftr-aln') == 'right')
 {
-	echo '<footer class="fx:r fxa:2 fxc:3 ht:min6 wd:100%">' . "\n";
-	echo '<!--  ct:FOOTER -->' . "\n";
-	echo '<div class="aln:text-right dsp:block fnt:size-smaller">' . "\n";
+	echo '<footer class="fx:r fxa:2 fxc:3 ht:min6 fb:100%">' . "\n";
+	echo '<div class="aln:txt-rt dsp:block fnt:size-smaller">' . "\n";
 }
 else
 {
-	echo '<footer class="fx:r fxa:3 fxc:3 ht:min6 wd:100%">' . "\n";
-	echo '<!--  ct:FOOTER -->' . "\n";
-	echo '<div class="aln:text-center dsp:block fnt:size-smaller">' . "\n";
+	echo '<footer class="fx:r fxa:3 fxc:3 ht:min6 fb:100%">' . "\n";
+	echo '<div class="aln:txt-ct dsp:block fnt:size-smaller">' . "\n";
 }
 echo '<p>' . blog_copyright() . '</p>';
 echo '<p><a href="https://xidipity.com/">Xidipity Theme</a> &sdot; Powered by <a href="https://wordpress.org">WordPress</a>';
@@ -43,18 +40,12 @@ echo '<cmt name="end">FOOTER/PHP</cmt>' . "\n";
 echo '</div>' . "\n";
 echo '<cmt name="end">PAGE</cmt>' . "\n";
 /*
- ***
- * function: wp_footer
- * dsc: wordpress footer
- * ver: 200415
- * fnt: add wordpress footer code
- * ref: developer.wordpress.org/reference/functions/wp_footer/
- ***
+ *** ref: developer.wordpress.org/reference/functions/wp_footer/
 */
 wp_footer();
 echo '</body>' . "\n";
 echo '</html>' . "\n";
 /*
- * EOF: footer.php / 28200801
+ * EOF: footer.php / 29200815
 */
 ?>
