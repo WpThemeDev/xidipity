25d21e5eb932f0a0ce8234e5a842e745f5578b2f
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-18 20:57:51 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,17 +14,17 @@ echo '<div class="fxd:1 fxe:2 fb:100%">' . "\n";
 if (xty('ftr-aln') == 'left')
 {
 	echo '<footer class="fx:r fxa:1 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-lt dsp:block fnt:size-smaller">' . "\n";
+	echo '<div class="aln:txt-lt dsp:block fnt:siz-1">' . "\n";
 }
 elseif (xty('ftr-aln') == 'right')
 {
 	echo '<footer class="fx:r fxa:2 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-rt dsp:block fnt:size-smaller">' . "\n";
+	echo '<div class="aln:txt-rt dsp:block fnt:siz-1">' . "\n";
 }
 else
 {
 	echo '<footer class="fx:r fxa:3 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-ct dsp:block fnt:size-smaller">' . "\n";
+	echo '<div class="aln:txt-ct dsp:block fnt:siz-1">' . "\n";
 }
 echo '<p>' . blog_copyright() . '</p>';
 echo '<p><a href="https://xidipity.com/">Xidipity Theme</a> &sdot; Powered by <a href="https://wordpress.org">WordPress</a>';
