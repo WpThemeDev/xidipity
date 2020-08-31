f5935698b3ad90804c11aa7e3b91de5655d389df
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-08-31 19:44:25 -0400

    Update no-sidebar.php

--- page-templates/no-sidebar.php
+++ page-templates/no-sidebar.php
@@ -6,7 +6,7 @@
  * The template for displaying page without sidebar
  *
  * ###:  page-templates/no-sidebar.php
- * bld:  29200815
+ * bld:  29200901
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -42,7 +42,7 @@ if (have_posts())
 	echo '</header>' . "\n";
 	echo '<cmt name="end">PAGE/BODY/HEADER</cmt>' . "\n";
 	echo '<cmt name="begin">PAGE/BODY/CONTENT</cmt>' . "\n";
-	echo '<div class="bkg:content ht:min10 mar:bt+0.5 wd:100%">' . "\n";
+	echo '<div class="bkg:content dsp:flw-rt ht:min10 mar:bt+0.5 wd:100%">' . "\n";
 	/*
 	 *** yoast breadcrumbs plugin
 	*/
@@ -96,6 +96,6 @@ get_footer();
 */
 wp_reset_postdata();
 /*
- * EOF: page-templates/no-sidebar.php / 29200815
+ * EOF: page-templates/no-sidebar.php / 29200901
 */
 ?>
