ada6feb0ab67f2ff5ee1819a006d43fcd7e797d2
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-09-04 12:22:40 -0400

    Update plugin.js

--- assets/tinymceplugins/add-horizontal-rule/plugin.js
+++ assets/tinymceplugins/add-horizontal-rule/plugin.js
@@ -19,55 +19,75 @@ tinymce.PluginManager.add('add_horz_rule', function(editor) {
 			text: '•\xa0Single Narrow',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="aln:ct rul:1x wd:70%" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="aln:ct rul:1x wd:70%" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Single Wide',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="rul:1x" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="rul:1x" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Double Narrow',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="aln:ct rul:dbl wd:70%" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="aln:ct rul:dbl wd:70%" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Double Wide',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="rul:dbl" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="rul:dbl" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Gradient Narrow',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="aln:ct rul:grd wd:70%" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="aln:ct rul:grd wd:70%" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Gradient Wide',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<hr class="rul:grd" />');
+				var html = '<!--  TMPL:HR -->';
+				html += '<hr class="rul:grd" />';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Emblem Narrow',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<div class="fx:r fxa:3 fxc:3 fb:100%"><div class="fx:r fxa:2 fxc:3 fb:25% sm)fb:30%"><hr class="wd:100%" /></div><div class="fx:r fxa:3 fxc:3 mar:hrz+1.5 txt:bas-500 fnt:wgt-700 wd:3">/<span style="width:1px;"></span>/</div><div class="fx:r fxa:1 fxc:3 fb:25% sm)fb:30%"><hr class="wd:100%" /></div></div>');
+				var html = '<!--  TMPL:HR -->';
+				html += '<div class="fx:r fxa:3 fxc:3 fb:100%"><div class="fx:r fxa:2 fxc:3 fb:25% sm)fb:30%"><hr class="wd:100%" /></div><div class="fx:r fxa:3 fxc:3 mar:hrz+1.5 txt:bas-500 fnt:wgt-700 wd:3">/<span style="width:1px;"></span>/</div><div class="fx:r fxa:1 fxc:3 fb:25% sm)fb:30%"><hr class="wd:100%" /></div></div>';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}, {
 			text: '•\xa0Emblem Wide',
 			icon: false,
 			onclick: function() {
-				editor.insertContent('<div class="fx:r fxa:3 fxc:3 fb:100%"><div class="fx:r fxa:2 fxc:3 fb:50%"><hr class="wd:100%" /></div><div class="fx:r fxa:3 fxc:3 mar:hrz+1.5 txt:bas-500 fnt:wgt-700 wd:3">/<span style="width:1px;"></span>/</div><div class="fx:r fxa:1 fxc:3 fb:50%"><hr class="wd:100%" /></div></div>');
+				var html = '<!--  TMPL:HR -->';
+				html += '<div class="fx:r fxa:3 fxc:3 fb:100%"><div class="fx:r fxa:2 fxc:3 fb:50%"><hr class="wd:100%" /></div><div class="fx:r fxa:3 fxc:3 mar:hrz+1.5 txt:bas-500 fnt:wgt-700 wd:3">/<span style="width:1px;"></span>/</div><div class="fx:r fxa:1 fxc:3 fb:50%"><hr class="wd:100%" /></div></div>';
+				html += '<!-- /TMPL:HR -->';
+				editor.insertContent(html);
 			}
 		}],
-		onPostRender: function() {
-			// default
-			this.value = '<hr class="rul:2x" />';
-		}
 	});
 });
 
