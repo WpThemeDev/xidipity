fc867e51fdfb386b387dd7d614f2745a0e92ecb4
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-09-06 16:33:48 -0400

    Update plugin.js

--- assets/tinymceplugins/add-icon/plugin.js
+++ assets/tinymceplugins/add-icon/plugin.js
@@ -20,89 +20,91 @@ tinymce.PluginManager.add('add_icon', function (editor, url) {
 				title: 'Add Icon',
 				body: [{
 					type: "container",
-					html: '<form method="post" style="font-family:sans-serif;font-size:16px;"><table style="border-collapse:separate; border-spacing:16px; padding-right:16px;"><tbody style="border-color: transparent;"><tr><td style="width:33.3333%"><label for="ico_siz" style="line-height:200%;">Size:</label><br /><select id="sz_id" style="border:1px solid #e9e7e4;"><option>inherit</option><option>+1/2</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option><option>+6</option></select></td><td style="width:33.3333%"><label for="margin_lt" style="line-height:200%;">Margin left:</label><br /><select id="lt_id" style="border:1px solid #e9e7e4;"><option>none</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option></select></td><td style="width:33.3333%; text-align: right;"><div style="text-align: left;"><label for="margin_rt" style="line-height:200%;">Margin right:</label><br /><select id="rt_id" style="border:1px solid #e9e7e4;"><option>none</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option></select></div></td></tr><tr><td colspan="3"><label for="icon">Icon Tag:</label><br /><input type="text" id="tag_id" name="ico_tag" value="" style="font-family:monospace; border: 1px solid #e9e7e4; width:100%;"></td></tr></tbody></table></form>'
+					html: '<form method="post" style="font-family:sans-serif;font-size:16px;"><table style="border-collapse:separate; border-spacing:16px; padding-right:16px;"><tbody style="border-color: transparent;"><tr><td style="width:33.3333%"><label for="ico_siz" style="line-height:200%;">Size:</label><br /><select id="sz_id" style="border:1px solid #e9e7e4;"><option>inherit</option><option>+1/2</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option><option>+6</option></select></td><td style="width:33.3333%"><label for="margin_lt" style="line-height:200%;">Margin left:</label><br /><select id="lt_id" style="border:1px solid #e9e7e4;"><option>none</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option></select></td><td style="width:33.3333%; text-align: right;"><div style="text-align: left;"><label for="margin_rt" style="line-height:200%;">Margin right:</label><br /><select id="rt_id" style="border:1px solid #e9e7e4;"><option>none</option><option>+1</option><option>+2</option><option>+3</option><option>+4</option><option>+5</option></select></div></td></tr><tr><td colspan="3"><label for="icon">Icon Tag:</label><br /><input type="text" id="ic_id" name="ico_tag" value="" style="font-family:monospace; border: 1px solid #e9e7e4; width:100%;"></td></tr></tbody></table></form>'
 					}],
 				onSubmit: function () {
 					var sz_idx = document.getElementById("sz_id").selectedIndex;
 					var lt_idx = document.getElementById("lt_id").selectedIndex;
 					var rt_idx = document.getElementById("rt_id").selectedIndex;
-					var ic_tag = document.getElementById("tag_id").value;
-					
-					var ico_class = '';
-					switch (sz_idx) {
-						case 1:
-							ico_class = 'fnt:siz-md-1x';
-							break;
-						case 2:
-							ico_class = 'fnt:siz-lg';
-							break;
-						case 3:
-							ico_class = 'fnt:siz-lg-1x';
-							break;
-						case 4:
-							ico_class = 'fnt:siz-lg-2x';
-							break;
-						case 5:
-							ico_class = 'fnt:siz-lg-3x';
-							break;
-						case 6:
-							ico_class = 'fnt:siz-lg-4x';
-							break;
-						case 7:
-							ico_class = 'fnt:siz-lg-5x';
-							break;
-					}
-					var lt_class = '';
-					switch (lt_idx) {
-						case 1:
-							lt_class = 'mar:lt+0.125';
-							break;
-						case 2:
-							lt_class = 'mar:lt+0.25';
-							break;
-						case 3:
-							lt_class = 'mar:lt+0.5';
-							break;
-						case 4:
-							lt_class = 'mar:lt+0.75';
-							break;
-						case 5:
-							lt_class = 'mar:lt+1';
-							break;
-					}
-					var rt_class = '';
-					switch (rt_idx) {
-						case 1:
-							rt_class = 'mar:rt+0.125';
-							break;
-						case 2:
-							rt_class = 'mar:rt+0.25';
-							break;
-						case 3:
-							rt_class = 'mar:rt+0.5';
-							break;
-						case 4:
-							rt_class = 'mar:rt+0.75';
-							break;
-						case 5:
-							rt_class = 'mar:rt+1';
-							break;
-					}
-					if ((sz_idx + lt_idx + rt_idx) == 0) {
-						editor.insertContent(ic_tag, {format: 'raw'});
-					} else {
-						var html_class = ico_class;
-						html_class += ' ';
-						html_class += lt_class;
-						html_class += ' ';
-						html_class += rt_class;
-						var span_class = html_class.replace(/\s+/g,' ').trim();
-						var html = '<span class="';
-						html += span_class;
-						html += '">';
-						html += ic_tag;
-						html += '</span>';
-						editor.insertContent(html, {format: 'raw'});
+					var ic_tag = document.getElementById("ic_id").value;
+
+					if (ic_tag !== '') {					
+						var ico_class = '';
+						switch (sz_idx) {
+							case 1:
+								ico_class = 'fnt:siz-md-1x';
+								break;
+							case 2:
+								ico_class = 'fnt:siz-lg';
+								break;
+							case 3:
+								ico_class = 'fnt:siz-lg-1x';
+								break;
+							case 4:
+								ico_class = 'fnt:siz-lg-2x';
+								break;
+							case 5:
+								ico_class = 'fnt:siz-lg-3x';
+								break;
+							case 6:
+								ico_class = 'fnt:siz-lg-4x';
+								break;
+							case 7:
+								ico_class = 'fnt:siz-lg-5x';
+								break;
+						}
+						var lt_class = '';
+						switch (lt_idx) {
+							case 1:
+								lt_class = 'mar:lt+0.125';
+								break;
+							case 2:
+								lt_class = 'mar:lt+0.25';
+								break;
+							case 3:
+								lt_class = 'mar:lt+0.5';
+								break;
+							case 4:
+								lt_class = 'mar:lt+0.75';
+								break;
+							case 5:
+								lt_class = 'mar:lt+1';
+								break;
+						}
+						var rt_class = '';
+						switch (rt_idx) {
+							case 1:
+								rt_class = 'mar:rt+0.125';
+								break;
+							case 2:
+								rt_class = 'mar:rt+0.25';
+								break;
+							case 3:
+								rt_class = 'mar:rt+0.5';
+								break;
+							case 4:
+								rt_class = 'mar:rt+0.75';
+								break;
+							case 5:
+								rt_class = 'mar:rt+1';
+								break;
+						}
+						if ((sz_idx + lt_idx + rt_idx) == 0) {
+							editor.insertContent(ic_tag, {format: 'raw'});
+						} else {
+							var html_class = ico_class;
+							html_class += ' ';
+							html_class += lt_class;
+							html_class += ' ';
+							html_class += rt_class;
+							var span_class = html_class.replace(/\s+/g,' ').trim();
+							var html = '<span class="';
+							html += span_class;
+							html += '">';
+							html += ic_tag;
+							html += '</span>';
+							editor.insertContent(html, {format: 'raw'});
+						}						
 					}
 				}
 			});
