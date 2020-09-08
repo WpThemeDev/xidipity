8f26f1ff9aa6d832fc2a568461ac8fd9f1db2e68
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-09-07 20:25:14 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -486,9 +486,9 @@ function xidipity_scripts()
 	wp_enqueue_style('xidipity-md-icons', 'https://fonts.googleapis.com/icon?family=Material+Icons', array() , wp_get_theme()
 		->get('Version') , 'all');
 	/*: clipboard js :*/
-	wp_enqueue_script('xidipity-clipboardjs', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.1/clipboard.min.js', array() , '2.0.1', false);
+	//wp_enqueue_script('xidipity-clipboardjs', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.1/clipboard.min.js', array() , '2.0.1', false);
 	/*: sweet alert js :*/
-	wp_enqueue_script('xidipity-sweetalert', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js', array() , '2.1.2', false);
+	//wp_enqueue_script('xidipity-sweetalert', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js', array() , '2.1.2', false);
 	/*: comment reply ? :*/
 	if (is_singular() && comments_open() && get_option('thread_comments'))
 	{
