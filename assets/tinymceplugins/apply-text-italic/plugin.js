38385fee2b85b2e26ff2c3765d5085c0ea135750
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-09-20 07:37:38 -0400

    Update plugin.js

--- assets/tinymceplugins/apply-text-italic/plugin.js
+++ assets/tinymceplugins/apply-text-italic/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce apply-text-italic plugin 
  *
  * ###:  plugin.js
- * bld:  29200901
+ * bld:  29201000
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -14,9 +14,6 @@ tinymce.PluginManager.add('apply_txt_italic', function(editor) {
 	function setTag(argTAG) {
 		var mceNODE = editor.selection.getNode();
 		// selected contents
-		var textSELECTED = editor.selection.getContent({
-			format: 'text'
-		});
 		var textHTML = editor.selection.getContent({
 			format: 'html'
 		});
@@ -62,42 +59,6 @@ tinymce.PluginManager.add('apply_txt_italic', function(editor) {
 		editor.selection.setContent(htmlUPDATE);
 		editor.undoManager.add();
 		return;
-	}
-
-	function getTag(argTARGET, argSELECTOR) {
-		// arguments
-		// 	argTARGET		nodeTARGET
-		// 	argSELECTOR		node selector (style/class)
-		var retVAL = '';
-		if (argTARGET !== '') {
-			var idxLT = 0; // starting index
-			var idxRT = 0; // ending index
-			var qFLAG = false; // quote flag
-			switch (argSELECTOR) {
-				//case ('style'):
-				//	idxLT = argTARGET.indexOf('style');
-				//	break;
-				case ('class'):
-					idxLT = argTARGET.indexOf('class');
-					break;
-				default:
-					idxLT = 0;
-			}
-			if (idxLT > 0) {
-				qFLAG = false;
-				for (idxRT = idxLT; idxRT < argTARGET.length; idxRT++) {
-					if (argTARGET.substring(idxRT, idxRT + 1) == '"') {
-						if (qFLAG) {
-							break;
-						} else {
-							qFLAG = true;
-						}
-					}
-				}
-				retVAL = argTARGET.substring(idxLT, idxRT + 1);
-			}
-		}
-		return retVAL;
 	}
 
 	function clrMCE(argHTML) {
@@ -164,5 +125,5 @@ tinymce.PluginManager.add('apply_txt_italic', function(editor) {
 });
 
 /*
- * EOF: apply-text-italic / plugin.js / 29200901
+ * EOF: apply-text-italic / plugin.js / 29201000
  */
