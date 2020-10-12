c5e7e0ca3a8753a7eedefe6414c6ef6b04d2569e
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-10-12 11:10:24 -0400

    Update plugin.js

--- assets/tinymceplugins/apply-text-alignment/plugin.js
+++ assets/tinymceplugins/apply-text-alignment/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce apply-text-alignment plugin
  *
  * ###:  plugin.js
- * bld:  29201000
+ * bld:  30201018
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -12,215 +12,280 @@ tinymce.PluginManager.add('apply_txt_align', function(editor) {
 	'use strict';
 
 	function setTag(argTAG) {
-		var mceNODE = editor.selection.getNode();
-		var mceNAME = mceNODE.nodeName.toLowerCase();
-		// selected contents
-		var textHTML = editor.selection.getContent({
+		if (argTAG === undefined) {
+			argTAG = '';
+		}
+		// init ret val
+		var newHTML = '';
+		// selection html node
+		var htmlNODE = editor.selection.getNode();
+		// get selection node name
+		var selNODE = htmlNODE.nodeName.toLowerCase();
+		// selection content in html format
+		var selTXT = editor.selection.getContent({
 			format: 'html'
 		});
-		// node text
-		var textNODE = clrMCE(mceNODE.innerHTML);
-		// alignment should not be assigned to styling tags, use parent
-		if (mceNAME.match(/i|u|s|sup|sub|kbd|em|strong/g) !== null) {
-			var mcePARENT = mceNODE.parentNode;
-			mceNODE = mcePARENT;
-		}		
-		// node html
-		var htmlNODE = mceNODE.outerHTML;
-		if (textHTML == textNODE) {
-			// full node
-			var nodeTARGET = htmlNODE.substring(0, htmlNODE.indexOf('>') + 1); // selector (ie. <span>...</span>)
-			var htmlUPDATE = ''; // new html
-			var tagTARGET = ''; // tag selector
-			var tagUPDATE = ''; // updated tag
-			var htmlTEMP = ''; // bucket variable
-			switch (true) {
-				case (argTAG.match(/~/g) !== null):
-					if (nodeTARGET.match(/margin-left:/g) == null) {
-						switch (true) {
-							case (nodeTARGET.match(/style/g) !== null):
-								tagTARGET = getTag(nodeTARGET, 'style');
-								tagUPDATE = tagTARGET.slice(0, -1) + ' margin-left:2rem;"';
-								htmlUPDATE = htmlNODE.replace(tagTARGET, tagUPDATE);
-								break;
-							case (nodeTARGET.match(/class/g) !== null):
-								tagTARGET = getTag(nodeTARGET, 'class');
-								tagUPDATE = tagTARGET + ' style="margin-left:2rem;"';
-								htmlTEMP = htmlNODE.replace(tagTARGET, tagUPDATE);
-								htmlUPDATE = htmlTEMP;
-								break;
-							default:
-								tagTARGET = nodeTARGET.substring(0, nodeTARGET.indexOf('>') + 1);
-								tagUPDATE = tagTARGET.slice(0, -1) + ' style="margin-left:2rem;">';
-								htmlTEMP = htmlNODE.replace(tagTARGET, tagUPDATE);
-								htmlUPDATE = htmlTEMP;
-						}						
-					} else {
-						tagTARGET = nodeTARGET.substring(nodeTARGET.indexOf('margin-left:'),nodeTARGET.indexOf('rem',idxLT)+3);
-						var idxLT = tagTARGET.indexOf('margin-left:')+12;
-						var idxRT = tagTARGET.indexOf('rem',idxLT);
-						var numTARGET = tagTARGET.substring(idxLT,idxRT);
-						var numUPDATE = Number(tagTARGET.substring(idxLT,idxRT))+2;
-						tagUPDATE = tagTARGET.replace(numTARGET,numUPDATE.toString())
-						htmlUPDATE = htmlNODE.replace(tagTARGET, tagUPDATE);
-					}
-					break;
-				case (nodeTARGET.match(/class/g) !== null):
-					tagTARGET = getTag(nodeTARGET, 'class');
-					tagUPDATE = tagTARGET.slice(0, -1) + ' ' + argTAG + '"';
-					htmlUPDATE = htmlNODE.replace(tagTARGET, tagUPDATE);
-					break;
-				default:
-					tagTARGET = nodeTARGET.substring(0, nodeTARGET.indexOf('>') + 1);
-					tagUPDATE = tagTARGET.slice(0, -1) + ' class="' + argTAG + '">';
-					htmlUPDATE = htmlNODE.replace(tagTARGET, tagUPDATE);
-			}
-			// remove mce style data tags
-			htmlTEMP = clrMCE(htmlUPDATE);
-			htmlUPDATE = htmlTEMP;
-			mceNODE.remove();
-		} else {
-			// partical node
-			if (argTAG.match(/~/g) !== null) {
-				htmlUPDATE = argTAG.substring(0,argTAG.indexOf('~'));
-				htmlUPDATE += textHTML;
-				htmlUPDATE += argTAG.substring(argTAG.indexOf('~')+1);
-			} else {
-				htmlUPDATE = '<span class="' + argTAG + '">';
-				htmlUPDATE += textHTML;
-				htmlUPDATE += '</span>';
-			}
+		var selTMP = '';
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/gi))) {
+			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
+			selTXT = selTMP;
+			// select parent node
+			var tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p,span');
+			htmlNODE = tmpNODE;
+			// get new selection node name
+			selNODE = htmlNODE.nodeName.toLowerCase();
 		}
-		editor.selection.setContent(htmlUPDATE);
-		editor.undoManager.add();
+		selTMP = htmlNODE.innerHTML;
+		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
+		selTMP = editor.dom.getOuterHTML(htmlNODE);
+		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
+		// check for multiple paragraphs
+		var tagCNT = (selTXT.match(/<\/.*?>/gi) || []).length;
+		switch (true) {
+			case (tagCNT > 1):
+				// strip carrage returns
+				var selNEW = selTXT.replace(/(\r\n|\n|\r)/gm, '');
+				// delimit tags with comma
+				var tagDELIM = selNEW.replace(/(<\/(div|h[1-6]|p)>)(<(div|h[1-6]|p)>)/g, '$1,$3');
+				// add to array
+				var htmlARRAY = tagDELIM.split(',').filter(function (el) {
+					return el != '';
+				});
+				// loop through array
+				var idx;
+				var itmARRAY;
+				var spanNODE;
+				var tagALN;
+				var tagHTML;
+				var tagNEW;
+				var tagTMP;
+				for (idx in htmlARRAY) {
+					// get span item
+					itmARRAY = htmlARRAY[idx];
+					// node
+					spanNODE = itmARRAY.match(/\bp|h[1-6]|div/)[0];
+					// get tag
+					tagHTML = getTag(itmARRAY, spanNODE);
+					// check tag for class element
+					tagALN = getAln(tagHTML);
+					tagNEW = '';
+					if (isEmpty(tagALN)) {
+						if (isEmpty(tagHTML.match(/class/))) {
+							tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' class="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
+
+						} else {
+							tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
+							tagNEW = tidyCss(tagTMP);
+						}
+					} else {
+						tagNEW = tagHTML.replace(tagALN, argTAG);
+					}
+					newHTML += itmARRAY.replace(tagHTML, tagNEW);
+				}
+				break;
+			case (selNODE == 'span'):
+				// get span
+				var spanHTML = getSpan(selHTML);
+				// check tag for class element
+				tagALN = getAln(spanHTML);
+				var spanNEW = '';
+				if (isEmpty(tagALN)) {
+					// existing class without weight
+					var spanTMP = spanHTML.substring(0, spanHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
+					spanNEW = tidyCss(spanTMP);
+				} else {
+					// existing class with alignment
+					spanNEW = spanHTML.replace(tagALN, argTAG);
+				}
+				newHTML = selHTML.replace(spanHTML, spanNEW);
+				break;
+			case (selTXT == selFULL):
+				// get tag
+				tagHTML = getTag(selHTML, selNODE);
+				// check tag for class element
+				tagALN = getAln(tagHTML);
+				tagNEW = '';
+				if (isEmpty(tagALN)) {
+					if (isEmpty(tagHTML.match(/class/))) {
+						//
+						tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' class="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
+
+					} else {
+						tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
+						tagNEW = tidyCss(tagTMP);
+					}
+				} else {
+					tagNEW = tagHTML.replace(tagALN, argTAG);
+				}
+				newHTML = selHTML.replace(tagHTML, tagNEW);
+				break;
+			default:
+				newHTML = '<p class="' + argTAG + '">' + selTXT + '</p>';
+		}
+		if (!isEmpty(newHTML)) {
+			editor.execCommand('mceReplaceContent', false, newHTML);
+		}
 		return;
 	}
 
-	function getTag(argTARGET, argSELECTOR) {
-		// arguments
-		// 	argTARGET		nodeTARGET
-		// 	argSELECTOR		node selector (style/class)
-		var retVAL = '';
-		if (argTARGET !== '') {
-			var idxLT = 0; // starting index
-			var idxRT = 0; // ending index
-			var qFLAG = false; // quote flag
-			switch (argSELECTOR) {
-				//case ('style'):
-				//	idxLT = argTARGET.indexOf('style');
-				//	break;
-				case ('class'):
-					idxLT = argTARGET.indexOf('class');
-					break;
-				default:
-					idxLT = 0;
-			}
-			if (idxLT > 0) {
-				qFLAG = false;
-				for (idxRT = idxLT; idxRT < argTARGET.length; idxRT++) {
-					if (argTARGET.substring(idxRT, idxRT + 1) == '"') {
-						if (qFLAG) {
-							break;
-						} else {
-							qFLAG = true;
-						}
-					}
-				}
-				retVAL = argTARGET.substring(idxLT, idxRT + 1);
-			}
+	function tidyCss(argCSS) {
+		// argCSS = css to tidy
+		if (argCSS === undefined) {
+			argCSS = '';
 		}
-		return retVAL;
+		var htmlVAL = argCSS;
+		if (!isEmpty(argCSS)) {
+			var curCSS = argCSS.substring(argCSS.indexOf('"') + 1, argCSS.lastIndexOf('"'));
+			var arrNEW = curCSS.split(' ').map(function (item) {
+				return item.trim();
+			}).filter(function (el) {
+				return el != '';
+			});
+			arrNEW.sort();
+			var newCSS = (arrNEW.join(' ')).trim();
+			htmlVAL = argCSS.replace(curCSS, newCSS);
+		}
+		// returns tidy css
+		return htmlVAL;
 	}
 
-	function clrMCE(argHTML) {
-		var htmlUPDATE = ''; // updated html
-		if (argHTML !== '') {
-			var idxLT = argHTML.indexOf('data-mce-style'); // starting index
-			var idxRT = 0; // ending index
-			var qFLAG = false; // quote flag
-			var mceTARGET = ''; // mce tag
-			if (idxLT > 0) {
-				qFLAG = false;
-				for (idxRT = idxLT; idxRT < argHTML.length; idxRT++) {
-					if (argHTML.substring(idxRT, idxRT + 1) == '"') {
-						if (qFLAG) {
-							break;
-						} else {
-							qFLAG = true;
-						}
-					}
-				}
-				mceTARGET = argHTML.substring(idxLT - 1, idxRT + 1);
-				htmlUPDATE = argHTML.replace(mceTARGET, '');
-			} else {
-				htmlUPDATE = argHTML;
-			}
-		} else {
-			htmlUPDATE = argHTML;
+	function getSpan(argHTML) {
+		// argHTML = inner/outer HTML
+		if (argHTML === undefined) {
+			argHTML = '';
 		}
-		return htmlUPDATE;
+		var htmlVAL = '';
+		if (!isEmpty(argHTML.match(/span/g))) {
+			var tmpVAL = argHTML.match(/<span\b[^>]*>(?:(?=([^<]+))\1|<(?!span\b[^>]*>))*?<\/span>/gi)[0];
+			htmlVAL = tmpVAL.substring(0, tmpVAL.indexOf('>') + 1);
+		}
+		return htmlVAL;
 	}
+
+	function getAln(argTAG) {
+		// argTAG = tag which may contain class
+		if (argTAG === undefined) {
+			argTAG = '';
+		}
+		var htmlVAL = '';
+		if (!isEmpty(argTAG) && !isEmpty(argTAG.match(/aln:txt-/g))) {
+			htmlVAL = argTAG.substring(argTAG.indexOf('aln:txt-'), argTAG.indexOf('aln:txt-') + 10);
+		}
+		// return weight or ''
+		return htmlVAL;
+	}
+
+	function getTag(argHTML, argTAG) {
+		// argHTML = extHTML
+		if (argHTML === undefined) {
+			argHTML = '';
+		}
+		if (argTAG === undefined) {
+			argTAG = '';
+		}
+		var htmlVAL = '';
+		var regEXP = new RegExp('^<' + argTAG, 'gi');
+		if (!isEmpty(argHTML.match(regEXP)) && !isEmpty(argTAG.match(/div|h[1-6]|\bp\b/gi))) {
+			regEXP = RegExp('^<' + argTAG + '\\b[^>]*>', 'gi');
+			htmlVAL = argHTML.match(regEXP)[0];
+		}
+		// return tag or ''
+		return htmlVAL;
+	}
+
+	function isEmpty(argSTR) {
+		return (!argSTR || 0 === argSTR.length);
+	}
+
+	function isReady() {
+		var blnVAL = true;
+		var selTXT = editor.selection.getContent({
+			format : 'text'
+		});
+		if (isEmpty(selTXT)) {
+			alert('SYSTEM MESSAGE\nInvalid or missing text selection.');
+			blnVAL = false;
+		}
+		return blnVAL;
+	}
+
 	editor.addButton('apply_txt_align', {
 		type: 'splitbutton',
 		title: 'Align Text',
 		icon: false,
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTcgMTV2MmgxMHYtMkg3em0tNCA2aDE4di0ySDN2MnptMC04aDE4di0ySDN2MnptNC02djJoMTBWN0g3ek0zIDN2MmgxOFYzSDN6Ii8+PC9zdmc+',
 		onclick: function () {
-			setTag('aln:txt-ct');
+			if (isReady()) {
+				setTag('aln:txt-ct');
+			}
 		},
 		menu: [{
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTE1IDE1SDN2MmgxMnYtMnptMC04SDN2MmgxMlY3ek0zIDEzaDE4di0ySDN2MnptMCA4aDE4di0ySDN2MnpNMyAzdjJoMThWM0gzeiIvPjwvc3ZnPg==',
 			text: '\xa0Align left',
 			onclick: function() {
-				setTag('aln:txt-lt');
+				if (isReady()) {
+					setTag('aln:txt-lt');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTcgMTV2MmgxMHYtMkg3em0tNCA2aDE4di0ySDN2MnptMC04aDE4di0ySDN2MnptNC02djJoMTBWN0g3ek0zIDN2MmgxOFYzSDN6Ii8+PC9zdmc+',
 			text: '\xa0Align Center',
 			onclick: function() {
-				setTag('aln:txt-ct');
+				if (isReady()) {
+					setTag('aln:txt-ct');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTMgMjFoMTh2LTJIM3Yyem02LTRoMTJ2LTJIOXYyem0tNi00aDE4di0ySDN2MnptNi00aDEyVjdIOXYyek0zIDN2MmgxOFYzSDN6Ii8+PC9zdmc+',
 			text: '\xa0Align Right',
 			onclick: function() {
-				setTag('aln:txt-rt');
+				if (isReady()) {
+					setTag('aln:txt-rt');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTMgMjFoMTh2LTJIM3Yyem0wLTRoMTh2LTJIM3Yyem0wLTRoMTh2LTJIM3Yyem0wLTRoMThWN0gzdjJ6bTAtNnYyaDE4VjNIM3oiLz48L3N2Zz4=',
 			text: '\xa0Justify',
 			onclick: function() {
-				setTag('aln:txt-jt');
+				if (isReady()) {
+					setTag('aln:txt-jt');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPHBhdGggZD0iTSAxMCAxMy41IEwgMTAgMTguNSBMIDEyIDE4LjUgTCAxMiA3LjUgTCAxNCA3LjUgTCAxNCAxOC41IEwgMTYgMTguNSBMIDE2IDcuNSBMIDE4IDcuNSBMIDE4IDUuNSBMIDEwIDUuNSBDIDcuNzkgNS41IDYgNy4yOSA2IDkuNSBDIDYgMTEuNzEgNy43OSAxMy41IDEwIDEzLjUgWiIvPgo8L3N2Zz4=',
 			text: '\xa0Indent',
 			onclick: function() {
-				setTag('<p>~</p>');
+				if (isReady()) {
+					setTag('aln:txt-bi');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTkgMTB2NWgyVjRoMnYxMWgyVjRoMlYySDlDNi43OSAyIDUgMy43OSA1IDZzMS43OSA0IDQgNHptMTIgOGwtNC00djNINXYyaDEydjNsNC00eiIvPjwvc3ZnPg==',
 			text: '\xa0Hanging Indent',
 			onclick: function() {
-				setTag('idt:hang');
+				if (isReady()) {
+					setTag('aln:txt-hi');
+				}
 			}
 		}, {
 			icon: true,
 			image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTEwIDEwdjVoMlY0aDJ2MTFoMlY0aDJWMmgtOEM3Ljc5IDIgNiAzLjc5IDYgNnMxLjc5IDQgNCA0em0tMiA3di0zbC00IDQgNCA0di0zaDEydi0ySDh6Ii8+PC9zdmc+',
 			text: '\xa0Paragraph Indent',
 			onclick: function() {
-				setTag('idt:para');
+				if (isReady()) {
+					setTag('aln:txt-pi');
+				}
 			}
 		}],
 	});
 });
 
 /*
- * EOF: apply-text-alignment / plugin.js / 29201000
+ * EOF: apply-text-alignment / plugin.js / 30201018
  */
