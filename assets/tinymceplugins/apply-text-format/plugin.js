638878b042a901f67c414d7c608f41798de78125
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-10-24 16:17:40 -0400

    Update plugin.js

--- assets/tinymceplugins/apply-text-format/plugin.js
+++ assets/tinymceplugins/apply-text-format/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce apply-text-format plugin
  *
  * ###:  plugin.js
- * bld:  30201018
+ * bld:  30201101
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -11,22 +11,67 @@
 tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 	'use strict';
 
-	function setTag(argTAG) {
+	function setEmphasis(argTAG) {
 		if (argTAG === undefined) {
 			argTAG = '';
 		}
 		// init ret val
 		var newHTML = '';
+		// selection html node
+		var htmlNODE = editor.selection.getNode();
+		// get selection node name
+		var selNODE = htmlNODE.nodeName.toLowerCase();
 		// selection content in html format
 		var selTXT = editor.selection.getContent({
 			format: 'html'
 		});
-		// check for multiple paragraphs
-		if (!isEmpty(selTXT.match(/<u>|<s>|<sup>|<sub>|<kbd>/gi))) {
-			// existing class with alignment
-			newHTML = selTXT.replace(/\bu\b|\bs\b|\bsup\b|\bsub\b|\bkbd\b/, argTAG);
-		} else {
-			newHTML = '<' + argTAG + '>' + selTXT + '</' + argTAG + '>';
+		var selTMP = '';
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|\bu\b/i))) {
+			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
+			selTXT = selTMP;
+			// select parent node
+			var tmpNODE;
+			tmpNODE = editor.dom.getParent(htmlNODE,'div,h1,h2,h3,h4,h5,h6,p,span');
+			htmlNODE = tmpNODE;
+			// get new selection node name
+			selNODE = htmlNODE.nodeName.toLowerCase();
+		}
+		selTMP = htmlNODE.innerHTML;
+		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
+		selTMP = editor.dom.getOuterHTML(htmlNODE);
+		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
+		var preTAG;
+		var pstTAG;
+		switch (true) {
+			case (selNODE == 'body'):
+				// strip cr/lf
+				var selNEW = selTXT.replace(/(\r\n|\n|\r)/gm, '');
+				// delimit html string
+				var tagDELIM = selNEW.replace(/(\/(?!span>).*?>)(<.)/g, '$1,$2');
+				// create array from delimited string
+				var htmlARRAY = tagDELIM.split(',');
+				var lstREC = lastRec(htmlARRAY);
+				var idx = 0;
+				// loop through array
+				for (;htmlARRAY[idx];) {
+					if (idx > lstREC) {
+						newHTML += '<p>&nbsp;</p>';
+						break;
+					}
+					selHTML = htmlARRAY[idx];
+					preTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>/, 'si')[0]);
+					pstTAG = (isEmpty(selHTML.match(/<(\/p.*?)>|<(\/h[1-6].*?)>|<(\/div.*?)>/, 'si')) ? '' : selHTML.match(/<(\/p.*?)>|<(\/h[1-6].*?)>|<(d\/iv.*?)>/, 'si')[0]);
+					newHTML += selHTML.replace(preTAG,preTAG + '<' + argTAG + '>').replace(pstTAG,'</' + argTAG + '>' + pstTAG);
+					idx++;
+				}
+				break;
+			case (selTXT == selFULL || selNODE == 'span'):
+				preTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')[0]);
+				pstTAG = (isEmpty(selHTML.match(/<(\/p.*?)>|<(\/h[1-6].*?)>|<(\/div.*?)>|<(\/span.*?)>/, 'si')) ? '' : selHTML.match(/<(\/p.*?)>|<(\/h[1-6].*?)>|<(d\/iv.*?)>|<(\/span.*?)>/, 'si')[0]);
+				newHTML = selHTML.replace(preTAG,preTAG + '<' + argTAG + '>').replace(pstTAG,'</' + argTAG + '>' + pstTAG);
+				break;
+			default:
+				newHTML = '<' + argTAG + '>' + selTXT + '</' + argTAG + '>';
 		}
 		if (!isEmpty(newHTML)) {
 			editor.execCommand('mceReplaceContent', false, newHTML);
@@ -49,11 +94,12 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 			format: 'html'
 		});
 		var selTMP = '';
-		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/gi))) {
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|\bu\b/i))) {
 			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
 			selTXT = selTMP;
 			// select parent node
-			var tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p,span');
+			var tmpNODE;
+			tmpNODE = editor.dom.getParent(htmlNODE,'div,h1,h2,h3,h4,h5,h6,p,span');
 			htmlNODE = tmpNODE;
 			// get new selection node name
 			selNODE = htmlNODE.nodeName.toLowerCase();
@@ -62,86 +108,36 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
 		selTMP = editor.dom.getOuterHTML(htmlNODE);
 		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
-		// check for multiple paragraphs
-		var tagCNT = (selTXT.match(/<\/.*?>/gi) || []).length;
+		var curTAG;
+		var newTAG;
 		switch (true) {
-			case (tagCNT > 1):
-				// strip carrage returns
+			case (selNODE == 'body'):
+				// strip cr/lf
 				var selNEW = selTXT.replace(/(\r\n|\n|\r)/gm, '');
-				// delimit tags with comma
-				var tagDELIM = selNEW.replace(/(<\/(div|h[1-6]|p)>)(<(div|h[1-6]|p)>)/g, '$1,$3');
-				// add to array
-				var tmpARRAY = tagDELIM.split(',');
-				var htmlARRAY = tmpARRAY.filter(function (el) {
-					return el != '';
-				});
+				// delimit html string
+				var tagDELIM = selNEW.replace(/(\/(?!span>).*?>)(<.)/g, '$1,$2');
+				// create array from delimited string
+				var htmlARRAY = tagDELIM.split(',');
+				var lstREC = lastRec(htmlARRAY);
+				var idx = 0;
 				// loop through array
-				var idx;
-				var itmARRAY;
-				var spanNODE;
-				var tagALN;
-				var tagHTML;
-				var tagNEW;
-				var tagTMP;
-				for (idx in htmlARRAY) {
-					// get span item
-					itmARRAY = htmlARRAY[idx];
-					// node
-					spanNODE = itmARRAY.match(/\bp|h[1-6]|div/)[0];
-					// get tag
-					tagHTML = getTag(itmARRAY, spanNODE);
-					// check tag for class element
-					tagALN = getEmb(tagHTML);
-					tagNEW = '';
-					if (isEmpty(tagALN)) {
-						if (isEmpty(tagHTML.match(/class/))) {
-							tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' class="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
-
-						} else {
-							tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-							tagNEW = tidyCss(tagTMP);
-						}
-					} else {
-						tagNEW = tagHTML.replace(tagALN, argTAG);
+				for (;htmlARRAY[idx];) {
+					if (idx > lstREC) {
+						newHTML += '<p>&nbsp;</p>';
+						break;
 					}
-					newHTML += itmARRAY.replace(tagHTML, tagNEW);
+					// save array item to var
+					selHTML = htmlARRAY[idx];
+					curTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>/, 'si')[0]);
+					newTAG = getClass(selHTML, argTAG);
+					newHTML += selHTML.replace(curTAG,newTAG);
+					idx++;
 				}
 				break;
-			case (selNODE == 'span'):
-				// get span
-				var spanHTML = getSpan(selHTML);
-				// check tag for class element
-				tagALN = getEmb(spanHTML);
-				var spanNEW = '';
-				if (isEmpty(tagALN)) {
-					// existing class without weight
-					var spanTMP = spanHTML.substring(0, spanHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-					spanNEW = tidyCss(spanTMP);
-				} else {
-					// existing class with alignment
-					spanNEW = spanHTML.replace(tagALN, argTAG);
-				}
-				newHTML = selHTML.replace(spanHTML, spanNEW);
-				break;
-			case (selTXT == selFULL):
-				// get tag
-				tagHTML = getTag(selHTML, selNODE);
-				// check tag for class element
-				tagALN = getEmb(tagHTML);
-				tagNEW = '';
-				if (isEmpty(tagALN)) {
-					if (isEmpty(tagHTML.match(/class/))) {
-						//
-						tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' class="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
-
-					} else {
-						tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-						tagNEW = tidyCss(tagTMP);
-					}
-				} else {
-					tagNEW = tagHTML.replace(tagALN, argTAG);
-				}
-				newHTML = selHTML.replace(tagHTML, tagNEW);
+			case (selTXT == selFULL || selNODE == 'span'):
+				curTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')[0]);
+				newTAG = getClass(selHTML, argTAG);
+				newHTML = selHTML.replace(curTAG,newTAG);
 				break;
 			default:
 				newHTML = '<span class="' + argTAG + '">' + selTXT + '</span>';
@@ -152,69 +148,110 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 		return;
 	}
 
-	function tidyCss(argCSS) {
-		// argCSS = css to tidy
-		if (argCSS === undefined) {
-			argCSS = '';
-		}
-		var htmlVAL = argCSS;
-		if (!isEmpty(argCSS)) {
-			var curCSS = argCSS.substring(argCSS.indexOf('"') + 1, argCSS.lastIndexOf('"'));
-			var arrNEW = curCSS.split(' ').map(function (item) {
-				return item.trim();
-			}).filter(function (el) {
-				return el != '';
-			});
-			arrNEW.sort();
-			var newCSS = (arrNEW.join(' ')).trim();
-			htmlVAL = argCSS.replace(curCSS, newCSS);
-		}
-		// returns tidy css
-		return htmlVAL;
-	}
-
-	function getSpan(argHTML) {
-		// argHTML = inner/outer HTML
-		if (argHTML === undefined) {
-			argHTML = '';
-		}
-		var htmlVAL = '';
-		if (!isEmpty(argHTML.match(/span/g))) {
-			var tmpVAL = argHTML.match(/<span\b[^>]*>(?:(?=([^<]+))\1|<(?!span\b[^>]*>))*?<\/span>/gi)[0];
-			htmlVAL = tmpVAL.substring(0, tmpVAL.indexOf('>') + 1);
-		}
-		return htmlVAL;
-	}
-
-	function getEmb(argTAG) {
-		// argTAG = tag which may contain class
-		if (argTAG === undefined) {
-			argTAG = '';
-		}
-		var htmlVAL = '';
-		if (!isEmpty(argTAG) && !isEmpty(argTAG.match(/trn:/g))) {
-			htmlVAL = argTAG.substring(argTAG.indexOf('trn:'), argTAG.indexOf('trn:') + 10);
-		}
-		// return weight or ''
-		return htmlVAL;
-	}
-
-	function getTag(argHTML, argTAG) {
-		// argHTML = extHTML
+	function getClass(argHTML, argTAG) {
+		// argHTML = (selHTML) current style
+		// argTAG = update
 		if (argHTML === undefined) {
 			argHTML = '';
 		}
 		if (argTAG === undefined) {
 			argTAG = '';
 		}
-		var htmlVAL = '';
-		var regEXP = new RegExp('^<' + argTAG, 'gi');
-		if (!isEmpty(argHTML.match(regEXP)) && !isEmpty(argTAG.match(/div|h[1-6]|\bp\b/gi))) {
-			regEXP = RegExp('^<' + argTAG + '\\b[^>]*>', 'gi');
-			htmlVAL = argHTML.match(regEXP)[0];
+		var htmlVAL = argHTML;
+		if (!isEmpty(argHTML) && !isEmpty(argTAG)) {
+			// pull key
+			var argKEY = argTAG.match(/^(.*?)-|^(.*?)\+/)[0].replace(/.$/, '');
+			// pull tag
+			var tagHTML = (isEmpty(argHTML.match(/^<(.|\n)*?>/, 'si')) ? '' : argHTML.match(/^<(.|\n)*?>/, 'si')[0]);
+			var tmpVAL1;
+			var tmpVAL2;
+			if (isEmpty(tagHTML.match(/class/))) {
+				tmpVAL1 = tagHTML.match(/^<p.*?>|^<h[1-6].*?>|<span.*?>|^<div.*?>/)[0].replace(/>$/, '') + ' class="' + argTAG + '">';
+				htmlVAL = tidyTag(tmpVAL1);
+			} else {
+				var regEXP = new RegExp(argKEY, 'gis');
+				if (isEmpty(tagHTML.match(regEXP))) {
+					tmpVAL2 = tagHTML.match(/class(.*?)".*?"/)[0].replace(/"$/, '') + ' ' + argTAG + '"';
+					tmpVAL1 = tidyClass(tmpVAL2);
+					htmlVAL = tagHTML.replace(/class(.*?)".*?"/, tmpVAL1);
+				} else {
+					regEXP = RegExp('(' + argKEY + ')(.*?)"');
+					var curTAG = tagHTML.match(regEXP)[0].replace(/"/, '');
+					htmlVAL = tagHTML.replace(curTAG, argTAG);
+				}
+			}
 		}
-		// return tag or ''
 		return htmlVAL;
+	}
+
+	function hasContent(argHTML) {
+		// argHTML = HTML to validate
+		if (argHTML === undefined) {
+			argHTML = '';
+		}
+		var htmlDIV = document.createElement('htmlDIV');
+		htmlDIV.innerHTML = argHTML;
+		// strip html to see what's left
+		var htmlVAL = htmlDIV.textContent || htmlDIV.innerText || '';
+		return (htmlVAL.length > 0);
+	}
+
+	function tidyTag(argTAG) {
+		// argTAG = elements to order
+		if (argTAG === undefined) {
+			argTAG = '';
+		}
+		var htmlVAL = argTAG;
+		if (!isEmpty(argTAG)) {
+			var curTAG = argTAG.match(/\s.*"/)[0].replace(/^\s/, '');
+			var tagDELIM = curTAG.replace(/"\s/, '",');
+			var arrTAG = tagDELIM.split(',');
+			arrTAG.sort();
+			var newTAG = (arrTAG.join(' '));
+			htmlVAL = argTAG.replace(curTAG, newTAG);
+		}
+		// return html
+		return htmlVAL;
+	}
+
+	function tidyClass(argCLS) {
+		// argCLS = classes to tidy
+		if (argCLS === undefined) {
+			argCLS = '';
+		}
+		var htmlVAL = argCLS;
+		if (!isEmpty(argCLS)) {
+			var selCLS = argCLS.match(/"(.*?)"/, 'gi')[0];
+			var curCLS = selCLS.replace(/"/g, '');
+			var arrCLS = curCLS.split(' ');
+			arrCLS.sort();
+			var newCLS = (arrCLS.join(' '));
+			htmlVAL = argCLS.replace(curCLS, newCLS);
+		}
+		// returns tidy classes
+		return htmlVAL;
+	}
+
+	function lastRec(argARRAY) {
+		if (argARRAY === undefined) {
+			argARRAY = [''];
+		}
+		var cntITM;
+		// 0 based
+		var idx = argARRAY.length - 1;
+		for (;argARRAY[idx];) {
+			// save array item to var
+			cntITM = argARRAY[idx];
+			if (hasContent(cntITM)) {
+				break;
+			} else {
+				idx--;
+			}
+		}
+		if (idx < 1) {
+			idx = argARRAY.length - 1;
+		}
+		return idx;
 	}
 
 	function isEmpty(argSTR) {
@@ -240,7 +277,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTEyIDE3YzMuMzEgMCA2LTIuNjkgNi02VjNoLTIuNXY4YzAgMS45My0xLjU3IDMuNS0zLjUgMy41UzguNSAxMi45MyA4LjUgMTFWM0g2djhjMCAzLjMxIDIuNjkgNiA2IDZ6bS03IDJ2MmgxNHYtMkg1eiIvPjwvc3ZnPg==',
 		onclick: function () {
 			if (isReady()) {
-				setTag('u');
+				setEmphasis('u');
 			}
 		},
 		menu: [{
@@ -249,7 +286,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 				text: '\xa0Underline',
 				onclick: function () {
 					if (isReady()) {
-						setTag('u');
+						setEmphasis('u');
 					}
 				},
 },
@@ -259,7 +296,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 				text: '\xa0Strike through',
 				onclick: function () {
 					if (isReady()) {
-						setTag('s');
+						setEmphasis('s');
 					}
 				}
 },
@@ -269,7 +306,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 				text: '\xa0Super script',
 				onclick: function () {
 					if (isReady()) {
-						setTag('sup');
+						setEmphasis('sup');
 					}
 				}
 },
@@ -279,7 +316,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 				text: '\xa0Sub script',
 				onclick: function () {
 					if (isReady()) {
-						setTag('sub');
+						setEmphasis('sub');
 					}
 				}
 },
@@ -313,7 +350,7 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 				text: '\xa0Keyboard',
 				onclick: function () {
 					if (isReady()) {
-						setTag('kbd');
+						setEmphasis('kbd');
 					}
 				}
 },
@@ -330,5 +367,5 @@ tinymce.PluginManager.add('apply_txt_formats', function (editor) {
 });
 
 /*
- * EOF: apply-text-format / plugin.js / 30201018
+ * EOF: apply-text-format / plugin.js / 30201101
  */
