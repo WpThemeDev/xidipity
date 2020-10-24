b2c731da6bfbb950ffe5c65b7fa66cc5d7f83e15
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-10-24 16:23:45 -0400

    Update plugin.js

--- assets/tinymceplugins/apply-text-weight/plugin.js
+++ assets/tinymceplugins/apply-text-weight/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce apply-text-weight plugin
  *
  * ###:  plugin.js
- * bld:  30201018
+ * bld:  30201101
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -11,7 +11,7 @@
 tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 	'use strict';
 
-	function setTag(argTAG) {
+	function setStyle(argTAG) {
 		if (argTAG === undefined) {
 			argTAG = '';
 		}
@@ -26,11 +26,16 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 			format: 'html'
 		});
 		var selTMP = '';
-		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/gi))) {
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|span|strong|sub|sup|\bu\b/i))) {
 			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
 			selTXT = selTMP;
 			// select parent node
-			var tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p,span');
+			var tmpNODE;
+			if (selNODE == 'span') {
+				tmpNODE = editor.dom.getParent(htmlNODE,'div,h1,h2,h3,h4,h5,h6,p');
+			} else {
+				tmpNODE = editor.dom.getParent(htmlNODE,'div,h1,h2,h3,h4,h5,h6,p,span');
+			}
 			htmlNODE = tmpNODE;
 			// get new selection node name
 			selNODE = htmlNODE.nodeName.toLowerCase();
@@ -39,85 +44,36 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
 		selTMP = editor.dom.getOuterHTML(htmlNODE);
 		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
-		// check for multiple paragraphs
-		var tagCNT = (selTXT.match(/<\/.*?>/gi) || []).length;
 		switch (true) {
-			case (tagCNT > 1):
-				// strip carrage returns
+			case (selNODE == 'body'):
+				// strip cr/lf
 				var selNEW = selTXT.replace(/(\r\n|\n|\r)/gm, '');
 				// delimit tags with comma
-				var tagDELIM = selNEW.replace(/(<\/(div|h[1-6]|p)>)(<(div|h[1-6]|p)>)/g, '$1,$3');
-				// add to array
-				var tmpARRAY = tagDELIM.split(',');
-				var htmlARRAY = tmpARRAY.filter(function (el) {
-					return el != '';
-				});
+				var tagDELIM = selNEW.replace(/(\/(?!span>).*?>)(<.)/g, '$1,$2');
+				// create array from delimited string
+				var htmlARRAY = tagDELIM.split(',');
+				var lstREC = lastRec(htmlARRAY);
+				var idx = 0;
+				var curTAG;
+				var newTAG;
 				// loop through array
-				var idx;
-				var itmARRAY;
-				var spanNODE;
-				var tagWGT;
-				var tagHTML;
-				var tagNEW;
-				var tagTMP;
-				for (idx in htmlARRAY) {
-					// get span item
-					itmARRAY = htmlARRAY[idx];
-					// node
-					spanNODE = itmARRAY.match(/\bp|h[1-6]|div/)[0];
-					// get tag
-					tagHTML = getTag(itmARRAY, spanNODE);
-					// check tag for style element
-					tagWGT = getWgt(tagHTML);
-					tagNEW = '';
-					if (isEmpty(tagWGT)) {
-						if (isEmpty(tagHTML.match(/style/))) {
-							tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' style="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
-
-						} else {
-							tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-							tagNEW = tidyCss(tagTMP);
-						}
-					} else {
-						tagNEW = tagHTML.replace(tagWGT, argTAG);
+				for (;htmlARRAY[idx];) {
+					if (idx > lstREC) {
+						newHTML += '<p>&nbsp;</p>';
+						break;
 					}
-					newHTML += itmARRAY.replace(tagHTML, tagNEW);
+					// save array item to var
+					selHTML = htmlARRAY[idx];
+					curTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')[0]);
+					newTAG = getStyle(selHTML, argTAG);
+					newHTML += selHTML.replace(curTAG,newTAG);
+					idx++;
 				}
 				break;
-			case (selNODE == 'span'):
-				// get span
-				var spanHTML = getSpan(selHTML);
-				// check tag for style element
-				tagWGT = getWgt(spanHTML);
-				var spanNEW = '';
-				if (isEmpty(tagWGT)) {
-					// existing style without weight
-					var spanTMP = spanHTML.substring(0, spanHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-					spanNEW = tidyCss(spanTMP);
-				} else {
-					// existing style with weight
-					spanNEW = spanHTML.replace(tagWGT, argTAG);
-				}
-				newHTML = selHTML.replace(spanHTML, spanNEW);
-				break;
-			case (selTXT == selFULL):
-				// get tag
-				tagHTML = getTag(selHTML, selNODE);
-				// check tag for style element
-				tagWGT = getWgt(tagHTML);
-				tagNEW = '';
-				if (isEmpty(tagWGT)) {
-					if (isEmpty(tagHTML.match(/style/))) {
-						tagNEW = tagHTML.substring(0, tagHTML.indexOf('>')) + ' style="' + argTAG + '">' + tagHTML.substring(tagHTML.indexOf('>') + 1);
-
-					} else {
-						tagTMP = tagHTML.substring(0, tagHTML.indexOf('>') - 1) + ' ' + argTAG + '">';
-						tagNEW = tidyCss(tagTMP);
-					}
-				} else {
-					tagNEW = tagHTML.replace(tagWGT, argTAG);
-				}
-				newHTML = selHTML.replace(tagHTML, tagNEW);
+			case (selTXT == selFULL || selNODE == 'span'):
+				curTAG = (isEmpty(selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')) ? '' : selHTML.match(/<(p.*?)>|<(h[1-6].*?)>|<(div.*?)>|<(span.*?)>/, 'si')[0]);
+				newTAG = getStyle(selHTML, argTAG);
+				newHTML = selHTML.replace(curTAG,newTAG);
 				break;
 			default:
 				newHTML = '<span style="' + argTAG + '">' + selTXT + '</span>';
@@ -128,55 +84,43 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 		return;
 	}
 
-	function tidyCss(argCSS) {
-		// argCSS = css to tidy
-		if (argCSS === undefined) {
-			argCSS = '';
+	function lastRec(argARRAY) {
+		if (argARRAY === undefined) {
+			argARRAY = [''];
 		}
-		var htmlVAL = argCSS;
-		if (!isEmpty(argCSS)) {
-			var curCSS = argCSS.substring(argCSS.indexOf('"') + 1, argCSS.lastIndexOf('"'));
-			var arrNEW = curCSS.split(';').map(function (item) {
-				return item.trim();
-			}).filter(function (el) {
-				return el != '';
-			});
-			arrNEW.sort();
-			var newCSS = (arrNEW.join('; ') + ';').trim();
-			htmlVAL = argCSS.replace(curCSS, newCSS);
+		var cntITM;
+		// 0 based
+		var idx = argARRAY.length - 1;
+		for (;argARRAY[idx];) {
+			// save array item to var
+			cntITM = argARRAY[idx];
+			if (hasContent(cntITM)) {
+				break;
+			} else {
+				idx--;
+			}
 		}
-		// returns tidy css
-		return htmlVAL;
+		if (idx < 1) {
+			idx = argARRAY.length - 1;
+		}
+		return idx;
 	}
 
-	function getSpan(argHTML) {
-		// argHTML = inner/outer HTML
+	function hasContent(argHTML) {
+		// argHTML = HTML to validate
 		if (argHTML === undefined) {
 			argHTML = '';
 		}
-		var htmlVAL = '';
-		if (!isEmpty(argHTML.match(/span/g))) {
-			var tmpVAL = argHTML.match(/<span\b[^>]*>(?:(?=([^<]+))\1|<(?!span\b[^>]*>))*?<\/span>/gi)[0];
-			htmlVAL = tmpVAL.substring(0, tmpVAL.indexOf('>') + 1);
-		}
-		return htmlVAL;
+		var htmlDIV = document.createElement('htmlDIV');
+		htmlDIV.innerHTML = argHTML;
+		// strip html to see what's left
+		var htmlVAL = htmlDIV.textContent || htmlDIV.innerText || '';
+		return (htmlVAL.length > 0);
 	}
 
-	function getWgt(argTAG) {
-		// argTAG = tag which may contain style
-		if (argTAG === undefined) {
-			argTAG = '';
-		}
-		var htmlVAL = '';
-		if (!isEmpty(argTAG) && !isEmpty(argTAG.match(/font-weight:/g))) {
-			htmlVAL = argTAG.substring(argTAG.indexOf('font-weight:'), argTAG.indexOf(';', argTAG.indexOf('font-weight:') + 1) + 1);
-		}
-		// return weight or ''
-		return htmlVAL;
-	}
-
-	function getTag(argHTML, argTAG) {
-		// argHTML = extHTML
+	function getStyle(argHTML, argTAG) {
+		// argHTML = (selHTML) current style
+		// argTAG = update
 		if (argHTML === undefined) {
 			argHTML = '';
 		}
@@ -184,12 +128,44 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 			argTAG = '';
 		}
 		var htmlVAL = '';
-		var regEXP = new RegExp('^<' + argTAG, 'gi');
-		if (!isEmpty(argHTML.match(regEXP)) && !isEmpty(argTAG.match(/div|h[1-6]|\bp\b/gi))) {
-			regEXP = RegExp('^<' + argTAG + '\\b[^>]*>', 'gi');
-			htmlVAL = argHTML.match(regEXP)[0];
+		if (!isEmpty(argHTML) && !isEmpty(argTAG)) {
+			// pull key
+			var argKEY = argTAG.match(/^.*?:/)[0].replace(':', '');
+			// pull tag
+			var tagHTML = (isEmpty(argHTML.match(/^<(.|\n)*?>/, 'si')) ? '' : argHTML.match(/^<(.|\n)*?>/, 'si')[0]);
+			if (isEmpty(tagHTML.match(/style/))) {
+				htmlVAL = tagHTML.match(/^<p|^<h[1-6]|<span|^<div/)[0] + ' style="' + argTAG + '">';
+			} else {
+				var regEXP = new RegExp(argKEY, 'gis');
+				if (isEmpty(tagHTML.match(regEXP))) {
+					var tmpVAL2 = tagHTML.match(/style.*?;"/)[0].replace(/;"/, ';') + ' ' + argTAG + '"';
+					var tmpVAL1 = tidyStyle(tmpVAL2);
+					htmlVAL = tagHTML.replace(/style.*?;"/, tmpVAL1);
+				} else {
+					regEXP = RegExp('(' + argKEY + ')(.*?;)');
+					htmlVAL = tagHTML.replace(regEXP, argTAG);
+				}
+			}
 		}
-		// return tag or ''
+		return htmlVAL;
+	}
+
+	function tidyStyle(argSTY) {
+		// argSTY = class(s) to tidy
+		if (argSTY === undefined) {
+			argSTY = '';
+		}
+		var htmlVAL = argSTY;
+		if (!isEmpty(argSTY)) {
+			var regEXP = new RegExp(/"(.*?)"/, 'gi');
+			var selSTY = argSTY.match(regEXP)[0];
+			var curSTY = selSTY.replace(/"/g, '').replace(/;[ ]{1,}/g, ',').replace(/;/g, '');
+			var arrSTY = curSTY.split(',');
+			arrSTY.sort();
+			var newSTY = '"' + (arrSTY.join('; ')) + ';"';
+			htmlVAL = argSTY.replace(regEXP, newSTY);
+		}
+		// returns tidy style
 		return htmlVAL;
 	}
 
@@ -216,7 +192,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTE1LjYgMTAuNzljLjk3LS42NyAxLjY1LTEuNzcgMS42NS0yLjc5IDAtMi4yNi0xLjc1LTQtNC00SDd2MTRoNy4wNGMyLjA5IDAgMy43MS0xLjcgMy43MS0zLjc5IDAtMS41Mi0uODYtMi44Mi0yLjE1LTMuNDJ6TTEwIDYuNWgzYy44MyAwIDEuNS42NyAxLjUgMS41cy0uNjcgMS41LTEuNSAxLjVoLTN2LTN6bTMuNSA5SDEwdi0zaDMuNWMuODMgMCAxLjUuNjcgMS41IDEuNXMtLjY3IDEuNS0xLjUgMS41eiIvPjwvc3ZnPg==',
 		onclick: function () {
 			if (isReady()) {
-				setTag('font-weight: 400;');
+				setStyle('font-weight: 400;');
 			}
 		},
 		menu: [{
@@ -224,7 +200,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '100\xa0-\xa0Thin',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 100;');
+						setStyle('font-weight: 100;');
 					}
 				}
 			},
@@ -233,7 +209,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '200\xa0-\xa0Xtra Light',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 200;');
+						setStyle('font-weight: 200;');
 					}
 				}
 			},
@@ -242,7 +218,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '300\xa0-\xa0Light',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 300;');
+						setStyle('font-weight: 300;');
 					}
 				}
 			},
@@ -251,7 +227,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '400\xa0-\xa0Normal',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 400;');
+						setStyle('font-weight: 400;');
 					}
 				}
 			},
@@ -260,7 +236,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '500\xa0-\xa0Medium',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 500;');
+						setStyle('font-weight: 500;');
 					}
 				}
 			},
@@ -269,7 +245,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '600\xa0-\xa0Semi Bold',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 600;');
+						setStyle('font-weight: 600;');
 					}
 				}
 			},
@@ -278,7 +254,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '700\xa0-\xa0Bold',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 700;');
+						setStyle('font-weight: 700;');
 					}
 				}
 			},
@@ -287,7 +263,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '800\xa0-\xa0Xtra Bold',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 800;');
+						setStyle('font-weight: 800;');
 					}
 				}
 			},
@@ -296,7 +272,7 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 				text: '900\xa0-\xa0Black',
 				onclick: function () {
 					if (isReady()) {
-						setTag('font-weight: 900;');
+						setStyle('font-weight: 900;');
 					}
 				}
 			}
@@ -304,5 +280,5 @@ tinymce.PluginManager.add('apply_txt_weight', function (editor) {
 	});
 });
 /*
- * EOF: apply-text-weight / plugin.js / 30201018
+ * EOF: apply-text-weight / plugin.js / 30201101
  */
