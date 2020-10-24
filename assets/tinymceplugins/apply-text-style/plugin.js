30e924ea3f6d2370cba5e2735036b401d382dd75
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-10-24 16:21:58 -0400

    Update plugin.js

--- assets/tinymceplugins/apply-text-style/plugin.js
+++ assets/tinymceplugins/apply-text-style/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce apply-text-style plugin
  *
  * ###:  plugin.js
- * bld:  30201018
+ * bld:  30201101
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -12,8 +12,6 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 	'use strict';
 
 	function getTxt() {
-		// init ret val
-		var newHTML = '';
 		// selection html node
 		var htmlNODE = editor.selection.getNode();
 		// get selection node name
@@ -23,34 +21,43 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 			format: 'html'
 		});
 		var selTMP = '';
-		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/gi))) {
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|span|strong|sub|sup|\bu\b/i))) {
 			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
 			selTXT = selTMP;
 			// select parent node
-			var tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p,span');
+			var tmpNODE;
+			if (selNODE == 'span') {
+				tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p');
+			} else {
+				tmpNODE = editor.dom.getParent(htmlNODE, 'div,h1,h2,h3,h4,h5,h6,p,span');
+			}
 			htmlNODE = tmpNODE;
 			// get new selection node name
 			selNODE = htmlNODE.nodeName.toLowerCase();
 		}
-		selTMP = htmlNODE.innerHTML;
-		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
 		selTMP = editor.dom.getOuterHTML(htmlNODE);
 		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
-		if (selNODE == 'body') {
-			alert('SYSTEM MESSAGE\nInvalid or too broad of a selection.');
-		} else {
-			var valCLASS = getClass(selHTML);
-			var valSTYLE = getStyle(selHTML);
-			var valTAG = getTag(selHTML);
-			if (isEmpty(valTAG + valSTYLE + valCLASS)) {
-				alert('SYSTEM MESSAGE\nSelected text missing style arguments.');
-			} else {
-				sessionStorage.setItem('memStat', '*');
-				sessionStorage.setItem('memClass', valCLASS);
-				sessionStorage.setItem('memStyle', valSTYLE);
-				sessionStorage.setItem('memTag', valTAG);
-			}
-			editor.selection.collapse();
+		selTMP = htmlNODE.innerHTML;
+		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
+		switch (true) {
+			case (selNODE == 'body'):
+				alert('SYSTEM MESSAGE\nInvalid or too broad of a selection.');
+				break;
+			case (isEmpty(selHTML.match(/class|style|span|<em>|<i>|<kbd>|<s>|<strong>|<sub>|<sup>|<u>/))):
+				alert('SYSTEM MESSAGE\nSelection missing style elements.');
+				break;
+			default:
+				switch (true) {
+					case (selTXT == selFULL):
+						sessionStorage.setItem('srcHTML', selHTML);
+						break;
+					case (!isEmpty(selTXT.match(/<span>/))):
+						sessionStorage.setItem('srcHTML', selFULL);
+						break;
+					default:
+						sessionStorage.setItem('srcHTML', selTXT);
+				}
+				editor.selection.collapse();
 		}
 		return;
 	}
@@ -67,7 +74,7 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 			format: 'html'
 		});
 		var selTMP = '';
-		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/,'gi'))) {
+		if (!isEmpty(selNODE.match(/em|i|kbd|\bs\b|strong|sub|sup|u/, 'gi'))) {
 			selTMP = '<' + selNODE + '>' + selTXT + '</' + selNODE + '>';
 			selTXT = selTMP;
 			// select parent node
@@ -80,137 +87,297 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 		var selFULL = selTMP.replace(/\sdata-mce-style.+"/g, '');
 		selTMP = editor.dom.getOuterHTML(htmlNODE);
 		var selHTML = selTMP.replace(/\sdata-mce-style.+"/g, '');
-		var valCLASS = sessionStorage.getItem('memClass');
-		var valSTYLE = sessionStorage.getItem('memStyle');
-		var valTAG = sessionStorage.getItem('memTag');
+		var preTAG;
+		var pstTAG;
+		var preEmp;
+		var pstEmp;
+		var regEXP;
 		switch (true) {
-			case (selTXT == selFULL && selNODE !== 'span'):
-				var preTAG = selHTML.match(/<p\b|<h[1-6]|<div/, 's')[0];
-				var pstTAG = preTAG.replace(/</, '</') + '>';
-				var preEmp = sessionStorage.getItem('memTag');
-				var pstEmp = preEmp.replace(/</, '</').trim();
-				switch (true) {
-					case (!isEmpty(valCLASS) && !isEmpty(valSTYLE)):
-						newHTML = preTAG + ' ' + valCLASS + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					case (!isEmpty(valCLASS)):
-						newHTML = preTAG + ' ' + valCLASS + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					case (!isEmpty(valSTYLE)):
-						newHTML = preTAG + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					default:
-						newHTML = preTAG + '>' + preEmp + selTXT + pstEmp + pstTAG;
+			//case (selTXT == selFULL && selNODE !== 'span'):
+			case (selTXT == selFULL):
+				// full paragraph
+				regEXP = new RegExp(/^<p.*?span.*?>|^<h[1-6].*?span.*?>|^<div.*?span.*?>/, 'g');
+				preTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+				if (isEmpty(preTAG)) {
+					regEXP = new RegExp(/^<p.*?>|^<h[1-6].*?>|^<div.*?>/, 'g');
+					preTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+				}
+				regEXP = new RegExp(/<\/span.*?\/p.*?>$|<\/span.*?\/h[1-6].*?>$|<\/span.*?\/div.*?>$/, 'g');
+				pstTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+				if (isEmpty(pstTAG)) {
+					regEXP = new RegExp(/<\/p.*?>$|<\/h[1-6].*?>$|<\/div.*?>$/, 'g');
+					pstTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+				}
+				var newTAGS = newTag(sessionStorage.getItem('srcHTML'), selHTML);
+				if (newTAGS[2] == 0) {
+					alert('SYSTEM MESSAGE\nUnable to apply selected style.');
+				} else {
+					newHTML = selHTML.replace(preTAG, newTAGS[0]).replace(pstTAG, newTAGS[1]);
 				}
 				break;
 			case (selNODE == 'body'):
+				// multi paragraph
 				// strip carrage returns
 				var selNEW = selTXT.replace(/(\r\n|\n|\r)/gm, '');
 				// delimit tags with comma
 				var tagDELIM = selNEW.replace(/(<\/(div|h[1-6]|p)>)(<(div|h[1-6]|p)>)/g, '$1,$3');
 				// add to array
-				var tmpARRAY = tagDELIM.split(',');
-				var htmlARRAY = tmpARRAY.filter(function (el) {
-					return el != '';
-				});
-				// loop through array
-				var idx;
-				var itmARRAY;
-				//var spanNODE;
-				//var tagWGT;
-				//var tagHTML;
-				//var tagNEW;
-				//var tagTMP;
+				var htmlARRAY = tagDELIM.split(',');
+				var lstREC = lastRec(htmlARRAY);
+				var idx = 0;
+				var newTAGS;
 				newHTML = '';
-				for (idx in htmlARRAY) {
-					// get span item
-					itmARRAY = htmlARRAY[idx];
-					var preTAG = itmARRAY.match(/<p\b|<h[1-6]|<div/, 's')[0];
-					var pstTAG = preTAG.replace(/</, '</') + '>';
-					var preEmp = sessionStorage.getItem('memTag');
-					var pstEmp = preEmp.replace(/</, '</').trim();
-					var tmpHTML = document.createElement('div');
-					// strip html tags
-					tmpHTML.innerHTML = itmARRAY;
-					selTXT = tmpHTML.textContent || tmpHTML.innerText;
-					switch (true) {
-						case (!isEmpty(valCLASS) && !isEmpty(valSTYLE)):
-							newHTML += preTAG + ' ' + valCLASS + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-							break;
-						case (!isEmpty(valCLASS)):
-							newHTML += preTAG + ' ' + valCLASS + '>' + preEmp + selTXT + pstEmp + pstTAG;
-							break;
-						case (!isEmpty(valSTYLE)):
-							newHTML += preTAG + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-							break;
-						default:
-							newHTML += preTAG + '>' + preEmp + selTXT + pstEmp + pstTAG;
+				// loop through array
+				for (;htmlARRAY[idx];) {
+					if (idx > lstREC) {
+						newHTML += '<p>&nbsp;</p>';
+						break;
 					}
+					selHTML = htmlARRAY[idx];
+					regEXP = new RegExp(/^<p.*?span.*?>|^<h[1-6].*?span.*?>|^<div.*?span.*?>/, 'g');
+					preTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+					if (isEmpty(preTAG)) {
+						regEXP = new RegExp(/^<p.*?>|^<h[1-6].*?>|^<div.*?>/, 'g');
+						preTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+					}
+					regEXP = new RegExp(/<\/span.*?\/p.*?>$|<\/span.*?\/h[1-6].*?>$|<\/span.*?\/div.*?>$/, 'g');
+					pstTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+					if (isEmpty(pstTAG)) {
+						regEXP = new RegExp(/<\/p.*?>$|<\/h[1-6].*?>$|<\/div.*?>$/, 'g');
+						pstTAG = (isEmpty(selHTML.match(regEXP)) ? '' : selHTML.match(regEXP)[0]);
+					}
+					newTAGS = newTag(sessionStorage.getItem('srcHTML'), selHTML);
+					if (newTAGS[2] == 0) {
+						alert('(' + idx + ') SYSTEM MESSAGE\nUnable to apply selected style.');
+						sessionStorage.setItem('srcHTML', '');
+						break;
+					} else {
+						newHTML += selHTML.replace(preTAG, newTAGS[0]).replace(pstTAG, newTAGS[1]);
+					}
+					idx++;
 				}
 				break;
 			default:
-				preTAG = '<span';
-				pstTAG = '</span>';
-				preEmp = sessionStorage.getItem('memTag');
-				pstEmp = preEmp.replace(/</, '</').trim();
-				switch (true) {
-					case (!isEmpty(valCLASS) && !isEmpty(valSTYLE)):
-						newHTML = preTAG + ' ' + valCLASS + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					case (!isEmpty(valCLASS)):
-						newHTML = preTAG + ' ' + valCLASS + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					case (!isEmpty(valSTYLE)):
-						newHTML = preTAG + ' ' + valSTYLE + '>' + preEmp + selTXT + pstEmp + pstTAG;
-						break;
-					default:
-						newHTML = preTAG + '>' + preEmp + selTXT + pstEmp + pstTAG;
+				// paragraph fragment
+				var newTAGS = newTag(sessionStorage.getItem('srcHTML'), selTXT);
+				if (newTAGS[2] == 0) {
+					alert('SYSTEM MESSAGE\nUnable to apply selected style.');
+				} else {
+					newHTML = newTAGS[0] + selTXT + newTAGS[1];
 				}
 		}
 		if (!isEmpty(newHTML)) {
 			editor.execCommand('mceReplaceContent', false, newHTML);
 		}
-		clrMem();
+		sessionStorage.setItem('srcHTML', '');
 		return;
 	}
 
-	function getTag(argHTML) {
-		// argHTML = inner/outer HTML
+	function newTag(argSRC, argTAR) {
+		// argSRC = source html
+		// argTAR = target html
+		if (argSRC === undefined) {
+			argSRC = '';
+		}
+		if (argTAR === undefined) {
+			argTAR = '';
+		}
+		var htmlARRAY = new Array(3);
+		htmlARRAY[2] = 0;
+		if (isEmpty(argSRC.match(/class|style|<em>|<i>|<kbd>|<s>|<strong>|<sub>|<sup>|<u>/))) {
+			argSRC = '';
+			argTAR = '';
+		}
+		if (!isEmpty(argSRC) && !isEmpty(argTAR)) {
+			// vars
+			var tarPRE;
+			var tarPST;
+			var newPRE;
+			var newPST;
+			var regEXP;
+			var valCLASS = getElement(argSRC, 'class');
+			var valSTYLE = getElement(argSRC, 'style');
+			var valTAG = getElement(argSRC, 'tag');
+
+			// the tags to replace
+			regEXP = new RegExp(/^<p.*?span.*?>|^<h[1-6].*?span.*?>|^<div.*?span.*?>/, 'g');
+			tarPRE = (isEmpty(argTAR.match(regEXP)) ? '' : argTAR.match(regEXP)[0]);
+			if (isEmpty(tarPRE)) {
+				regEXP = new RegExp(/^<p.*?>|^<h[1-6].*?>|^<div.*?>/, 'g');
+				tarPRE = (isEmpty(argTAR.match(regEXP)) ? '' : argTAR.match(regEXP)[0]);
+			}
+			regEXP = new RegExp(/<\/span.*?\/p.*?>$|<\/span.*?\/h[1-6].*?>$|<\/span.*?\/div.*?>$/, 'g');
+			tarPST = (isEmpty(argTAR.match(regEXP)) ? '' : argTAR.match(regEXP)[0]);
+			if (isEmpty(tarPST)) {
+				regEXP = new RegExp(/<\/p.*?>$|<\/h[1-6].*?>$|<\/div.*?>$/, 'g');
+				tarPST = (isEmpty(argTAR.match(regEXP)) ? '' : argTAR.match(regEXP)[0]);
+			}
+
+			if (isEmpty(tarPRE)) {
+				// alignment not allowed
+				htmlARRAY[2] = 1;
+				regEXP = new RegExp(/(aln:txt-)(lt|ct|rt|jt|bi|hi|pi)/, 'g');
+				var tmpVAL = valCLASS.replace(regEXP, '').trim();
+				valCLASS = tmpVAL;
+				switch (true) {
+					// has class & style
+					case (!isEmpty(valCLASS) && !isEmpty(valSTYLE)):
+						newPRE = '<span class="' + valCLASS + '"> style="' + valSTYLE + '">';
+						newPST = '</span>';
+						break;
+						// has class & no style
+					case (!isEmpty(valCLASS)):
+						newPRE = '<span class="' + valCLASS + '">';
+						newPST = '</span>';
+						break;
+						// has style & no class
+					case (!isEmpty(valSTYLE)):
+						newPRE = '<span style="' + valSTYLE + '">';
+						newPST = '</span>';
+						break;
+					default:
+						newPRE = '';
+						newPST = '';
+						htmlARRAY[2] = 0;
+				}
+				if (!isEmpty(valTAG)) {
+					tmpVAL = newPRE;
+					newPRE = tmpVAL + '<' + valTAG + '>';
+					tmpVAL = newPST;
+					newPST = '</' + valTAG + '>' + tmpVAL;
+					htmlARRAY[2] = 1;
+				}
+			} else {
+				htmlARRAY[2] = 2;
+				switch (true) {
+					// has alignment & color style
+					case (!isEmpty(valCLASS.match(/(aln:txt-)(lt|ct|rt|jt|bi|hi|pi)/)) && !isEmpty(valSTYLE.match(/color:.*?;/))):
+						newPRE = '<p class="' + valCLASS + '">' + '<span style="' + valSTYLE + '">';
+						newPST = '</span></p>';
+						break;
+						// has alignment & some other style
+					case (!isEmpty(valCLASS.match(/(aln:txt-)(lt|ct|rt|jt|bi|hi|pi)/)) && !isEmpty(valSTYLE)):
+						newPRE = '<p class="' + valCLASS + '"> style="' + valSTYLE + '">';
+						newPST = '</p>';
+						break;
+						// has alignment & no style
+					case (!isEmpty(valCLASS.match(/(aln:txt-)(lt|ct|rt|jt|bi|hi|pi)/))):
+						newPRE = '<p class="' + valCLASS + '">';
+						newPST = '</p>';
+						break;
+						// has class & style
+					case (!isEmpty(valCLASS) && !isEmpty(valSTYLE)):
+						newPRE = tarPRE + '<span class="' + valCLASS + '"> style="' + valSTYLE + '">';
+						newPST = '</span>' + tarPST;
+						break;
+						// has class & no style
+					case (!isEmpty(valCLASS)):
+						tmpVAL = tarPRE.replace(/>/, '');
+						newPRE = tmpVAL + ' class="' + valCLASS + '">';
+						newPST = tarPST;
+						break;
+						// has color style
+					case (!isEmpty(valSTYLE.match(/color:.*?;/))):
+						newPRE = tarPRE + '<span style="' + valSTYLE + '">';
+						newPST = '</span>' + tarPST;
+						break;
+						// has style & no class
+					case (!isEmpty(valSTYLE)):
+						tmpVAL = tarPRE.replace(/>/, '');
+						newPRE = tmpVAL + ' style="' + valSTYLE + '">';
+						newPST = tarPST;
+						break;
+					default:
+						newPRE = '';
+						newPST = '';
+						htmlARRAY[2] = 0;
+				}
+				if (!isEmpty(valTAG)) {
+					if (isEmpty(newPRE)) {
+						tmpVAL = tarPRE;
+					} else {
+						tmpVAL = newPRE;
+					}
+					newPRE = tmpVAL + '<' + valTAG + '>';
+					if (isEmpty(newPST)) {
+						tmpVAL = tarPST;
+					} else {
+						tmpVAL = newPST;
+					}
+					newPST = '</' + valTAG + '>' + tmpVAL;
+					htmlARRAY[2] = 2;
+				}
+			}
+			htmlARRAY[0] = newPRE;
+			htmlARRAY[1] = newPST;
+		}
+		return htmlARRAY;
+	}
+
+
+	function getElement(argHTML, argEL) {
+		// argHTML = html
+		// argEL = element (style/class/tag)
 		if (argHTML === undefined) {
 			argHTML = '';
 		}
+		if (argEL === undefined) {
+			argEL = '';
+		}
 		var htmlVAL = '';
-		var regEXP = new RegExp(/<em>|<i>|<kbd>|<s>|<strong>|<sub>|<sup>|<u>/,'is');
-		if (!isEmpty(argHTML.match(regEXP))) {
-			htmlVAL = argHTML.match(regEXP)[0];
+		if (!isEmpty(argHTML) && !isEmpty(argEL.match(/class|style|tag/))) {
+			var regEXP;
+			var htmlTMP;
+			if (argEL == 'tag') {
+				regEXP = new RegExp(/<em>|<i>|<kbd>|<s>|<strong>|<sub>|<sup>|<u>/, 'si');
+				htmlTMP = (isEmpty(argHTML.match(regEXP)) ? '' : argHTML.match(regEXP)[0]);
+				if (!isEmpty(htmlTMP)) {
+					htmlVAL = htmlTMP.replace('<', '').replace('>', '');
+				}
+			} else if (argEL == 'class' || argEL == 'style') {
+				regEXP = new RegExp(argEL + '.*?".*?"', 'si');
+				htmlTMP = (isEmpty(argHTML.match(regEXP)) ? '' : argHTML.match(regEXP)[0]);
+				if (!isEmpty(htmlTMP)) {
+					regEXP = RegExp(argEL);
+					htmlVAL = htmlTMP.replace(regEXP, '').replace(/=/, '').replace(/"/g, '');
+				}
+			}
 		}
 		return htmlVAL;
 	}
 
-	function getClass(argHTML) {
-		// argHTML = inner/outer HTML
+	function hasContent(argHTML) {
+		// argHTML = HTML to validate
 		if (argHTML === undefined) {
 			argHTML = '';
 		}
-		var htmlVAL = '';
-		if (!isEmpty(argHTML.match(/class/))) {
-			var regEXP = new RegExp(/class\b=".*?"/, 'is');
-			htmlVAL = argHTML.match(regEXP)[0];
-		}
-		return htmlVAL;
+		var htmlDIV = document.createElement('htmlDIV');
+		htmlDIV.innerHTML = argHTML;
+		// strip html to see what's left
+		var htmlVAL = htmlDIV.textContent || htmlDIV.innerText || '';
+		return (htmlVAL.length > 0);
 	}
 
-	function getStyle(argHTML) {
-		// argHTML = inner/outer HTML
-		if (argHTML === undefined) {
-			argHTML = '';
+	function lastRec(argARRAY) {
+		if (argARRAY === undefined) {
+			argARRAY = [''];
 		}
-		var htmlVAL = '';
-		if (!isEmpty(argHTML.match(/style/))) {
-			var regEXP = new RegExp(/style\b.*?;"/, 'is');
-			htmlVAL = argHTML.match(regEXP)[0];
+		var cntITM;
+		// 0 based
+		var idx = argARRAY.length - 1;
+		for (; argARRAY[idx];) {
+			// save array item to var
+			cntITM = argARRAY[idx];
+			if (hasContent(cntITM)) {
+				break;
+			} else {
+				idx--;
+			}
 		}
-		return htmlVAL;
+		if (idx < 1) {
+			idx = argARRAY.length - 1;
+		}
+		return idx;
 	}
 
 	function isEmpty(argSTR) {
@@ -220,22 +387,14 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 	function isReady() {
 		var blnVAL = true;
 		var selTXT = editor.selection.getContent({
-			format : 'text'
+			format: 'text'
 		});
 		if (isEmpty(selTXT)) {
 			alert('SYSTEM MESSAGE\nInvalid or missing text selection.');
 			blnVAL = false;
-			clrMem();
+			sessionStorage.setItem('srcHTML', '');
 		}
 		return blnVAL;
-	}
-
-	function clrMem() {
-		sessionStorage.setItem('memStat', '');
-		sessionStorage.setItem('memClass', '');
-		sessionStorage.setItem('memStyle', '');
-		sessionStorage.setItem('memTag', '');
-		return;
 	}
 
 	editor.addButton('app_txt_style', {
@@ -243,16 +402,16 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 		icon: false,
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTE4IDRWM2MwLS41NS0uNDUtMS0xLTFINWMtLjU1IDAtMSAuNDUtMSAxdjRjMCAuNTUuNDUgMSAxIDFoMTJjLjU1IDAgMS0uNDUgMS0xVjZoMXY0SDl2MTFjMCAuNTUuNDUgMSAxIDFoMmMuNTUgMCAxLS40NSAxLTF2LTloOFY0aC0zeiIvPjwvc3ZnPg==',
 		onclick: function () {
-			if (sessionStorage.getItem('memStat') == null) {
-				clrMem();
+			if (sessionStorage.getItem('srcHTML') == null) {
+				sessionStorage.setItem('srcHTML', '');
 			}
-			if (isEmpty(sessionStorage.getItem('memStat'))) {
+			if (isEmpty(sessionStorage.getItem('srcHTML'))) {
 				if (isReady()) {
 					getTxt();
 				}
 			} else {
 				if (isReady()) {
-					if (isEmpty(sessionStorage.getItem('memClass') + sessionStorage.getItem('memStyle') + sessionStorage.getItem('memTag'))) {
+					if (isEmpty(sessionStorage.getItem('srcHTML'))) {
 						alert('SYSTEM MESSAGE\nInvalid or missing style elements.');
 					} else {
 						setTxt();
@@ -264,5 +423,5 @@ tinymce.PluginManager.add('app_txt_style', function (editor) {
 });
 
 /*
- * EOF: apply-text-style / plugin.js / 30201018
+ * EOF: apply-text-style / plugin.js / 30201101
  */
