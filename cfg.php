dc73d861b35e405745527f18018c9f4788724ca5
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-09 15:53:45 -0500

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,19 @@
+<?php
+// xidipity configuration constants
+define('CopyRightHolder', get_bloginfo( 'name' ));
+define('EmojiDisplay', 'yes');
+define('FontAwesomeVersion', '5.12.1');
+define('FavoriteIcon', 'default');
+define('FooterAlignment', 'center');
+define('HeaderAlignment', 'center');
+define('HeaderHeight', '12rem');
+define('HeaderImage', 'none');
+define('HeaderLogo', 'https://doc.xidipity.com/wp-content/uploads/2020/12/s_xidipity_logo_320x137.png');
+define('MenuAlignment', 'center');
+define('MenuDisplay', 'yes');
+define('MenuWidth', '80%');
+define('MessageDefault', 'Hello :)');
+define('SidebarAlignment', 'right');
+define('FeaturedImage', 'left');
+define('ThemeSEO', get_bloginfo( 'name' ) . ' WordPress Theme');
+?>
