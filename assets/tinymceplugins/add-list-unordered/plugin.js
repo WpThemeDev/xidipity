b57433ce1d479be360613a0bbb6fc5e0620fa4a5
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-13 07:18:42 -0500

    Update plugin.js

--- assets/tinymceplugins/add-list-unordered/plugin.js
+++ assets/tinymceplugins/add-list-unordered/plugin.js
@@ -3,7 +3,7 @@
  * Tinymce add-list-unordered plugin
  *
  * ###:  plugin.js
- * bld:  30201115
+ * bld:  30201201
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2019-2020 John Baer
  *
@@ -17,8 +17,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 		image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDBWMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNNCAxMC41Yy0uODMgMC0xLjUuNjctMS41IDEuNXMuNjcgMS41IDEuNSAxLjUgMS41LS42NyAxLjUtMS41LS42Ny0xLjUtMS41LTEuNXptMC02Yy0uODMgMC0xLjUuNjctMS41IDEuNVMzLjE3IDcuNSA0IDcuNSA1LjUgNi44MyA1LjUgNiA0LjgzIDQuNSA0IDQuNXptMCAxMmMtLjgzIDAtMS41LjY4LTEuNSAxLjVzLjY4IDEuNSAxLjUgMS41IDEuNS0uNjggMS41LTEuNS0uNjctMS41LTEuNS0xLjV6TTcgMTloMTR2LTJIN3Yyem0wLTZoMTR2LTJIN3Yyem0wLTh2MmgxNFY1SDd6Ii8+PC9zdmc+',
 		onclick: function() {
 			var dom = editor.dom;
-			var uniqueID = 'tag';
-			uniqueID += Math.floor(Math.random() * 1000);
+			var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 			var html = '<ul><li id="';
 			html += uniqueID;
 			html += '"></li></ul>';
@@ -31,8 +30,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 			text: '•\xa0Standard',
 			onclick: function() {
 				var dom = editor.dom;
-				var uniqueID = 'tag';
-				uniqueID += Math.floor(Math.random() * 1000);
+				var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 				var html = '<ul><li id="';
 				html += uniqueID;
 				html += '"></li></ul>';
@@ -45,8 +43,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 			text: '•\xa0Circle',
 			onclick: function() {
 				var dom = editor.dom;
-				var uniqueID = 'tag';
-				uniqueID += Math.floor(Math.random() * 1000);
+				var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 				var html = '<ul class="circle"><li id="';
 				html += uniqueID;
 				html += '"></li></ul>';
@@ -59,8 +56,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 			text: '•\xa0Dash',
 			onclick: function() {
 				var dom = editor.dom;
-				var uniqueID = 'tag';
-				uniqueID += Math.floor(Math.random() * 1000);
+				var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 				var html = '<ul class="dash"><li id="';
 				html += uniqueID;
 				html += '"></li></ul>';
@@ -73,8 +69,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 			text: '•\xa0Square',
 			onclick: function() {
 				var dom = editor.dom;
-				var uniqueID = 'tag';
-				uniqueID += Math.floor(Math.random() * 1000);
+				var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 				var html = '<ul class="square"><li id="';
 				html += uniqueID;
 				html += '"></li></ul>';
@@ -87,8 +82,7 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 			text: '•\xa0Mixed',
 			onclick: function() {
 				var dom = editor.dom;
-				var uniqueID = 'tag';
-				uniqueID += Math.floor(Math.random() * 1000);
+				var uniqueID = (Math.random().toString(16)+"000000000").substr(4,5);
 				var html = '<ul class="mixed"><li id="';
 				html += uniqueID;
 				html += '"></li></ul>';
@@ -99,7 +93,6 @@ tinymce.PluginManager.add('add_lst_unorder', function(editor) {
 		}]
 	});
 });
-
 /*
- * EOF: add-list-unordered / plugin.js / 30201115
+ * EOF: add-list-unordered / plugin.js / 30201201
  */
