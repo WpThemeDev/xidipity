9097a3f5133ad91b08da5dec63d4fbd012381978
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-23 13:51:23 -0500

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -16,9 +16,9 @@ define('MessageDefault', 'Hello :)');
 define('SidebarAlignment', 'right');
 define('FeaturedImage', 'left');
 define('ThemeSEO', get_bloginfo( 'name' ) . ' WordPress Theme');
-define('FontSans', 'Roboto');
-define('FontSerif', 'Roboto Slab');
-define('FontMono', 'Roboto Mono');
-define('FontCursive', 'Kalam');
-define('FontFantasy', 'Kaushan Script');
+define('FontSans', '<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">');
+define('FontSerif', '<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">');
+define('FontMono', '<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">');
+define('FontCursive', '<link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap" rel="stylesheet">');
+define('FontFantasy', '<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">');
 ?>
