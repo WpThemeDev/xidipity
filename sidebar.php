fec256ff6938c82a326f054fefd4cbc83382f81c
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-31 10:29:15 -0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -6,14 +6,14 @@
  * ###:  sidebar.php
  * bld:  28200801
  * src:  github.com/WpThemeDev/xidipity/
- * (C)   2019-2020 John Baer
+ * (C)   2018-2021 John Baer
  *
  */
-echo '<cmt name="begin">SIDEBAR/PHP</cmt>' . "\n";
+echo '<!-- SIDEBAR/PHP -->' . "\n";
 echo '<section class="aln:text-left fxd:4 fxe:6 wd:0 fb:100% md)fb:20 prt[dsp:none]">' . "\n";
 dynamic_sidebar('sidebar-1');
 echo '</section>' . "\n";
-echo '<cmt name="end">SIDEBAR/PHP</cmt>' . "\n";
+echo '<!-- /SIDEBAR/PHP -->' . "\n";
 /*
  * EOF: sidebar.php / 28200801
 */
