f1c4a79f6d02b12592310a9126ba46c51e920064
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-31 10:38:17 -0500

    Update extras.php

--- inc/extras.php
+++ inc/extras.php
@@ -6,7 +6,7 @@
  * ###:  inc/extras.php
  * bld:  30201115
  * src:  github.com/WpThemeDev/xidipity/
- * (C)   2019-2020 John Baer
+ * (C)   2018-2021 John Baer
  *
  *  PRG     Build     Description
  *  ------  --------  ---------------------------------------------------------
