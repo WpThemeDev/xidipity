ea9a0ece6eefe3bc8ac38361cfdbae211ef79182
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2020-12-31 10:57:39 -0500

    Update plugin.js

--- assets/tinymceplugins/apply-text-italic/plugin.js
+++ assets/tinymceplugins/apply-text-italic/plugin.js
@@ -5,7 +5,7 @@
  * ###:  plugin.js
  * bld:  30201101
  * src:  github.com/WpThemeDev/xidipity/
- * (C)   2019-2020 John Baer
+ * (C)   2018-2021 John Baer
  *
  */
 tinymce.PluginManager.add('apply_txt_italic', function (editor) {
