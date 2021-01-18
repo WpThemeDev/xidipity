f98e7bb12e2b1a76c53cc95fc62cca7647c622df
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2021-01-18 16:39:16 -0500

    Update front-page.php

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -6,7 +6,7 @@
  * The template for displaying font page
  *
  * ###:  page-templates/font-page.php
- * bld:  31201215
+ * bld:  01210115
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2018-2021 John Baer
  *
@@ -39,7 +39,7 @@ else
 	echo '<main class="fx:rw md)fx:r fxa:1 fxc:1 sm)mar:hrz+0.5">' . "\n";
 	echo '<section class="fxd:4 fxe:6 wd:0 fb:100% mar:bt+0.5 md)mar:rt+0.5">' . "\n";
 }
-echo '<article class="box:shadow bkg:content txt:content dsp:block pad:hrz+1 ht:min10 wd:100%">' . "\n";
+echo '<article class="box:shadow bkg:content txt:content dsp:blk pad:hrz+1 ht:min10 wd:100%">' . "\n";
 if (have_posts())
 {
 	the_post();
@@ -95,6 +95,6 @@ get_footer();
 */
 wp_reset_postdata();
 /*
- * EOF: page-templates/font-page.php / 31201215
+ * EOF: page-templates/font-page.php / 01210115
 */
 ?>
