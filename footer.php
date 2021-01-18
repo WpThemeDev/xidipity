055465c47ffd4fe4f5b107792acc3dcc72c68f1d
John Baer <37503384+WpThemeDev@users.noreply.github.com>
2021-01-18 16:25:59 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  * The template for displaying page footer
  *
  * ###:  footer.php
- * bld:  31201215
+ * bld:  01210115
  * src:  github.com/WpThemeDev/xidipity/
  * (C)   2018-2021 John Baer
  *
@@ -14,17 +14,17 @@ echo '<div class="fxd:1 fxe:2 fb:100%">' . "\n";
 if (xty('ftr-aln') == 'left')
 {
 	echo '<footer class="fx:r fxa:1 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-lt dsp:block fnt:siz-1">' . "\n";
+	echo '<div class="aln:txt-lt dsp:blk fnt:siz-1">' . "\n";
 }
 elseif (xty('ftr-aln') == 'right')
 {
 	echo '<footer class="fx:r fxa:2 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-rt dsp:block fnt:siz-1">' . "\n";
+	echo '<div class="aln:txt-rt dsp:blk fnt:siz-1">' . "\n";
 }
 else
 {
 	echo '<footer class="fx:r fxa:3 fxc:3 ht:min6 fb:100%">' . "\n";
-	echo '<div class="aln:txt-ct dsp:block fnt:siz-1">' . "\n";
+	echo '<div class="aln:txt-ct dsp:blk fnt:siz-1">' . "\n";
 }
 echo '<p>' . blog_copyright() . '</p>';
 echo '<p><a href="https://xidipity.com/">Xidipity Theme</a> &sdot; Powered by <a href="https://wordpress.org">WordPress</a>';
@@ -46,6 +46,6 @@ wp_footer();
 echo '</body>' . "\n";
 echo '</html>' . "\n";
 /*
- * EOF: footer.php / 31201215
+ * EOF: footer.php / 01210115
 */
 ?>
